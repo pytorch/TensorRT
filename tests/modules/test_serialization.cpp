f3370c4770f39dc08af566c1c4e297d5166caf66
Naren Dasan <naren@narendasan.com>
2020-05-30 21:27:27 -0700

    test: Test serialization to make sure it works
    Signed-off-by: Naren Dasan <naren@narendasan.com>
    Signed-off-by: Naren Dasan <narens@nvidia.com>

---
+++ tests/modules/test_serialization.cpp
@@ -0,0 +1,34 @@
+#include "module_test.h"
+
+TEST_P(ModuleTests, SerializedModuleIsStillCorrect) {
+    std::vector<torch::jit::IValue> jit_inputs_ivalues;
+    std::vector<torch::jit::IValue> trt_inputs_ivalues;
+    for (auto in_shape : input_shapes) {
+        auto in = at::randint(5, in_shape, {at::kCUDA});
+        jit_inputs_ivalues.push_back(in.clone());
+        trt_inputs_ivalues.push_back(in.clone());
+    }
+
+    auto trt_mod = trtorch::CompileGraph(mod, input_shapes);
+    torch::jit::IValue trt_results_ivalues = trtorch::tests::util::RunModuleForward(trt_mod, trt_inputs_ivalues);
+    std::vector<at::Tensor> trt_results;
+    trt_results.push_back(trt_results_ivalues.toTensor());
+
+    trt_mod.save("test_serialization_mod.ts");
+    auto jit_mod = torch::jit::load("test_serialization_mod.ts");
+
+    torch::jit::IValue jit_results_ivalues = trtorch::tests::util::RunModuleForward(jit_mod, jit_inputs_ivalues);
+    std::vector<at::Tensor> jit_results;
+    jit_results.push_back(jit_results_ivalues.toTensor());
+
+    for (size_t i = 0; i < trt_results.size(); i++) {
+        ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[i], trt_results[i].reshape_as(jit_results[i]), 2e-5));
+    }
+}
+
+
+INSTANTIATE_TEST_SUITE_P(CompiledModuleForwardIsCloseSuite,
+                         ModuleTests,
+                         testing::Values(
+                            PathAndInSize({"tests/modules/resnet18_traced.jit.pt",
+                                          {{1,3,224,224}}})));
