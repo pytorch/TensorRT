57143c237996d59c67054ee196879e0d0284d90c
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2020-06-08 19:18:56 -0700

    test(/tests/core/converters/): added 3 tests for linear, bilinear, trilinear ops. removed redundant tests Signed-off-by: Abhiram Iyer <abhirami@nvidia.com>
    Signed-off-by: Abhiram Iyer <abhi.iyer.ai@gmail.com>

--- tests/core/converters/test_interpolate.cpp
+++ tests/core/converters/test_interpolate.cpp
@@ -33,35 +33,6 @@ TEST(Converters, ATenUpsampleNearest1dConvertsCorrectly) {
     ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt, 2e-6));
 }
 
-TEST(Converters, ATenUpsampleNearest2dConvertsCorrectly1dOutputSize) {
-    const auto graph = R"IR(
-      graph(%0 : Tensor):
-        %2 : int = prim::Constant[value=10]()
-        %3 : int[] = prim::ListConstruct(%2, %2)
-        %4 : None = prim::Constant()
-        %5 : Tensor = aten::upsample_nearest2d(%0, %3, %4, %4)
-        return (%5))IR";
-    
-    auto g = std::make_shared<torch::jit::Graph>();
-
-    torch::jit::parseIR(graph, &*g);
-
-    // Input Tensor needs to be 4D for TensorRT upsample_nearest2d
-    auto in = at::randint(1, 10, {10, 2, 2, 2}, {at::kCUDA});
-
-    auto jit_in = at::clone(in);
-    auto params = trtorch::core::conversion::get_named_params(g->inputs(), {});
-    auto jit_results = trtorch::tests::util::RunGraph(g, params, {jit_in});
-
-    auto trt_in = at::clone(in);
-    params = trtorch::core::conversion::get_named_params(g->inputs(), {});
-    auto trt_results = trtorch::tests::util::RunGraphEngine(g, params, {trt_in});
-
-    auto trt = trt_results[0].reshape(jit_results[0].sizes());
-
-    ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt, 2e-6));
-}
-
 TEST(Converters, ATenUpsampleNearest2dConvertsCorrectly2dOutputSize) {
     const auto graph = R"IR(
       graph(%0 : Tensor):
@@ -91,7 +62,7 @@ TEST(Converters, ATenUpsampleNearest2dConvertsCorrectly2dOutputSize) {
     ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt, 2e-6));
 }
 
-TEST(Converters, ATenUpsampleNearest3dConvertsCorrectly1dOutputSize) {
+TEST(Converters, ATenUpsampleNearest3dConvertsCorrectly3dOutputSize) {
     const auto graph = R"IR(
       graph(%0 : Tensor):
         %2 : int = prim::Constant[value=10]()
@@ -120,20 +91,81 @@ TEST(Converters, ATenUpsampleNearest3dConvertsCorrectly1dOutputSize) {
     ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt, 2e-6));
 }
 
-TEST(Converters, ATenUpsampleNearest3dConvertsCorrectly3dOutputSize) {
+TEST(Converters, ATenUpsampleLinear1dConvertsCorrectly) {
     const auto graph = R"IR(
       graph(%0 : Tensor):
         %2 : int = prim::Constant[value=10]()
-        %3 : int[] = prim::ListConstruct(%2, %2, %2)
-        %4 : None = prim::Constant()
-        %5 : Tensor = aten::upsample_nearest3d(%0, %3, %4, %4, %4)
-        return (%5))IR";
+        %3 : int[] = prim::ListConstruct(%2)
+        %4 : bool = prim::Constant[value=1]()
+        %5 : None = prim::Constant()
+        %6 : Tensor = aten::upsample_linear1d(%0, %3, %4, %5)
+        return (%6))IR";
     
     auto g = std::make_shared<torch::jit::Graph>();
 
     torch::jit::parseIR(graph, &*g);
 
-    // Input Tensor needs to be 5D for TensorRT upsample_nearest3d
+    // Input Tensor needs to be 3D for TensorRT upsample_linear1d
+    auto in = at::randint(1, 10, {10, 2, 2}, {at::kCUDA});
+
+    auto jit_in = at::clone(in);
+    auto params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+    auto jit_results = trtorch::tests::util::RunGraph(g, params, {jit_in});
+
+    auto trt_in = at::clone(in);
+    params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+    auto trt_results = trtorch::tests::util::RunGraphEngine(g, params, {trt_in});
+
+    auto trt = trt_results[0].reshape(jit_results[0].sizes());
+
+    ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt, 2e-6));
+}
+
+TEST(Converters, ATenUpsampleBilinear2dConvertsCorrectly2dOutputSize) {
+    const auto graph = R"IR(
+      graph(%0 : Tensor):
+        %2 : int = prim::Constant[value=10]()
+        %3 : int[] = prim::ListConstruct(%2, %2)
+        %4 : bool = prim::Constant[value=1]()
+        %5 : None = prim::Constant()
+        %6 : Tensor = aten::upsample_bilinear2d(%0, %3, %4, %5, %5)
+        return (%6))IR";
+    
+    auto g = std::make_shared<torch::jit::Graph>();
+
+    torch::jit::parseIR(graph, &*g);
+
+    // Input Tensor needs to be 4D for TensorRT upsample_bilinear2d
+    auto in = at::randint(1, 10, {10, 2, 2, 2}, {at::kCUDA});
+
+    auto jit_in = at::clone(in);
+    auto params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+    auto jit_results = trtorch::tests::util::RunGraph(g, params, {jit_in});
+
+    auto trt_in = at::clone(in);
+    params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+    auto trt_results = trtorch::tests::util::RunGraphEngine(g, params, {trt_in});
+
+    auto trt = trt_results[0].reshape(jit_results[0].sizes());
+
+    ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt, 2e-6));
+}
+
+TEST(Converters, ATenUpsampleTrilinear3dConvertsCorrectly3dOutputSize) {
+    const auto graph = R"IR(
+      graph(%0 : Tensor):
+        %2 : int = prim::Constant[value=10]()
+        %3 : int[] = prim::ListConstruct(%2, %2, %2)
+        %4 : bool = prim::Constant[value=1]()
+        %5 : None = prim::Constant()
+        %6 : Tensor = aten::upsample_trilinear3d(%0, %3, %4, %5, %5, %5)
+        return (%6))IR";
+    
+    auto g = std::make_shared<torch::jit::Graph>();
+
+    torch::jit::parseIR(graph, &*g);
+
+    // Input Tensor needs to be 5D for TensorRT upsample_trilinear3d
     auto in = at::randint(1, 10, {10, 2, 2, 2, 2}, {at::kCUDA});
 
     auto jit_in = at::clone(in);
