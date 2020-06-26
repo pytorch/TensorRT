d8161425b91fa8134b78b9d86cc762301526fb3f
Abhiram Iyer <abhi.iyer.ai@gmail.com>
2020-06-26 11:52:04 -0700

    test(aten::select): added test case for single call to select Signed-off-by: Abhiram Iyer <abhirami@nvidia.com>
    Signed-off-by: Abhiram Iyer <abhi.iyer.ai@gmail.com>
    Signed-off-by: Vinh Nguyen <vinhn@nvidia.com>

--- tests/core/converters/test_select.cpp
+++ tests/core/converters/test_select.cpp
@@ -4,6 +4,32 @@
 #include "tests/util/util.h"
 #include "core/compiler.h"
 
+TEST(Converters, ATenSelectIntConvertsCorrectly) {
+    const auto graph = R"IR(
+      graph(%0 : Tensor):
+        %2 : int = prim::Constant[value=0]()
+        %3 : Tensor = aten::select(%0, %2, %2)
+        return (%3))IR";
+    
+    auto g = std::make_shared<torch::jit::Graph>();
+
+    torch::jit::parseIR(graph, &*g);
+
+    auto in = at::randint(1, 10, {4, 4, 4}, {at::kCUDA});
+
+    auto jit_in = at::clone(in);
+    auto params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+    auto jit_results = trtorch::tests::util::RunGraph(g, params, {jit_in});
+
+    auto trt_in = at::clone(in);
+    params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+    auto trt_results = trtorch::tests::util::RunGraphEngine(g, params, {trt_in});
+
+    auto trt = trt_results[0].reshape(jit_results[0].sizes());
+
+    ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt, 2e-6));
+}
+
 TEST(Converters, ATenSelectIntTwiceConvertsCorrectly) {
     const auto graph = R"IR(
       graph(%0 : Tensor):
