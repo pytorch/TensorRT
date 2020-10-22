efd2099bbc04b2c9cc4ec99a10604fa308814273
Naren Dasan <naren@narendasan.com>
2020-10-22 12:39:45 -0700

    fix(//py): Fix long description section of setup.py
    Thanks to @mneilly for catching this
    
    Signed-off-by: Naren Dasan <naren@narendasan.com>
    Signed-off-by: Naren Dasan <narens@nvidia.com>

--- py/setup.py
+++ py/setup.py
@@ -190,7 +190,7 @@ ext_modules = [
                             )
 ]
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
