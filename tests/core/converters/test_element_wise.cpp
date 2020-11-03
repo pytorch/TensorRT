9425ba6b6a55dc20c1a20c75f2a3d1e5b716d424
uni19 <csljxing@gmail.com>
2020-11-03 21:22:06 +0800

    update test case for element_wise
    Signed-off-by: uni19 <csljxing@gmail.com>

--- tests/core/converters/test_element_wise.cpp
+++ tests/core/converters/test_element_wise.cpp
@@ -4,19 +4,25 @@
 #include "tests/util/util.h"
 #include "torch/csrc/jit/ir/irparser.h"
 
-void pointwise_test_helper(std::string graph_ir, bool singleInput) {
-  auto g = std::make_shared<torch::jit::Graph>();
-  torch::jit::parseIR(graph_ir, &*g);
-
-  // singleInput case is enabled when elementwise operation is performed
-  // with an input and a constant embedded in graph
-  std::vector<at::Tensor> torch_inputs;
-  torch_inputs.push_back(at::randint(1, 5, {5}, {at::kCUDA}));
-  if (!singleInput) {
-    torch_inputs.push_back(at::randint(1, 5, {5}, {at::kCUDA}));
-  }
-  auto params = trtorch::core::conversion::get_named_params(g->inputs(), {});
-  auto jit_results = trtorch::tests::util::RunGraph(g, params, torch_inputs);
+void pointwise_test_helper(std::string graph_ir, bool singleInput,
+                           std::vector<int64_t> shape1={5}, std::vector<int64_t> shape2={5}) {
+    auto g = std::make_shared<torch::jit::Graph>();
+    torch::jit::parseIR(graph_ir, &*g);
+    
+    // singleInput case is enabled when elementwise operation is performed 
+    // with an input and a constant embedded in graph
+    std::vector<at::Tensor> torch_inputs;
+    torch_inputs.push_back(at::randint(1, 5, shape1, {at::kCUDA}));
+    if (!singleInput) {
+        torch_inputs.push_back(at::randint(1, 5, shape2, {at::kCUDA}));
+    }
+    auto params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+    auto jit_results = trtorch::tests::util::RunGraph(g, params, torch_inputs);
+    
+    std::vector<at::Tensor> trt_inputs;
+    for (auto in : torch_inputs) {
+        trt_inputs.push_back(at::clone(in));
+    }
 
   std::vector<at::Tensor> trt_inputs;
   for (auto in : torch_inputs) {
@@ -35,17 +41,10 @@ TEST(Converters, ATenAddConvertsCorrectly) {
         %2 : int = prim::Constant[value=1]()
         %3 : Tensor = aten::add(%0, %1, %2)
         return (%3))IR";
-  pointwise_test_helper(graph, false);
+    pointwise_test_helper(graph, false);
+    pointwise_test_helper(graph, false, {3, 4}, {4});
 }
 
-// TEST(Converters, ATenAddWithScaleConvertsCorrectly) {
-//     const auto graph = R"IR(
-//       graph(%0 : Tensor, %1 : Tensor):
-//         %2 : int = prim::Constant[value=2]()
-//         %3 : Tensor = aten::add(%0, %1, %2)
-//         return (%3))IR";
-//     pointwise_test_helper(graph, false);
-// }
 
 TEST(Converters, ATenSubConvertsCorrectly) {
   const auto graph = R"IR(
@@ -53,24 +52,18 @@ TEST(Converters, ATenSubConvertsCorrectly) {
         %2 : int = prim::Constant[value=1]()
         %3 : Tensor = aten::sub(%0, %1, %2)
         return (%3))IR";
-  pointwise_test_helper(graph, false);
+    pointwise_test_helper(graph, false);
+    pointwise_test_helper(graph, false, {3, 4}, {4});
 }
 
-// TEST(Converters, ATenSubWithScaleConvertsCorrectly) {
-//     const auto graph = R"IR(
-//       graph(%0 : Tensor, %1 : Tensor):
-//         %2 : float = prim::Constant[value=0.5]()
-//         %3 : Tensor = aten::add(%0, %1, %2)
-//         return (%3))IR";
-//     pointwise_test_helper(graph, false);
-// }
 
 TEST(Converters, ATenMulConvertsCorrectly) {
   const auto graph = R"IR(
       graph(%0 : Tensor, %1 : Tensor):
         %2 : Tensor = aten::mul(%0, %1)
         return (%2))IR";
-  pointwise_test_helper(graph, false);
+    pointwise_test_helper(graph, false);
+    pointwise_test_helper(graph, false, {3, 4}, {4});
 }
 
 TEST(Converters, ATenDivConvertsCorrectly) {
@@ -78,7 +71,8 @@ TEST(Converters, ATenDivConvertsCorrectly) {
       graph(%0 : Tensor, %1 : Tensor):
         %2 : Tensor = aten::div(%0, %1)
         return (%2))IR";
-  pointwise_test_helper(graph, false);
+    pointwise_test_helper(graph, false);
+    pointwise_test_helper(graph, false, {3, 4}, {4});
 }
 
 TEST(Converters, ATenPowTensorConvertsCorrectly) {
@@ -86,7 +80,8 @@ TEST(Converters, ATenPowTensorConvertsCorrectly) {
        graph(%x.1 : Tensor, %x2.1 : Tensor):
           %3 : Tensor = aten::pow(%x.1, %x2.1)
           return (%3))IR";
-  pointwise_test_helper(graph, false);
+    pointwise_test_helper(graph, false);
+    pointwise_test_helper(graph, false, {3, 4}, {4});
 }
 
 TEST(Converters, ATenPowScalarConvertsCorrectly) {
