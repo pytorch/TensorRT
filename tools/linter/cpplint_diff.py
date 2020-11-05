e688a0344ae538db8e4c37bc922ce4f3b0fbe3e4
Naren Dasan <naren@narendasan.com>
2020-11-04 16:41:34 -0800

    tools(//tools/linter): Fix diff command for C++ to actually throw errors
    Signed-off-by: Naren Dasan <naren@narendasan.com>
    Signed-off-by: Naren Dasan <narens@nvidia.com>

--- tools/linter/cpplint_diff.py
+++ tools/linter/cpplint_diff.py
@@ -12,7 +12,7 @@ def lint(target_files):
     for f in target_files:
         with open("/tmp/changes.txt", "w") as changes:
             subprocess.run(['clang-format', f], stdout=changes)
-        output = subprocess.run(["git", "diff", "-u", "--color", f, "/tmp/changes.txt"])
+        output = subprocess.run(["git", "diff", "-u", "--color", "--exit-code", f, "/tmp/changes.txt"])
         if output.returncode != 0:
             failure = True
     return failure
@@ -32,7 +32,8 @@ if __name__ == "__main__":
         path = BAZEL_ROOT + '/' + p[2:]
         files = utils.glob_files(path, VALID_CPP_FILE_TYPES)
         if files != []:
-            failure = lint(files)
+            if lint(files):
+                failure = True
     if failure:
         print("\033[91mERROR:\033[0m Some files do not conform to style guidelines")
         sys.exit(1)
