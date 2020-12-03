6bbcecb322f70269ff59a10da6739c44f0f73c4d
inocsin <vcheungyi@163.com>
2020-12-03 08:10:48 +0800

    support leaky_relu converter/test_case
    Signed-off-by: inocsin <vcheungyi@163.com>

--- tests/core/conversion/converters/test_activation.cpp
+++ tests/core/conversion/converters/test_activation.cpp
@@ -156,3 +156,24 @@ TEST(Converters, ATenPReLUMultiChannelConvertsCorrectly) {
 
   ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt_results[0], 2e-6));
 }
+
+TEST(Converters, ATenLeakyReluConvertsCorrectly) {
+  const auto graph = R"IR(
+      graph(%0 : Tensor):
+        %1 : float = prim::Constant[value=0.15]()
+        %2 : Tensor = aten::leaky_relu(%0, %1)
+        return (%2))IR";
+
+  auto g = std::make_shared<torch::jit::Graph>();
+  torch::jit::parseIR(graph, &*g);
+
+  auto in = at::randint(-5, 5, {5}, {at::kCUDA});
+  auto params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+  auto jit_results = trtorch::tests::util::RunGraph(g, params, {in});
+
+  in = at::clone(in);
+  params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+  auto trt_results = trtorch::tests::util::RunGraphEngine(g, params, {in});
+
+  ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt_results[0], 2e-6));
+}
