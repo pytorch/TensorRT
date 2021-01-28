68a50b2671f54c8d309ed4890dc89fd6485e25ae
Naren Dasan <naren@narendasan.com>
2021-01-28 15:34:18 -0800

    tests(remove_to): Adding a basic remove to test
    Signed-off-by: Naren Dasan <naren@narendasan.com>
    Signed-off-by: Naren Dasan <narens@nvidia.com>

---
+++ tests/core/lowering/test_remove_to.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include "core/compiler.h"
+#include "core/lowering/passes/passes.h"
+#include "gtest/gtest.h"
+#include "tests/util/util.h"
+#include "torch/csrc/jit/ir/irparser.h"
+#include "torch/csrc/jit/ir/subgraph_matcher.h"
+
+TEST(LoweringPasses, RemoveContiguousLowersCorrectly) {
+  std::string source_graph = R"IR(
+    graph(%x.1):
+      %6 : None = prim::Constant()
+      %4 : bool = prim::Constant[value=0]()
+      %3 : int = prim::Constant[value=5]() # experiments/test.py:8:17
+      %y.1 : Tensor = aten::to(%x.1, %3, %4, %4, %6)
+      %11 : Tensor = aten::relu(%y.1)
+      return (%3))IR";
+  std::string target_graph = R"IR(
+    graph(%x.1):
+      %11 : Tensor = aten::relu(%x.1)
+      return (%11))IR";
+
+  auto sg = std::make_shared<torch::jit::Graph>();
+  torch::jit::parseIR(source_graph, &*sg);
+  trtorch::core::lowering::passes::RemoveContiguous(sg);
+
+  auto tg = std::make_shared<torch::jit::Graph>();
+  torch::jit::parseIR(target_graph, &*tg);
+
+  ASSERT_TRUE(!torch::jit::findPatternMatches(*tg, *sg).empty());
+}
