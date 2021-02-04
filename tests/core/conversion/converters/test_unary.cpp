149df1dabb629fa58dce59e514941be4d365b41c
Naren Dasan <naren@narendasan.com>
2021-02-04 13:12:13 -0800

    refactor(tests): Apply linting for tests
    Signed-off-by: Naren Dasan <naren@narendasan.com>
    Signed-off-by: Naren Dasan <narens@nvidia.com>

--- tests/core/conversion/converters/test_unary.cpp
+++ tests/core/conversion/converters/test_unary.cpp
@@ -22,8 +22,9 @@ std::string gen_test_graph(const std::string& unary) {
     torch::jit::parseIR(graph, &*g);                                                      \
                                                                                           \
     float offset = 0;                                                                     \
-    if (strcmp(#name, "Acosh") == 0) offset += 1;   /*input larger than 1 for acosh*/     \
-    auto in = at::empty({10}, {at::kCUDA}).uniform_(0+offset, 0.5+offset);                \
+    if (strcmp(#name, "Acosh") == 0)                                                      \
+      offset += 1; /*input larger than 1 for acosh*/                                      \
+    auto in = at::empty({10}, {at::kCUDA}).uniform_(0 + offset, 0.5 + offset);            \
     auto params = trtorch::core::conversion::get_named_params(g->inputs(), {});           \
     auto jit_results = trtorch::tests::util::RunGraph(g, params, {in});                   \
                                                                                           \
