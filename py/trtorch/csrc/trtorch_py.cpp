93765271810142e09afe4cef61ef75cedb617d1e
Dheeraj Peri <peri.dheeraj@gmail.com>
2021-02-21 15:11:54 -0800

    Add int8 calibration API in python
    Signed-off-by: Dheeraj Peri <peri.dheeraj@gmail.com>

--- py/trtorch/csrc/trtorch_py.cpp
+++ py/trtorch/csrc/trtorch_py.cpp
@@ -4,16 +4,127 @@
 #include "Python.h"
 #include "core/compiler.h"
 #include "core/conversion/conversion.h"
+#include "NvInfer.h"
 #include "tensorrt_classes.h"
 #include "torch/csrc/jit/python/pybind_utils.h"
 #include "torch/custom_class.h"
 #include "torch/script.h"
 #include "torch/torch.h"
-
+#include "utils.h"
+using namespace nvinfer1;
+using namespace pybind11::literals;
 namespace py = pybind11;
 
 namespace trtorch {
 namespace pyapi {
+
+  // Use CRTP to share code among several different classes.
+  template <typename Derived>
+  class pyCalibratorTrampoline : public Derived
+  {
+  public:
+      using Derived::Derived; // Inherit constructors
+
+      int getBatchSize() const noexcept override
+      {
+          PYBIND11_OVERLOAD_PURE_NAME(int, Derived, "get_batch_size", getBatchSize);
+      }
+
+      bool getBatch(void* bindings[], const char* names[], int nbBindings) noexcept override
+      {
+          py::gil_scoped_acquire gil{};
+
+          py::function pyGetBatch = tensorrt::utils::getOverload(static_cast<Derived*>(this), "get_batch");
+          std::vector<const char*> namesVec(names, names + nbBindings);
+          py::object result = pyGetBatch(namesVec);
+          // Copy over into the other data structure.
+          if (!result.is_none() && result.cast<std::vector<size_t>>().size() != 0)
+          {
+              std::memcpy(bindings, result.cast<std::vector<size_t>>().data(), nbBindings * sizeof(void*));
+              return true;
+          }
+          return false;
+      }
+
+      const void* readCalibrationCache(std::size_t& length) noexcept override
+      {
+          py::gil_scoped_acquire gil{};
+
+          py::function pyReadCalibrationCache = tensorrt::utils::getOverload(static_cast<Derived*>(this), "read_calibration_cache");
+          py::buffer cache = pyReadCalibrationCache();
+          if (!cache.is_none())
+          {
+              py::buffer_info info = cache.request();
+              length = info.size * info.itemsize;
+              return info.ptr;
+          }
+          return nullptr;
+      }
+
+      void writeCalibrationCache(const void* ptr, std::size_t length) noexcept override
+      {
+          py::gil_scoped_acquire gil{};
+
+          py::function pyWriteCalibrationCache = tensorrt::utils::getOverload(static_cast<Derived*>(this), "write_calibration_cache");
+
+  # if PYBIND11_VERSION_MAJOR < 2 || PYBIND11_VERSION_MAJOR == 2 && PYBIND11_VERSION_MINOR < 6
+          py::buffer_info info{
+              const_cast<void*>(ptr),                 /* Pointer to buffer */
+              sizeof(uint8_t),                           /* Size of one scalar */
+              py::format_descriptor<uint8_t>::format(), /* Python struct-style format descriptor */
+              1,                                      /* Number of dimensions */
+              { length },                             /* Buffer dimensions */
+              { sizeof(uint8_t) }                        /* Strides (in bytes) for each index */
+          };
+          py::memoryview cache{info};
+  #else
+          py::memoryview cache{py::memoryview::from_buffer(static_cast<const uint8_t*>(ptr), {length}, {sizeof(uint8_t)} )};
+  #endif
+          pyWriteCalibrationCache(cache);
+      }
+  };
+
+
+  class pyIInt8Calibrator : public pyCalibratorTrampoline<IInt8Calibrator>
+  {
+  public:
+      using Derived = pyCalibratorTrampoline<IInt8Calibrator>;
+      using Derived::Derived;
+
+      CalibrationAlgoType getAlgorithm() noexcept override
+      {
+          PYBIND11_OVERLOAD_PURE_NAME(CalibrationAlgoType, IInt8Calibrator, "get_algorithm", getAlgorithm);
+      }
+  };
+
+  class pyIInt8LegacyCalibrator : public pyCalibratorTrampoline<IInt8LegacyCalibrator>
+  {
+  public:
+      using Derived = pyCalibratorTrampoline<IInt8LegacyCalibrator>;
+      using Derived::Derived;
+
+      double getQuantile() const noexcept override
+      {
+          PYBIND11_OVERLOAD_PURE_NAME(double, IInt8LegacyCalibrator, "get_quantile", getQuantile);
+      }
+
+      double getRegressionCutoff() const noexcept override
+      {
+          PYBIND11_OVERLOAD_PURE_NAME(double, IInt8LegacyCalibrator, "get_regression_cutoff", getRegressionCutoff);
+      }
+
+      const void* readHistogramCache(std::size_t& length) noexcept override
+      {
+          PYBIND11_OVERLOAD_PURE_NAME(
+              const void*, IInt8LegacyCalibrator, "read_histogram_cache", readHistogramCache, length);
+      }
+
+      void writeHistogramCache(const void* ptr, std::size_t length) noexcept override
+      {
+          PYBIND11_OVERLOAD_PURE_NAME(
+              void, IInt8LegacyCalibrator, "write_histogram_cache", writeHistogramCache, ptr, length);
+      }
+  };
 
 torch::jit::Module CompileGraph(const torch::jit::Module& mod, CompileSpec& info) {
   py::gil_scoped_acquire gil;
@@ -98,6 +209,43 @@ PYBIND11_MODULE(_C, m) {
       .value("safe_dla", EngineCapability::kSAFE_DLA, "Use safety DLA kernels only")
       .value("default", EngineCapability::kDEFAULT, "Use default behavior");
 
+  py::enum_<CalibrationAlgoType>(m, "CalibrationAlgoType", "Type of calibration algorithm")
+      .value("LEGACY_CALIBRATION", CalibrationAlgoType::kLEGACY_CALIBRATION)
+      .value("ENTROPY_CALIBRATION", CalibrationAlgoType::kENTROPY_CALIBRATION)
+      .value("ENTROPY_CALIBRATION_2", CalibrationAlgoType::kENTROPY_CALIBRATION_2)
+      .value("MINMAX_CALIBRATION", CalibrationAlgoType::kMINMAX_CALIBRATION);
+
+  py::class_<IInt8Calibrator, pyIInt8Calibrator>(m, "IInt8Calibrator", "Int8 Calibrator base class")
+      .def(py::init_alias<>()) // Always initialize trampoline class.
+      .def("get_batch_size", &IInt8Calibrator::getBatchSize, "Get batch size")
+      .def("get_algorithm", &IInt8Calibrator::getAlgorithm, "Get algorithm");
+      // For documentation purposes only
+      // .def("get_batch", docGetBatch<IInt8Calibrator>, "names"_a, "Get Batch")
+      // .def("read_calibration_cache", docReadCalibrationCache<IInt8Calibrator>,
+      //     "Read calibration cache")
+      // .def("write_calibration_cache", docWriteCalibrationCache<IInt8Calibrator>, "cache"_a,
+      //     "Write calibration cache");
+  py::class_<IInt8LegacyCalibrator, IInt8Calibrator, pyIInt8LegacyCalibrator>(m, "IInt8LegacyCalibrator", "Int8 Legacy Calibrator class")
+      .def(py::init_alias<>()) // Always initialize trampoline class.
+      .def("get_batch_size", &IInt8LegacyCalibrator::getBatchSize, "Get batch size")
+      .def("get_algorithm", &IInt8LegacyCalibrator::getAlgorithm, "Get algorithm");
+  //
+  py::class_<IInt8EntropyCalibrator, IInt8Calibrator, pyCalibratorTrampoline<IInt8EntropyCalibrator>>(m, "IInt8EntropyCalibrator", "Int8 Entropy Calibrator class")
+      .def(py::init_alias<>()) // Always initialize trampoline class.
+      .def("get_batch_size", &IInt8EntropyCalibrator::getBatchSize, "Get batch size")
+      .def("get_algorithm", &IInt8EntropyCalibrator::getAlgorithm, "Get algorithm");
+  //
+  py::class_<IInt8EntropyCalibrator2, IInt8Calibrator, pyCalibratorTrampoline<IInt8EntropyCalibrator2>>(m, "IInt8EntropyCalibrator2", "Int8 Entropy Calibrator2 class")
+      .def(py::init_alias<>()) // Always initialize trampoline class.
+      .def("get_batch_size", &IInt8EntropyCalibrator2::getBatchSize, "Get batch size")
+      .def("get_algorithm", &IInt8EntropyCalibrator2::getAlgorithm, "Get algorithm");
+  // ;
+  //
+  py::class_<IInt8MinMaxCalibrator, IInt8Calibrator, pyCalibratorTrampoline<IInt8MinMaxCalibrator>>(m, "IInt8MinMaxCalibrator", "Int8 MinMax Calibrator class")
+      .def(py::init_alias<>()) // Always initialize trampoline class.
+      .def("get_batch_size", &IInt8MinMaxCalibrator::getBatchSize, "Get batch size")
+      .def("get_algorithm", &IInt8MinMaxCalibrator::getAlgorithm, "Get algorithm");
+  // ;
   py::class_<CompileSpec>(m, "CompileSpec")
       .def(py::init<>())
       .def_readwrite("input_ranges", &CompileSpec::input_ranges)
