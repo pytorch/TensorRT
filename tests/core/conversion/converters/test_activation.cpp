29c95536ba1400985574a27fd02f2d22f4ef5e39
Bo Wang <wangbo1995ee@163.com>
2021-02-22 21:11:14 -0600

    add ELU converter to core library
    Signed-off-by: Bo Wang <wangbo1995ee@163.com>
    Signed-off-by: developer0hye <developer.0hye@gmail.com>

--- tests/core/conversion/converters/test_activation.cpp
+++ tests/core/conversion/converters/test_activation.cpp
@@ -177,3 +177,26 @@ TEST(Converters, ATenLeakyReluConvertsCorrectly) {
 
   ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt_results[0], 2e-6));
 }
+
+TEST(Converters, ATenEluConvertsCorrectly) {
+  const auto graph = R"IR(
+       graph(%x.1 : Tensor):
+        %2 : float = prim::Constant[value=1.]()
+        %3 : int = prim::Constant[value=1]()
+        %result.2 : Tensor = aten::elu(%x.1, %2, %3, %3)
+        return (%result.2))IR";
+
+  auto g = std::make_shared<torch::jit::Graph>();
+  torch::jit::parseIR(graph, &*g);
+
+  auto in = at::randint(-5, 5, {1, 10, 1, 1}, {at::kCUDA});
+
+  auto params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+  auto jit_results = trtorch::tests::util::RunGraph(g, params, {in});
+
+  in = at::clone(in);
+  params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+  auto trt_results = trtorch::tests::util::RunGraphEngine(g, params, {in});
+
+  ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt_results[0], 2e-6));
+}
