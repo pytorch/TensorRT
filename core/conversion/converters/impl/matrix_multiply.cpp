5d608186fd89c1187c183c6100aaaac30ffda325
inocsin <vcheungyi@163.com>
2021-03-08 16:06:22 +0800

    feat: remove output reshape in aten::matmul
    Signed-off-by: inocsin <vcheungyi@163.com>

--- core/conversion/converters/impl/matrix_multiply.cpp
+++ core/conversion/converters/impl/matrix_multiply.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include "core/conversion/converters/converters.h"
 #include "core/util/prelude.h"
 
@@ -18,29 +19,31 @@ auto mm_registrations TRTORCH_UNUSED = RegisterNodeConversionPatterns().pattern(
        auto other = args[1].ITensorOrFreeze(ctx);
        auto other_shape = util::toVec(other->getDimensions());
        LOG_DEBUG("other tensor shape: " << other->getDimensions());
-
+       int max_dim = std::max(in_shape.size(), other_shape.size());
+       int min_dim = std::min(in_shape.size(), other_shape.size());
        // add support when self dims != other dims
-       if (in_shape.size() != other_shape.size() && in_shape.size() > 2) {
-         auto shuffle = ctx->net->addShuffle(*self);
+       if (in_shape.size() != other_shape.size()) {
+         auto org_tensor = in_shape.size() > other_shape.size() ? other : self;
+         auto shuffle = ctx->net->addShuffle(*org_tensor);
+
+         int diff_dim = max_dim - min_dim;
+         auto old_shape = in_shape.size() > other_shape.size() ? other_shape : in_shape;
          std::vector<int64_t> new_shape;
-         int val = 1;
-         for (int i = 0; i < (int)in_shape.size() - 1; i++) {
-           val = val * in_shape[i];
+         for (int i = 0; i < diff_dim; i++) {
+           new_shape.push_back(1);
          }
-         new_shape.push_back(val);
-         new_shape.push_back(in_shape[in_shape.size() - 1]);
+         for (int i = 0; i < min_dim; i++) {
+           new_shape.push_back(old_shape[i]);
+         }
          shuffle->setReshapeDimensions(util::toDims(new_shape));
+         auto reshaped_tensor = shuffle->getOutput(0);
+         auto new_self = in_shape.size() > other_shape.size() ? self : reshaped_tensor;
+         auto new_other = other_shape.size() > in_shape.size() ? other : reshaped_tensor;
          auto mm_layer = ctx->net->addMatrixMultiply(
-             *shuffle->getOutput(0), nvinfer1::MatrixOperation::kNONE, *other, nvinfer1::MatrixOperation::kNONE);
+             *new_self, nvinfer1::MatrixOperation::kNONE, *new_other, nvinfer1::MatrixOperation::kNONE);
          mm_layer->setName(util::node_info(n).c_str());
-         std::vector<int64_t> out_shape;
-         for (int i = 0; i < (int)in_shape.size() - 1; i++) {
-           out_shape.push_back(in_shape[i]);
-         }
-         out_shape.push_back(other_shape[other_shape.size() - 1]);
-         auto out_shuffle = ctx->net->addShuffle(*mm_layer->getOutput(0));
-         out_shuffle->setReshapeDimensions(util::toDims(out_shape));
-         auto out_tensor = ctx->AssociateValueAndTensor(n->outputs()[0], out_shuffle->getOutput(0));
+
+         auto out_tensor = ctx->AssociateValueAndTensor(n->outputs()[0], mm_layer->getOutput(0));
          LOG_DEBUG("Output tensor shape: " << out_tensor->getDimensions());
        } else {
          auto mm_layer = ctx->net->addMatrixMultiply(
