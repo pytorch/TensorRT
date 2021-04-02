0a0e922a96c2701ea17eb090e8296c18a4abcf9f
Bo Wang <wangbo1995ee@163.com>
2021-04-02 11:42:23 -0500

    chore: fix format bugs
    Signed-off-by: Bo Wang <wangbo1995ee@163.com>

--- py/trtorch/csrc/trtorch_py.cpp
+++ py/trtorch/csrc/trtorch_py.cpp
@@ -247,7 +247,7 @@ PYBIND11_MODULE(_C, m) {
       .def_readwrite("num_avg_timing_iters", &CompileSpec::num_avg_timing_iters)
       .def_readwrite("workspace_size", &CompileSpec::workspace_size)
       .def_readwrite("max_batch_size", &CompileSpec::max_batch_size)
-      .def_readwrite("torch_fallback", &CompileSpec::torch_fallback);
+      .def_readwrite("torch_fallback", &CompileSpec::torch_fallback)
       .def_readwrite("truncate_long_and_double", &CompileSpec::truncate_long_and_double);
 
   py::class_<Device>(m, "Device")
