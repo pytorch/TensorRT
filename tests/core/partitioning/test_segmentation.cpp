6d826d3de44029d6ae4fce287203166c2acbf094
Bo Wang <wangbo1995ee@163.com>
2021-04-07 17:54:47 -0500

    test: add tests for graph segmentation and shape analysis in partitioning
    Signed-off-by: Bo Wang <wangbo1995ee@163.com>

---
+++ tests/core/partitioning/test_segmentation.cpp
@@ -0,0 +1,156 @@
+#include <string>
+#include "gtest/gtest.h"
+#include "tests/util/util.h"
+#include "torch/script.h"
+#include "trtorch/trtorch.h"
+#include "core/lowering/lowering.h"
+#include "core/partitioning/partitioning.h"
+
+
+bool checkSegmentedBlockNumber(std::vector<trtorch::core::partitioning::SegmentedBlock>& segmented_blocks,
+                               trtorch::core::partitioning::SegmentedBlock::SegmentedBlockTarget target, int target_count) {
+  for (auto &seg_block : segmented_blocks) {
+    if (seg_block.target() == target) {
+      target_count--;
+    }
+  }
+  return target_count == 0;
+}
+
+bool checkSegmentedBlockNodesMapping(std::vector<trtorch::core::partitioning::SegmentedBlock>& segmented_blocks,
+                                     std::shared_ptr<torch::jit::Graph> g, std::vector<std::vector<int>> nodes_index) {
+  std::vector<torch::jit::Node*> graph_nodes;
+  for (const auto n : g->nodes()) {
+    if (n->kind() != torch::jit::prim::Constant) {
+      graph_nodes.push_back(n);
+    }
+  }
+  for (size_t i = 0; i < nodes_index.size(); ++i) {
+    size_t seg_block_node_id = 0;
+    for (int j : nodes_index[i]) {
+      if (segmented_blocks[i].raw_nodes()[seg_block_node_id++] != graph_nodes[j]) {
+        return false;
+      }
+    }
+    if (seg_block_node_id != segmented_blocks[i].raw_nodes().size()) return false;
+  }
+  return true;
+}
+
+TEST(Partitioning, SegmentingGraphDefaultCorrectly) {
+  torch::jit::script::Module mod;
+  try {
+  mod = torch::jit::load("tests/core/partitioning/test_base_model.jit");
+  } catch (const c10::Error& e) {
+  std::cerr << "error loading the model\n";
+  return;
+  }
+  auto graph_and_parameters = trtorch::core::lowering::Lower(mod, "forward");
+  auto g = graph_and_parameters.first;
+
+  trtorch::core::conversion::TorchFallback fallback_info;
+  fallback_info.enabled = true;
+  std::vector<trtorch::core::partitioning::SegmentedBlock> segmented_blocks = trtorch::core::partitioning::segment_graph(g, fallback_info);
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTensorRT, 2));
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTorch, 1));
+  ASSERT_TRUE(checkSegmentedBlockNodesMapping(segmented_blocks, g, {{0, 1, 2}, {3}, {4}}));
+}
+
+TEST(Partitioning, SegmentingGraphWithMinBlockSizeCorrectly) {
+  torch::jit::script::Module mod;
+  try {
+  mod = torch::jit::load("tests/core/partitioning/test_base_model.jit");
+  } catch (const c10::Error& e) {
+  std::cerr << "error loading the model\n";
+  return;
+  }
+  auto graph_and_parameters = trtorch::core::lowering::Lower(mod, "forward");
+  auto g = graph_and_parameters.first;
+
+  trtorch::core::conversion::TorchFallback fallback_info;
+  fallback_info.enabled = true;
+  fallback_info.min_block_size = 3;
+  std::vector<trtorch::core::partitioning::SegmentedBlock> segmented_blocks = trtorch::core::partitioning::segment_graph(g, fallback_info);
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTensorRT, 1));
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTorch, 1));
+  ASSERT_TRUE(checkSegmentedBlockNodesMapping(segmented_blocks, g, {{0, 1, 2}, {3, 4}}));
+}
+
+TEST(Partitioning, SegmentingGraphWithForcedOPeCorrectly) {
+  torch::jit::script::Module mod;
+  try {
+  mod = torch::jit::load("tests/core/partitioning/test_base_model.jit");
+  } catch (const c10::Error& e) {
+  std::cerr << "error loading the model\n";
+  return;
+  }
+  auto graph_and_parameters = trtorch::core::lowering::Lower(mod, "forward");
+  auto g = graph_and_parameters.first;
+
+  trtorch::core::conversion::TorchFallback fallback_info;
+  fallback_info.enabled = true;
+  fallback_info.forced_fallback_operators.push_back("aten::relu");
+  std::vector<trtorch::core::partitioning::SegmentedBlock> segmented_blocks = trtorch::core::partitioning::segment_graph(g, fallback_info);
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTensorRT, 3));
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTorch, 2));
+  ASSERT_TRUE(checkSegmentedBlockNodesMapping(segmented_blocks, g, {{0}, {1}, {2}, {3}, {4}}));
+}
+
+TEST(Partitioning, SegmentingGraphDefaultCorrectlyEdge) {
+  torch::jit::script::Module mod;
+  try {
+  mod = torch::jit::load("tests/core/partitioning/test_edge_model.jit");
+  } catch (const c10::Error& e) {
+  std::cerr << "error loading the model\n";
+  return;
+  }
+  auto graph_and_parameters = trtorch::core::lowering::Lower(mod, "forward");
+  auto g = graph_and_parameters.first;
+
+  trtorch::core::conversion::TorchFallback fallback_info;
+  fallback_info.enabled = true;
+  std::vector<trtorch::core::partitioning::SegmentedBlock> segmented_blocks = trtorch::core::partitioning::segment_graph(g, fallback_info);
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTensorRT, 2));
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTorch, 1));
+  ASSERT_TRUE(checkSegmentedBlockNodesMapping(segmented_blocks, g, {{0, 1}, {2}, {3, 4, 5, 6}}));
+}
+
+TEST(Partitioning, SegmentingGraphWithMinBlockSizeCorrectlyEdge) {
+  torch::jit::script::Module mod;
+  try {
+  mod = torch::jit::load("tests/core/partitioning/test_edge_model.jit");
+  } catch (const c10::Error& e) {
+  std::cerr << "error loading the model\n";
+  return;
+  }
+  auto graph_and_parameters = trtorch::core::lowering::Lower(mod, "forward");
+  auto g = graph_and_parameters.first;
+
+  trtorch::core::conversion::TorchFallback fallback_info;
+  fallback_info.enabled = true;
+  fallback_info.min_block_size = 3;
+  std::vector<trtorch::core::partitioning::SegmentedBlock> segmented_blocks = trtorch::core::partitioning::segment_graph(g, fallback_info);
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTensorRT, 1));
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTorch, 1));
+  ASSERT_TRUE(checkSegmentedBlockNodesMapping(segmented_blocks, g, {{0, 1, 2}, {3, 4, 5, 6}}));
+}
+
+TEST(Partitioning, SegmentingGraphWithForcedOPeCorrectlyEdge) {
+  torch::jit::script::Module mod;
+  try {
+  mod = torch::jit::load("tests/core/partitioning/test_edge_model.jit");
+  } catch (const c10::Error& e) {
+  std::cerr << "error loading the model\n";
+  return;
+  }
+  auto graph_and_parameters = trtorch::core::lowering::Lower(mod, "forward");
+  auto g = graph_and_parameters.first;
+
+  trtorch::core::conversion::TorchFallback fallback_info;
+  fallback_info.enabled = true;
+  fallback_info.forced_fallback_operators.push_back("aten::relu");
+  std::vector<trtorch::core::partitioning::SegmentedBlock> segmented_blocks = trtorch::core::partitioning::segment_graph(g, fallback_info);
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTensorRT, 3));
+  ASSERT_TRUE(checkSegmentedBlockNumber(segmented_blocks, trtorch::core::partitioning::SegmentedBlock::kTorch, 2));
+  ASSERT_TRUE(checkSegmentedBlockNodesMapping(segmented_blocks, g, {{0, 1}, {2}, {3}, {4}, {5, 6}}));
+}
---
+++ tests/core/partitioning/test_shape_analysis.cpp
@@ -0,0 +1,60 @@
+#include <string>
+#include "gtest/gtest.h"
+#include "torch/script.h"
+#include "core/lowering/lowering.h"
+#include "core/partitioning/partitioning.h"
+#include "core/util/trt_util.h"
+
+bool checkSegmentedBlockInputShape(std::vector<trtorch::core::partitioning::SegmentedBlock>& segmented_blocks, std::vector<std::vector<std::vector<int>>> in_shape) {
+  if (segmented_blocks.size() != in_shape.size()) return false;
+  for (size_t i = 0; i < segmented_blocks.size(); ++i) {
+    auto cur_block_in_shapes = segmented_blocks[i].in_shape();
+    if (cur_block_in_shapes.size() != in_shape[i].size()) return false;
+    for (size_t j = 0; j < cur_block_in_shapes.size(); ++j) {
+      auto cur_input_shape = trtorch::core::util::toVec(cur_block_in_shapes[j]);
+      for (size_t k = 0; k < cur_input_shape.size(); ++k) {
+        if (cur_input_shape[k] != in_shape[i][j][k])
+          return false;
+      }
+    }
+  }
+  return true;
+}
+
+TEST(Partitioning, InferSegmentedBlockShapeCorrectly) {
+  torch::jit::script::Module mod;
+  try {
+    mod = torch::jit::load("tests/core/partitioning/test_base_model.jit");
+  } catch (const c10::Error& e) {
+    std::cerr << "error loading the model\n";
+    return;
+  }
+  auto graph_and_parameters = trtorch::core::lowering::Lower(mod, "forward");
+  auto g = graph_and_parameters.first;
+
+  trtorch::core::conversion::TorchFallback fallback_info;
+  fallback_info.enabled = true;
+  std::vector<trtorch::core::conversion::InputRange> input_ranges{trtorch::core::conversion::InputRange({3, 3, 16, 16})};
+
+  std::vector<trtorch::core::partitioning::SegmentedBlock> segmented_blocks = trtorch::core::partitioning::Partition(g, input_ranges, fallback_info);
+  ASSERT_TRUE(checkSegmentedBlockInputShape(segmented_blocks, {{{3, 3, 16, 16}}, {{3, 16, 16, 16}}, {{3, 16, 16, 16}}}));
+}
+
+TEST(Partitioning, InferSegmentedBlockShapeCorrectlyEdge) {
+  torch::jit::script::Module mod;
+  try {
+  mod = torch::jit::load("tests/core/partitioning/test_edge_model.jit");
+  } catch (const c10::Error& e) {
+  std::cerr << "error loading the model\n";
+  return;
+  }
+  auto graph_and_parameters = trtorch::core::lowering::Lower(mod, "forward");
+  auto g = graph_and_parameters.first;
+
+  trtorch::core::conversion::TorchFallback fallback_info;
+  fallback_info.enabled = true;
+  std::vector<trtorch::core::conversion::InputRange> input_ranges{trtorch::core::conversion::InputRange({3, 3, 16, 16})};
+
+  std::vector<trtorch::core::partitioning::SegmentedBlock> segmented_blocks = trtorch::core::partitioning::Partition(g, input_ranges, fallback_info);
+  ASSERT_TRUE(checkSegmentedBlockInputShape(segmented_blocks, {{{3, 3, 16, 16}}, {{3, 32, 16, 16}}, {{3, 32, 16, 16}, {3, 16, 16, 16}}}));
+}
