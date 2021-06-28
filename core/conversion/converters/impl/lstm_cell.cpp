d0977b3e90ea17930f28374073c98b94ada2f142
Arvind Sridhar <arvindsridhar@berkeley.edu>
2021-06-28 10:15:17 -0700

    Add GRU converter code
    Signed-off-by: Arvind Sridhar <arvindsridhar@berkeley.edu>

--- core/conversion/converters/impl/lstm_cell.cpp
+++ core/conversion/converters/impl/lstm_cell.cpp
@@ -48,6 +48,122 @@ nvinfer1::ITensor* add_bias(
 }
 
 auto lstm_cell_registrations TRTORCH_UNUSED = RegisterNodeConversionPatterns().pattern(
+    {"aten::gru_cell(Tensor input, Tensor[] hx, Tensor w_ih, Tensor w_hh, Tensor? b_ih=None, Tensor? b_hh=None) -> Tensor",
+     [](ConversionCtx* ctx, const torch::jit::Node* n, args& args) -> bool {
+       auto input = args[0].ITensorOrFreeze(ctx);
+       auto w_ih = args[2].ITensorOrFreeze(ctx);
+       auto w_hh = args[3].ITensorOrFreeze(ctx);
+
+       LOG_DEBUG("Input tensor shape: " << input->getDimensions());
+       LOG_DEBUG("w_ih tensor shape: " << w_ih->getDimensions());
+       LOG_DEBUG("w_hh tensor shape: " << w_hh->getDimensions());
+
+       std::vector<nvinfer1::ITensor*> state;
+       auto hx = args[1].IValue()->toListRef();
+       for (unsigned int i = 0; i < hx.size(); i++) {
+         auto t = hx[i];
+
+         nvinfer1::ITensor* itensor;
+
+         if (t.isTensor()) {
+           itensor = tensor_to_const(ctx, t.toTensor());
+         } else {
+           auto cont = t.toCustomClass<TensorContainer>();
+           itensor = cont->tensor();
+         }
+
+         LOG_DEBUG("State tensor " << i << " shape: " << itensor->getDimensions());
+         state.push_back(itensor);
+       }
+
+       // calculate first half of gates
+       auto mm1 = ctx->net->addMatrixMultiply(
+           *input, nvinfer1::MatrixOperation::kNONE, *w_ih, nvinfer1::MatrixOperation::kTRANSPOSE);
+       TRTORCH_CHECK(mm1, "Unable to create matrix multiplication node: " << *n);
+       auto mm1_out = mm1->getOutput(0);
+
+       auto out1 = (args[4].isIValue() && args[4].IValue()->isNone())
+           ? mm1_out
+           : add_bias(mm1_out, args[4].ITensorOrFreeze(ctx), "b_ih", ctx, n);
+
+       // calculate second half of gates
+       auto mm2 = ctx->net->addMatrixMultiply(
+           *state[0], nvinfer1::MatrixOperation::kNONE, *w_hh, nvinfer1::MatrixOperation::kTRANSPOSE);
+       TRTORCH_CHECK(mm2, "Unable to create matrix multiplication node: " << *n);
+       auto mm2_out = mm2->getOutput(0);
+
+       auto out2 = (args[5].isIValue() && args[5].IValue()->isNone())
+           ? mm2_out
+           : add_bias(mm2_out, args[5].ITensorOrFreeze(ctx), "b_hh", ctx, n);
+
+       // chunk the first and second half of gates into 3 parts
+       auto dims = util::toVec(out1->getDimensions());
+       auto batch = dims[0];
+       auto hidden = dims[1] / 3;
+
+       std::vector<int64_t> size_vec = {batch, hidden};
+       std::vector<int64_t> stride_vec = {1, 1};
+       std::vector<int64_t> offset0 = {0, 0};
+       std::vector<int64_t> offset1 = {0, hidden};
+       std::vector<int64_t> offset2 = {0, 2 * hidden};
+
+       auto size = util::toDims(size_vec);
+       auto stride = util::toDims(stride_vec);
+
+       auto out1_r = ctx->net->addSlice(*out1, util::toDims(offset0), size, stride);
+       TRTORCH_CHECK(out1_r, "Unable to create Slice layer from node: " << *n);
+       auto out1_z = ctx->net->addSlice(*out1, util::toDims(offset1), size, stride);
+       TRTORCH_CHECK(out1_z, "Unable to create Slice layer from node: " << *n);
+       auto out1_n = ctx->net->addSlice(*out1, util::toDims(offset2), size, stride);
+       TRTORCH_CHECK(out1_n, "Unable to create Slice layer from node: " << *n);
+
+       auto out2_r = ctx->net->addSlice(*out2, util::toDims(offset0), size, stride);
+       TRTORCH_CHECK(out2_r, "Unable to create Slice layer from node: " << *n);
+       auto out2_z = ctx->net->addSlice(*out2, util::toDims(offset1), size, stride);
+       TRTORCH_CHECK(out2_z, "Unable to create Slice layer from node: " << *n);
+       auto out2_n = ctx->net->addSlice(*out2, util::toDims(offset2), size, stride);
+       TRTORCH_CHECK(out2_n, "Unable to create Slice layer from node: " << *n);
+
+       // compute the R gate
+       auto add_r = ctx->net->addElementWise(*out1_r, *out2_r, nvinfer1::ElementWiseOperation::kSUM);
+       TRTORCH_CHECK(add_r, "Unable to create ElementWise layer from node: " << *n);
+       auto activ_r = ctx->net->addActivation(*add_r->getOutput(0), nvinfer1::ActivationType::kSIGMOID);
+       TRTORCH_CHECK(activ_r, "Unable to create sigmoid activation layer from node: " << *n);
+       auto r_gate = activ_r->getOutput(0);
+
+       // compute the Z gate
+       auto add_z = ctx->net->addElementWise(*out1_z, *out2_z, nvinfer1::ElementWiseOperation::kSUM);
+       TRTORCH_CHECK(add_z, "Unable to create ElementWise layer from node: " << *n);
+       auto activ_z = ctx->net->addActivation(*add_z->getOutput(0), nvinfer1::ActivationType::kSIGMOID);
+       TRTORCH_CHECK(activ_z, "Unable to create sigmoid activation layer from node: " << *n);
+       auto z_gate = activ_z->getOutput(0);
+
+       // compute the N gate
+       auto out2_n_transform = ctx->net->addElementWise(*r_gate, *out2_n, nvinfer1::ElementWiseOperation::kPROD);
+       TRTORCH_CHECK(out2_n_transform, "Unable to create ElementWise layer from node: " << *n);
+       auto add_n = ctx->net->addElementWise(*out1_n, *out2_n_transform->getOutput(0), nvinfer1::ElementWiseOperation::kSUM);
+       TRTORCH_CHECK(add_n, "Unable to create ElementWise layer from node: " << *n);
+       auto activ_n = ctx->net->addActivation(*add_n->getOutput(0), nvinfer1::ActivationType::kTANH);
+       TRTORCH_CHECK(activ_n, "Unable to create tanh activation layer from node: " << *n);
+       auto n_gate = activ_n->getOutput(0);
+
+       // compute new state H'
+       auto interm_1 = ctx->net->addElementWise(*z_gate, *n_gate, nvinfer1::ElementWiseOperation::kPROD);
+       TRTORCH_CHECK(interm_1, "Unable to create ElementWise layer from node: " << *n);
+       auto interm_2 = ctx->net->addElementWise(*z_gate, *state[0], nvinfer1::ElementWiseOperation::kPROD);
+       TRTORCH_CHECK(interm_2, "Unable to create ElementWise layer from node: " << *n);
+       auto interm_3 = ctx->net->addElementWise(*n_gate, *interm_1->getOutput(0), nvinfer1::ElementWiseOperation::kSUB);
+       TRTORCH_CHECK(interm_3, "Unable to create ElementWise layer from node: " << *n);
+       auto h_prime = ctx->net->addElementWise(*interm_2->getOutput(0), *interm_3->getOutput(0), nvinfer1::ElementWiseOperation::kSUM);
+       TRTORCH_CHECK(h_prime, "Unable to create ElementWise layer from node: " << *n);
+       auto h_prime_out = h_prime->getOutput(0);
+
+       ctx->AssociateValueAndTensor(n->outputs()[0], h_prime_out);
+
+       LOG_DEBUG("Output tensor [h'] shape: " << h_prime->getDimensions());
+
+       return true;
+    }},
     {"aten::lstm_cell(Tensor input, Tensor[] hx, Tensor w_ih, Tensor w_hh, Tensor? b_ih=None, Tensor? b_hh=None) -> (Tensor, Tensor)",
      [](ConversionCtx* ctx, const torch::jit::Node* n, args& args) -> bool {
        auto input = args[0].ITensorOrFreeze(ctx);
