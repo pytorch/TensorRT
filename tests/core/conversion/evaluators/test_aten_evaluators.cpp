2973833abeb4142d51f6e596dafacdc8ec081fc3
Yu-Te Cheng <chengyute@gmail.com>
2021-07-14 06:07:56 -0700

    Add aten::clone converter.
    Signed-off-by: Yu-Te Cheng <chengyute@gmail.com>

--- tests/core/conversion/evaluators/test_aten_evaluators.cpp
+++ tests/core/conversion/evaluators/test_aten_evaluators.cpp
@@ -356,3 +356,27 @@ TEST(Evaluators, ATenAppendWithITensorAndTensorEvaluatesCorrectly) {
 
   ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt_results[0].reshape_as(jit_results[0]), 2e-6));
 }
+
+TEST(Evaluators, ATenCloneEvaluatesCorrectly) {
+  const auto graph = R"IR(
+      graph(%0 : Tensor):
+        %1 : None = prim::Constant()
+        %2 : Tensor = aten::clone(%0, %1)
+        %3 : Tensor = aten::relu(%0)
+        %4 : int = prim::Constant[value=1]()
+        %5 : Tensor = aten::add(%3, %2, %4)
+        return (%5))IR";
+
+  auto g = std::make_shared<torch::jit::Graph>();
+  torch::jit::parseIR(graph, &*g);
+
+  auto in = at::randint(1, 3, {224, 224}, {at::kCUDA});
+
+  auto params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+  auto jit_results = trtorch::tests::util::RunGraph(g, params, {in});
+
+  params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+  auto trt_results = trtorch::tests::util::RunGraphEngine(g, params, {in});
+
+  ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt_results[0].reshape_as(jit_results[0]), 2e-6));
+}
