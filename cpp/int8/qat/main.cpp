87dc72e7545cb6ba78f23532e900c2d0a2527290
Dheeraj Peri <peri.dheeraj@gmail.com>
2021-08-02 13:19:01 -0700

    chore: Fix build paths
    Signed-off-by: Dheeraj Peri <peri.dheeraj@gmail.com>

--- cpp/int8/qat/main.cpp
+++ cpp/int8/qat/main.cpp
@@ -80,7 +80,7 @@ int main(int argc, const char* argv[]) {
                           .map(torch::data::transforms::Normalize<>({0.4914, 0.4822, 0.4465}, {0.2023, 0.1994, 0.2010}))
                           .map(torch::data::transforms::Stack<>());
   auto eval_dataloader = torch::data::make_data_loader(
-      std::move(eval_dataset), torch::data::DataLoaderOptions().batch_size(128).workers(2));
+      std::move(eval_dataset), torch::data::DataLoaderOptions().batch_size(32).workers(2));
 
   /// Check the FP32 accuracy in JIT
   float correct = 0.0, total = 0.0;
