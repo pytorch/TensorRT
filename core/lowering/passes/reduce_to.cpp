b57a6ddba8a55e53e88f101d399cbe1b21042107
Dheeraj Peri <peri.dheeraj@gmail.com>
2021-08-10 01:45:32 -0700

    feat: Add aten::type_as lowering pass
    Signed-off-by: Dheeraj Peri <peri.dheeraj@gmail.com>

--- core/lowering/passes/reduce_to.cpp
+++ core/lowering/passes/reduce_to.cpp
@@ -12,16 +12,34 @@ void ReduceToOperation(std::shared_ptr<torch::jit::Graph>& graph) {
         graph(%x, %device, %dtype, %nb, %copy, %format):
             %out : Tensor = aten::to(%x, %device, %dtype, %nb, %copy, %format)
             return (%out))IR";
-  std::string to_general_pattern = R"IR(
+  std::string to_dtype_pattern = R"IR(
         graph(%x, %device, %dtype, %nb, %copy, %format):
             %out : Tensor = aten::to(%x, %dtype, %nb, %copy, %format)
             return (%out))IR";
 
+  std::string to_type_as_pattern = R"IR(
+        graph(%input, %other):
+            %out : Tensor = aten::type_as(%input, %other)
+            return (%out))IR";
+
+  std::string to_other_pattern = R"IR(
+        graph(%input, %other):
+            %5 : bool = prim::Constant[value=0]()
+            %6 : None = prim::Constant()
+            %out : Tensor = aten::to(%input, %other, %5, %5, %6)
+            return (%out))IR";
+
   // replace aten::to.device with aten::to.dtype
   torch::jit::SubgraphRewriter map_aten_device_to_dtype;
-  map_aten_device_to_dtype.RegisterRewritePattern(to_device_pattern, to_general_pattern);
+  map_aten_device_to_dtype.RegisterRewritePattern(to_device_pattern, to_dtype_pattern);
   map_aten_device_to_dtype.runOnGraph(graph);
-  LOG_GRAPH("Post lowering of aten::to.device -> " << *graph);
+
+  // replace aten::type_as with aten::to.other
+  torch::jit::SubgraphRewriter map_aten_type_as_to_other;
+  map_aten_type_as_to_other.RegisterRewritePattern(to_type_as_pattern, to_other_pattern);
+  map_aten_type_as_to_other.runOnGraph(graph);
+
+  LOG_GRAPH("Post lowering of [aten::to.device|aten::type_as] -> " << *graph);
 }
 
 } // namespace passes
--- tests/core/lowering/test_reduce_to_pass.cpp
+++ tests/core/lowering/test_reduce_to_pass.cpp
@@ -26,3 +26,26 @@ TEST(LoweringPasses, ReduceToCorrectly) {
 
   ASSERT_TRUE(!torch::jit::findPatternMatches(*tg, *sg).empty());
 }
+
+TEST(LoweringPasses, ReduceAtenTypeAsCorrectly) {
+  std::string source_graph = R"IR(
+    graph(%input, %other):
+        %out : Tensor = aten::type_as(%input, %other)
+        return (%out))IR";
+  std::string target_graph = R"IR(
+    graph(%input, %other):
+        %5 : bool = prim::Constant[value=0]()
+        %6 : None = prim::Constant()
+        %out : Tensor = aten::to(%input, %other, %5, %5, %6)
+        return (%out))IR";
+
+  trtorch::core::util::logging::get_logger().set_reportable_log_level(trtorch::core::util::logging::LogLevel::kGRAPH);
+  auto sg = std::make_shared<torch::jit::Graph>();
+  torch::jit::parseIR(source_graph, &*sg);
+  trtorch::core::lowering::passes::ReduceToOperation(sg);
+
+  auto tg = std::make_shared<torch::jit::Graph>();
+  torch::jit::parseIR(target_graph, &*tg);
+
+  ASSERT_TRUE(!torch::jit::findPatternMatches(*tg, *sg).empty());
+}
