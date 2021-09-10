0d6219ede4de9df535a1073b4669f769067dd05d
Yu-Te Cheng <chengyute@gmail.com>
2021-09-09 17:00:06 -0700

    feat: Add reflection_pad1d support.
    Signed-off-by: Yu-Te Cheng <chengyute@gmail.com>

--- core/conversion/converters/impl/reflection_pad.cpp
+++ core/conversion/converters/impl/reflection_pad.cpp
@@ -12,7 +12,8 @@ namespace converters {
 namespace impl {
 namespace {
 
-auto reflection_pad2d TRTORCH_UNUSED = RegisterNodeConversionPatterns().pattern(
+auto reflection_padXd TRTORCH_UNUSED = RegisterNodeConversionPatterns()
+  .pattern(
     {"aten::reflection_pad2d(Tensor self, int[4] padding) -> (Tensor)",
      [](ConversionCtx* ctx, const torch::jit::Node* n, args& args) -> bool {
        auto in = args[0].ITensor();
@@ -30,7 +31,7 @@ auto reflection_pad2d TRTORCH_UNUSED = RegisterNodeConversionPatterns().pattern(
        }
 
        std::vector<nvinfer1::ITensor*> tensors_vec;
-       // padding: (padding_left, padding_right, padding_top, padding_bottom)
+       // 2d padding: (padding_left, padding_right, padding_top, padding_bottom)
 
        for (int64_t i = 0; i < int(padding.size() / 2); i++) {
          int64_t axis = inRank - (i + 1); // axis = {inRank - 1, inRank - 2}
@@ -72,6 +73,66 @@ auto reflection_pad2d TRTORCH_UNUSED = RegisterNodeConversionPatterns().pattern(
            inDims = in->getDimensions();
          }
        }
+       auto out = ctx->AssociateValueAndTensor(n->outputs()[0], in);
+       LOG_DEBUG("Output tensor shape: " << out->getDimensions());
+
+       return true;
+     }})
+  .pattern(
+    {"aten::reflection_pad1d(Tensor self, int[2] padding) -> (Tensor)",
+     [](ConversionCtx* ctx, const torch::jit::Node* n, args& args) -> bool {
+       auto in = args[0].ITensor();
+       auto inDims = in->getDimensions();
+       int64_t inRank = inDims.nbDims;
+       auto padding = args[1].unwrapToIntList().vec();
+       if (padding.size() == 1) {
+         for (int64_t i = 0; i < 1; i++)
+           padding.push_back(padding[0]);
+       }
+   
+       std::vector<nvinfer1::ITensor*> tensors_vec;
+       // 1d padding: (padding_left, padding_right)
+
+
+      int64_t axis = inRank - 1; 
+      int64_t padding_index = 0;
+
+      if (padding[padding_index] > 0) { // left padding value
+        tensors_vec.clear();
+
+        for (int i = 0; i < padding[padding_index]; i++) {
+          at::Tensor left_indices = torch::tensor({padding[padding_index] - i}, torch::kInt32);
+          auto indicesTensor = tensor_to_const(ctx, left_indices);
+          auto left_gather_layer = ctx->net->addGather(*in, *indicesTensor, axis);
+          auto left_gather_out = left_gather_layer->getOutput(0);
+          tensors_vec.push_back(left_gather_out);
+        }
+        tensors_vec.push_back(in);
+        auto concat_layer = ctx->net->addConcatenation(tensors_vec.data(), tensors_vec.size());
+        concat_layer->setAxis(axis);
+        in = concat_layer->getOutput(0);
+        inDims = in->getDimensions();
+      }
+
+      if (padding[padding_index + 1] > 0) { // right padding value
+        tensors_vec.clear();
+        tensors_vec.push_back(in);
+
+        for (int i = 0; i < padding[padding_index + 1]; i++) {
+          nvinfer1::ITensor* indicesTensor = NULL;
+          auto indices = torch::tensor({inDims.d[axis] - 1 - (i + 1)}, torch::kInt32);
+          indicesTensor = tensor_to_const(ctx, indices);
+          auto right_gather_layer = ctx->net->addGather(*in, *indicesTensor, axis);
+          auto right_gather_out = right_gather_layer->getOutput(0);
+          tensors_vec.push_back(right_gather_out);
+        }
+
+        auto concat_layer = ctx->net->addConcatenation(tensors_vec.data(), tensors_vec.size());
+        concat_layer->setAxis(axis);
+        in = concat_layer->getOutput(0);
+        inDims = in->getDimensions();
+      }
+    
        auto out = ctx->AssociateValueAndTensor(n->outputs()[0], in);
        LOG_DEBUG("Output tensor shape: " << out->getDimensions());
 
--- tests/core/conversion/converters/test_reflection_pad.cpp
+++ tests/core/conversion/converters/test_reflection_pad.cpp
@@ -29,3 +29,26 @@ TEST(Converters, ATenReflection_pad2dTensorConvertsCorrectly) {
 
   ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt_results[0].reshape_as(jit_results[0]), 2e-6));
 }
+
+TEST(Converters, ATenReflection_pad1dTensorConvertsCorrectly) {
+  const auto graph = R"IR(
+      graph(%0 : Tensor):
+        %1 : int = prim::Constant[value=1]()
+        %2 : int = prim::Constant[value=2]()
+        %3 : int[] = prim::ListConstruct(%1, %2)
+        %4 : Tensor = aten::reflection_pad1d(%0, %3)
+        return (%4))IR";
+
+  auto g = std::make_shared<torch::jit::Graph>();
+  torch::jit::parseIR(graph, g.get());
+
+  auto in1 = at::randint(1, 10, {1, 2, 4}, {at::kCUDA});
+
+  auto params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+  auto jit_results = trtorch::tests::util::RunGraph(g, params, {in1});
+
+  params = trtorch::core::conversion::get_named_params(g->inputs(), {});
+  auto trt_results = trtorch::tests::util::RunGraphEngine(g, params, {in1});
+
+  ASSERT_TRUE(trtorch::tests::util::almostEqual(jit_results[0], trt_results[0].reshape_as(jit_results[0]), 2e-6));
+}
