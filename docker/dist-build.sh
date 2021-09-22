67ba228e28ce14babdc4cd70080e3532ddc63474
Boris Fomitchev <bfomitchev@nvidia.com>
2021-09-22 13:48:17 -0700

    Bumping up base container version, cleanup
    Signed-off-by: Boris Fomitchev <bfomitchev@nvidia.com>

--- docker/dist-build.sh
+++ docker/dist-build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p dist
 
-bazel build //:libtrtorch //:bin --compilation_mode opt
+bazel build //:libtrtorch --compilation_mode opt
 
 cd py && MAX_JOBS=1 LANG=en_US.UTF-8 LANGUAGE=en_US:en LC_ALL=en_US.UTF-8 python3 setup.py bdist_wheel --use-cxx11-abi
 
