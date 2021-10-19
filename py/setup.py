a1ba9d028f286fe4f0ee8d446ab518c3c95dcb48
Anurag Dixit <anuragd@nvidia.com>
2021-10-19 10:38:52 -0700

    (//docker): Review comments incorporated
    Signed-off-by: Anurag Dixit <anuragd@nvidia.com>
    
    (//py): Fix the version suffix
    
    Signed-off-by: Anurag Dixit <anuragd@nvidia.com>

--- py/setup.py
+++ py/setup.py
@@ -234,7 +234,7 @@ setup(name='trtorch',
       long_description=long_description,
       ext_modules=ext_modules,
       install_requires=[
-          'torch>=1.9.0+cu111,<1.11.0',
+          'torch>=1.9.0<1.11.0',
       ],
       setup_requires=[],
       cmdclass={
