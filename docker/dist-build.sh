b88414687e7e7fe86324a726432b1493995377e4
Boris Fomitchev <bfomitchev@nvidia.com>
2021-10-22 13:41:00 -0700

    Cleaned up dist_build.sh
    Signed-off-by: Boris Fomitchev <bfomitchev@nvidia.com>

--- docker/dist-build.sh
+++ docker/dist-build.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-mkdir -p dist
-
-cd py && MAX_JOBS=1 LANG=en_US.UTF-8 LANGUAGE=en_US:en LC_ALL=en_US.UTF-8 python3 setup.py bdist_wheel --use-cxx11-abi
-
-cd ..
-
-cp py/dist/* dist/
+mkdir -p dist && cd py \
+    && MAX_JOBS=1 LANG=en_US.UTF-8 LANGUAGE=en_US:en LC_ALL=en_US.UTF-8 python3 setup.py bdist_wheel --use-cxx11-abi \
+    && cp -r dist/* ../dist/
 
 pip3 install ipywidgets --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host=files.pythonhosted.org
 jupyter nbextension enable --py widgetsnbextension
-
 pip3 install timm
 
 # test install
-mkdir -p /opt/torch_tensorrt && pip3 uninstall -y torch_tensorrt && pip3 install dist/*.whl
+pip3 uninstall -y torch_tensorrt && pip3 install dist/*.whl
 
