d53f136a96f10bd016782b2ff3f147cf33f3c5d5
Naren Dasan <naren@narendasan.com>
2021-11-03 09:11:05 -0700

    fix(aten::conv1d): Update namespace, fix typo in dest IR for conv1d
    Signed-off-by: Naren Dasan <naren@narendasan.com>
    Signed-off-by: Naren Dasan <narens@nvidia.com>

--- core/lowering/passes/convNd_to_convolution.cpp
+++ core/lowering/passes/convNd_to_convolution.cpp
@@ -2,7 +2,7 @@
 
 #include "core/util/prelude.h"
 
-namespace trtorch {
+namespace torch_tensorrt {
 namespace core {
 namespace lowering {
 namespace passes {
@@ -12,6 +12,7 @@ void Conv1DToConvolution(std::shared_ptr<torch::jit::Graph>& graph) {
         graph(%x, %w, %b, %s, %p, %d, %g):
             %4 : Tensor = aten::conv1d(%x, %w, %b, %s, %p, %d, %g)
             return (%4))IR";
+
   std::string convolution_pattern = R"IR(
         graph(%x, %w, %b, %s, %p, %d, %g):
             %1 : bool = prim::Constant[value=0]()
@@ -43,7 +44,45 @@ void ConvTransposed1DToConvolution(std::shared_ptr<torch::jit::Graph>& graph) {
   LOG_GRAPH("Post map conv_transpose1d -> _convolution: " << *graph);
 }
 
+void Conv2DToConvolution(std::shared_ptr<torch::jit::Graph>& graph) {
+  std::string conv2d_pattern = R"IR(
+        graph(%x, %w, %b, %s, %p, %d, %g):
+            %4 : Tensor = aten::conv2d(%x, %w, %b, %s, %p, %d, %g)
+            return (%4))IR";
+  std::string convolution_pattern = R"IR(
+        graph(%x, %w, %b, %s, %p, %d, %g):
+            %1 : bool = prim::Constant[value=0]()
+            %2 : int[] = prim::Constant[value=[0, 0]]()
+            %4 : Tensor = aten::_convolution(%x, %w, %b, %s, %p, %d, %1, %2, %g, %1, %1, %1, %1)
+            return (%4))IR";
+
+  // replace matmul + add pattern to linear
+  torch::jit::SubgraphRewriter map_conv2d_to_convolution;
+  map_conv2d_to_convolution.RegisterRewritePattern(conv2d_pattern, convolution_pattern);
+  map_conv2d_to_convolution.runOnGraph(graph);
+  LOG_GRAPH("Post map conv2d -> _convolution: " << *graph);
+}
+
+void Conv3DToConvolution(std::shared_ptr<torch::jit::Graph>& graph) {
+  std::string conv3d_pattern = R"IR(
+        graph(%x, %w, %b, %s, %p, %d, %g):
+            %4 : Tensor = aten::conv3d(%x, %w, %b, %s, %p, %d, %g)
+            return (%4))IR";
+  std::string convolution_pattern = R"IR(
+        graph(%x, %w, %b, %s, %p, %d, %g):
+            %1 : bool = prim::Constant[value=0]()
+            %2 : int[] = prim::Constant[value=[0, 0, 0]]()
+            %4 : Tensor = aten::_convolution(%x, %w, %b, %s, %p, %d, %1, %2, %g, %1, %1, %1, %1)
+            return (%4))IR";
+
+  // replace matmul + add pattern to linear
+  torch::jit::SubgraphRewriter map_conv3d_to_convolution;
+  map_conv3d_to_convolution.RegisterRewritePattern(conv3d_pattern, convolution_pattern);
+  map_conv3d_to_convolution.runOnGraph(graph);
+  LOG_GRAPH("Post map conv3d -> _convolution: " << *graph);
+}
+
 } // namespace passes
 } // namespace lowering
 } // namespace core
-} // namespace trtorch
+} // namespace torch_tensorrt
