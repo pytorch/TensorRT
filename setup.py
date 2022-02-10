68b9a2cd0e18d41c42d45cde6c49b6e0df4f1a88
Yinghai Lu <yinghai@fb.com>
2022-02-10 00:20:31 -0800

    OSS setup.py and README

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='fx2trt_oss',
+    version='0.1',
+    description='Torch-FX to TensorRT Converter',
+    author='PyTorch Team',
+    packages=['fx2trt_oss.fx', 'fx2trt_oss.fx.converters', 'fx2trt_oss.fx.passes', 'fx2trt_oss.fx.tools',],
+    package_dir={'fx2trt_oss.fx': 'fx',
+        'fx2trt_oss.fx.converters': 'fx/converters',
+        'fx2trt_oss.fx.passes': 'fx/passes',
+        'fx2trt_oss.fx.tools': 'fx/tools',
+    },
+)
