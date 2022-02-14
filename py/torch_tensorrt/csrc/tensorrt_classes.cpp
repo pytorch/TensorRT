67629f6d956f541c8825aa46e990c87d88e1bfb2
inocsin <vcheungyi@163.com>
2022-02-14 15:24:43 +0800

    fix: fix bug that python api doesn't pass truncate_long_and_double value to internal.partition_info
    Signed-off-by: inocsin <vcheungyi@163.com>

--- py/torch_tensorrt/csrc/tensorrt_classes.cpp
+++ py/torch_tensorrt/csrc/tensorrt_classes.cpp
@@ -216,6 +216,7 @@ core::CompileSpec CompileSpec::toInternalCompileSpec() {
   info.partition_info.enabled = torch_fallback.enabled;
   info.partition_info.min_block_size = torch_fallback.min_block_size;
   info.partition_info.forced_fallback_operators = torch_fallback.forced_fallback_operators;
+  info.partition_info.truncate_long_and_double = truncate_long_and_double;
   info.lower_info.forced_fallback_modules = torch_fallback.forced_fallback_modules;
   info.convert_info.engine_settings.truncate_long_and_double = truncate_long_and_double;
 
