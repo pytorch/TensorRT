cb2ab70e4a17dc934ffd282bf1bd82de1f9189fe
Naren Dasan <naren@narendasan.com>
2022-03-07 19:18:45 -0800

    feat(//core/conversion/evaluators): aten::pow support
    Adds support for the following aten::pow variants in the evaluator
    library
    
    ```
     	"aten::pow.int(int a, int b) -> (float)",
            "aten::pow.float(float a, float b) -> (float)",
            "aten::pow.int_float(int a, float b) -> (float)",
            "aten::pow.float_int(float a, int b) -> (float)",
    ```
    
    Signed-off-by: Naren Dasan <naren@narendasan.com>
    Signed-off-by: Naren Dasan <narens@nvidia.com>

--- core/conversion/evaluators/eval_macros.h
+++ core/conversion/evaluators/eval_macros.h
@@ -77,6 +77,53 @@
        },                                                                                              \
        EvalOptions().validSchemas(schemas)});
 
+#define DEFINE_ARITHMATIC_TWO_INPUT_EVALUATOR(name, node_kind, operation, schemas)                        \
+  auto name##_registrations TORCHTRT_UNUSED = RegisterNodeEvaluators().evaluator(                      \
+      {c10::Symbol::fromQualString(node_kind),                                                         \
+       [](const torch::jit::Node* n, kwargs& args) -> c10::optional<torch::jit::IValue> {              \
+         if (args.at(n->input(0)).IValue()->isInt()) {                                                 \
+           auto a = args.at(n->input(0)).unwrapToInt();                                                \
+           if (args.at(n->input(1)).IValue()->isInt()) {                                               \
+             auto b = args.at(n->input(1)).unwrapToInt();                                              \
+             return operation;                                                                         \
+           } else if (args.at(n->input(1)).IValue()->isDouble()) {                                     \
+             auto b = args.at(n->input(1)).unwrapToDouble();                                           \
+             return operation;                                                                         \
+           } else if (args.at(n->input(1)).IValue()->isBool()) {                                       \
+             auto b = args.at(n->input(1)).unwrapToBool();                                             \
+             return operation;                                                                         \
+           } else {                                                                                    \
+             TORCHTRT_THROW_ERROR(                                                                     \
+                 "Unimplemented data type for "                                                        \
+                 << node_kind << " evaluator b arg:" << args.at(n->input(1)).IValue()->type()->str()); \
+             return {};                                                                                \
+           }                                                                                           \
+         } else if (args.at(n->input(0)).IValue()->isDouble()) {                                       \
+           auto a = args.at(n->input(0)).unwrapToDouble();                                             \
+           if (args.at(n->input(1)).IValue()->isInt()) {                                               \
+             auto b = args.at(n->input(1)).unwrapToInt();                                              \
+             return operation;                                                                         \
+           } else if (args.at(n->input(1)).IValue()->isDouble()) {                                     \
+             auto b = args.at(n->input(1)).unwrapToDouble();                                           \
+             return operation;                                                                         \
+           } else if (args.at(n->input(1)).IValue()->isBool()) {                                       \
+             auto b = args.at(n->input(1)).unwrapToBool();                                             \
+             return operation;                                                                         \
+           } else {                                                                                    \
+             TORCHTRT_THROW_ERROR(                                                                     \
+                 "Unimplemented data type for "                                                        \
+                 << node_kind << " evaluator b arg:" << args.at(n->input(1)).IValue()->type()->str()); \
+             return {};                                                                                \
+           }                                                                                           \
+         } else {                                                                                      \
+           TORCHTRT_THROW_ERROR(                                                                       \
+               "Unimplemented data type for "                                                          \
+               << node_kind << " evaluator a arg: " << args.at(n->input(0)).IValue()->type()->str());  \
+           return {};                                                                                  \
+         }                                                                                             \
+       },                                                                                              \
+       EvalOptions().validSchemas(schemas)});
+
 #define DEFINE_TWO_INPUT_SIMPLE_EVALUATOR(node_kind, node_name, operation, type, schemas) \
   auto node_kind##_registrations TORCHTRT_UNUSED = RegisterNodeEvaluators().evaluator(    \
       {c10::Symbol::fromQualString(node_name),                                            \
