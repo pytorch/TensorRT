01d98c72bd4f8182d29de6ec9a9f638c78595a7a
Dheeraj Peri <peri.dheeraj@gmail.com>
2022-03-16 23:19:14 -0700

    feat: Enable prim::DictConstruct to fallback without conversion check error
    Signed-off-by: Dheeraj Peri <peri.dheeraj@gmail.com>

--- core/conversion/conversion.cpp
+++ core/conversion/conversion.cpp
@@ -487,7 +487,8 @@ std::string ConvertBlockToEngine(
 std::unordered_map<c10::OperatorName, std::string> GetUnsupportedOpsInBlock(const torch::jit::Block* b) {
   std::unordered_map<c10::OperatorName, std::string> unsupported_ops;
   for (const auto n : b->nodes()) {
-    if (n->kind() != torch::jit::prim::Loop && n->kind() != torch::jit::prim::If && !OpSupported(n)) {
+    if (n->kind() != torch::jit::prim::Loop && n->kind() != torch::jit::prim::If && !OpSupported(n) &&
+        n->kind() != torch::jit::prim::DictConstruct) {
       auto schema = n->maybeSchema();
       TORCHTRT_CHECK(
           schema,
