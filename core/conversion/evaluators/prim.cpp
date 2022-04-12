991f0232d046e50420cabf6e8cafa1c862b56061
inocsin <vcheungyi@163.com>
2022-04-12 11:51:53 +0800

    [collection] rebase to master, update some api
    Signed-off-by: inocsin <vcheungyi@163.com>

--- core/conversion/evaluators/prim.cpp
+++ core/conversion/evaluators/prim.cpp
@@ -40,6 +40,68 @@ auto prim_registrations =
                       auto outputVec = outputs->toList().vec();
                       return std::move(c10::ivalue::Tuple::create(outputVec));
                     }})
+        .evaluator({torch::jit::prim::ListConstruct,
+                    [](const torch::jit::Node* n, kwargs& args) -> c10::optional<torch::jit::IValue> {
+                      const auto num_inputs = n->inputs().size();
+                      if (constTypesOnly(args)) {
+                        c10::ListTypePtr lt = n->output()->type()->expect<c10::ListType>();
+                        if (torch::jit::IntType::get() == lt->getElementType()) {
+                          c10::List<int64_t> list;
+                          list.reserve(num_inputs);
+                          for (auto in : n->inputs()) {
+                            list.emplace_back(std::move(args.at(in).unwrapToInt()));
+                          }
+                          return c10::optional<torch::jit::IValue>(std::move(torch::jit::IValue(list)));
+                        } else if (torch::jit::FloatType::get() == lt->getElementType()) {
+                          c10::List<double> list;
+                          list.reserve(num_inputs);
+                          for (auto in : n->inputs()) {
+                            list.emplace_back(std::move(args.at(in).unwrapToDouble()));
+                          }
+                          return c10::optional<torch::jit::IValue>(std::move(torch::jit::IValue(list)));
+                        } else if (lt->getElementType() == torch::jit::BoolType::get()) {
+                          c10::List<bool> list;
+                          list.reserve(num_inputs);
+                          for (auto in : n->inputs()) {
+                            list.emplace_back(std::move(args.at(in).unwrapToBool()));
+                          }
+                          return c10::optional<torch::jit::IValue>(std::move(torch::jit::IValue(list)));
+                        } else if (lt->getElementType()->isSubtypeOf(torch::jit::TensorType::get())) {
+                          c10::List<at::Tensor> list;
+                          list.reserve(num_inputs);
+                          for (auto in : n->inputs()) {
+                            if (args.at(in).isIValue()) {
+                              list.emplace_back(std::move(args.at(in).unwrapToTensor()));
+                            }
+                          }
+                          return c10::optional<torch::jit::IValue>(std::move(torch::jit::IValue(list)));
+                        } else {
+                          c10::TypePtr elementType = lt->getElementType();
+                          auto list = c10::impl::GenericList(elementType);
+                          list.reserve(num_inputs);
+                          for (auto in : n->inputs()) {
+                            list.emplace_back(std::move(*(args.at(in).IValue())));
+                          }
+                          return c10::optional<torch::jit::IValue>(std::move(torch::jit::IValue(list)));
+                        }
+                      } else {
+                        c10::ListTypePtr lt = n->output()->type()->expect<c10::ListType>();
+                        c10::TypePtr elementType = lt->getElementType();
+                        auto list = c10::impl::GenericList(elementType);
+                        list.reserve(num_inputs);
+                        for (auto in : n->inputs()) {
+                          if (args.at(in).isITensor()) {
+                            auto tensor_holder = TensorContainer();
+                            tensor_holder.hold_tensor(args.at(in).ITensor());
+                            auto ival = c10::IValue(std::move(c10::make_intrusive<TensorContainer>(tensor_holder)));
+                            list.emplace_back(std::move(ival));
+                          } else {
+                            list.emplace_back(std::move(args.at(in).unwrapToTensor()));
+                          }
+                        }
+                        return c10::optional<torch::jit::IValue>(std::move(torch::jit::IValue(list)));
+                      }
+                    }})
         .evaluator({c10::Symbol::fromQualString("prim::dtype"),
                     [](const torch::jit::Node* n, kwargs& args) -> c10::optional<torch::jit::IValue> {
                       auto input = args.at(n->input(0));
--- core/partitioning/shape_analysis.h
+++ core/partitioning/shape_analysis.h
@@ -6,9 +6,6 @@ namespace torch_tensorrt {
 namespace core {
 namespace partitioning {
 
-// std::unordered_map<const torch::jit::Value*, torch::jit::IValue> generateRandomInputs(
-//     std::unordered_map<const torch::jit::Value*, ir::Input>& input_ranges,
-//     std::unordered_map<const torch::jit::Value*, c10::optional<at::ScalarType>>& input_types);
 
 std::unordered_map<const torch::jit::Value*, torch::jit::IValue> generateRandomInputs(
     std::unordered_map<const torch::jit::Value*, std::vector<ir::Input>>& input_ranges,
