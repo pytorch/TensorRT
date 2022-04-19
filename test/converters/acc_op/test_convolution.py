Fix conv1d with explicit precision (#75824)
Summary:
X-link: https://github.com/pytorch/pytorch/pull/75824

Pull Request resolved: https://github.com/pytorch/fx2trt/pull/57

Reviewed By: jerryzh168

Differential Revision: D35621145

fbshipit-source-id: 639c373f5e3e187eb6971d7f2d4b5e92f11dfc62

test/converters/acc_op/test_convolution.py
@@ -37,7 +37,7 @@ class TestConvolutionConverter(AccTestCase):
                 return self.conv(x)
 
         inputs = [torch.randn(1, 3, 32)]
-        self.run_test(TestModule(), inputs, expected_ops={acc_ops.conv1d})
+        self.run_test(TestModule(), inputs, expected_ops={acc_ops.conv1d}, test_explicit_precision=True)
 
 
     @parameterized.expand(