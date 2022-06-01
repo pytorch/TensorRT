c0bcffb2a2f9a80ed4f4ccaab98243bdaab4b044
Brenton Chu <bchu@nvidia.com>
2022-05-31 17:32:13 -0700

    type conversion to int for equality check in bitwise test

--- tests/core/conversion/converters/test_bitwise.cpp
+++ tests/core/conversion/converters/test_bitwise.cpp
@@ -11,26 +11,29 @@ std::string gen_test_graph() {
           return (%3))IR";
 }
 
-#define test_bitwise_not(dtype)                                                            \
-  TEST(Converters, ATenBitwiseNot##dtype##ConvertsCorrectly) {                             \
-    const auto graph = gen_test_graph();                                                   \
-                                                                                           \
-    auto g = std::make_shared<torch::jit::Graph>();                                        \
-    torch::jit::parseIR(graph, g.get());                                                   \
-                                                                                           \
-    at::Tensor in;                                                                         \
-    if (strcmp(#dtype, "Integer") == 0)                                                    \
-      in = at::randint(-128, 128, {10}, {at::kCUDA}).toType(at::kInt);                     \
-    if (strcmp(#dtype, "Boolean") == 0)                                                    \
-      in = at::randint(0, 1, {10}, {at::kCUDA}).toType(at::kBool);                         \
-    auto params = torch_tensorrt::core::ir::get_static_params(g->inputs(), {});            \
-    auto jit_results = torch_tensorrt::tests::util::RunGraph(g, params, {in});             \
-                                                                                           \
-    in = at::clone(in);                                                                    \
-    params = torch_tensorrt::core::ir::get_static_params(g->inputs(), {});                 \
-    auto trt_results = torch_tensorrt::tests::util::RunGraphEngine(g, params, {in});       \
-                                                                                           \
-    ASSERT_TRUE(torch_tensorrt::tests::util::exactlyEqual(jit_results[0], trt_results[0]));\
+#define test_bitwise_not(dtype)                                                      \
+  TEST(Converters, ATenBitwiseNot##dtype##ConvertsCorrectly) {                       \
+    const auto graph = gen_test_graph();                                             \
+                                                                                     \
+    auto g = std::make_shared<torch::jit::Graph>();                                  \
+    torch::jit::parseIR(graph, g.get());                                             \
+                                                                                     \
+    at::Tensor in;                                                                   \
+    if (strcmp(#dtype, "Integer") == 0)                                              \
+      in = at::randint(-128, 128, {10}, {at::kCUDA}).toType(at::kInt);               \
+    if (strcmp(#dtype, "Boolean") == 0)                                              \
+      in = at::randint(0, 1, {10}, {at::kCUDA}).toType(at::kBool);                   \
+    auto params = torch_tensorrt::core::ir::get_static_params(g->inputs(), {});      \
+    auto jit_results = torch_tensorrt::tests::util::RunGraph(g, params, {in});       \
+                                                                                     \
+    in = at::clone(in);                                                              \
+    params = torch_tensorrt::core::ir::get_static_params(g->inputs(), {});           \
+    auto trt_results = torch_tensorrt::tests::util::RunGraphEngine(g, params, {in}); \
+                                                                                     \
+    auto jit_int = jit_results[0].toType(at::kInt);                                  \
+    auto trt_int = trt_results[0].toType(at::kInt);                                  \
+                                                                                     \
+    ASSERT_TRUE(torch_tensorrt::tests::util::exactlyEqual(jit_int, trt_int));        \
   }
 
 test_bitwise_not(Integer);
