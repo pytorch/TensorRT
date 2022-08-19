6ae3c4802828cf60324e0e9528538c2aa4067d07
Anurag Dixit <a.dixit91@gmail.com>
2022-08-18 18:49:28 -0700

    fix: truncate long and double for ptq test and fix for finetune dataloader
    Signed-off-by: Anurag Dixit <a.dixit91@gmail.com>

--- examples/int8/training/vgg16/finetune_qat.py
+++ examples/int8/training/vgg16/finetune_qat.py
@@ -251,7 +251,7 @@ def main():
         state = ckpt["state"]
 
     data = iter(training_dataloader)
-    images, _ = data.next()
+    images, _ = next(data)
 
     writer.add_graph(model, images.cuda())
     writer.close()
