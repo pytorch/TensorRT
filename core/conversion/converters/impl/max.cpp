9db2852b6d6d83e5ca22dc965643664aab1f7e58
Michael Feliz <104801882+mfeliz-cruise@users.noreply.github.com>
2022-09-02 11:41:59 -0700

    [feat] Add support for argmax and argmin (#1312)
    * [feat] Add support for argmax and argmin
    
    Adds support for aten::argmax and aten::argmin.
    
    Fixes # (issue)
    
    Please delete options that are not relevant and/or add your own.
    
    - Bug fix (non-breaking change which fixes an issue)
    - New feature (non-breaking change which adds functionality)
    - Breaking change (fix or feature that would cause existing functionality to not work as expected)
    - This change requires a documentation update
    
    - [ ] My code follows the style guidelines of this project (You can use the linters)
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas and hacks
    - [ ] I have made corresponding changes to the documentation
    - [ ] I have added tests to verify my fix or my feature
    - [ ] New and existing unit tests pass locally with my changes
    - [ ] I have added the relevant labels to my PR in so that relevant reviewers are notified
    
    * move max.cpp tests to test_max.cpp no functional change
    
    * fix permissions on max.cpp

--- core/conversion/converters/impl/max.cpp
+++ core/conversion/converters/impl/max.cpp
@@ -13,47 +13,95 @@ namespace conversion {
 namespace converters {
 namespace impl {
 namespace {
-auto max_registrations TORCHTRT_UNUSED = RegisterNodeConversionPatterns().pattern(
-    {"aten::max.dim(Tensor self, int dim, bool keepdim=False) -> (Tensor values, Tensor indices)",
-     [](ConversionCtx* ctx, const torch::jit::Node* n, args& args) -> bool {
-       auto self = args[0].ITensorOrFreeze(ctx);
-       auto dim = args[1].unwrapToInt();
-       auto keep_dims = args[2].unwrapToBool();
-       auto selfDim = util::toVec(self->getDimensions());
-       if (dim < 0) {
-         dim = selfDim.size() + dim;
-       }
-       uint32_t shiftDim = 1 << dim;
-       auto TopKOperation = nvinfer1::TopKOperation::kMAX;
-       auto topk_layer = ctx->net->addTopK(*self, TopKOperation, 1, shiftDim);
-       TORCHTRT_CHECK(topk_layer, "Unable to create max layer from node: " << *n);
-       auto topk_dims = util::toVec(topk_layer->getOutput(0)->getDimensions());
 
-       nvinfer1::ITensor* out0 = nullptr;
-       nvinfer1::ITensor* out1 = nullptr;
-       if (!keep_dims) {
-         if (topk_dims[dim] == 1) {
-           auto squeeze_layer = ctx->net->addShuffle(*topk_layer->getOutput(0));
-           squeeze_layer->setReshapeDimensions(util::squeezeDims(topk_layer->getOutput(0)->getDimensions(), dim));
-           TORCHTRT_CHECK(squeeze_layer, "Unable to create squeeze_layer layer from node: " << *n);
-           out0 = ctx->AssociateValueAndTensor(n->outputs()[0], squeeze_layer->getOutput(0));
+bool min_max_dim(ConversionCtx* ctx, const torch::jit::Node* n, args& args, nvinfer1::TopKOperation topKOperation) {
+  auto self = args[0].ITensorOrFreeze(ctx);
+  auto dim = args[1].unwrapToInt();
+  auto keep_dims = args[2].unwrapToBool();
+  auto selfDim = util::toVec(self->getDimensions());
+  if (dim < 0) {
+    dim = selfDim.size() + dim;
+  }
+  uint32_t reduce_axes_mask = 1 << dim;
+  auto topk_layer = ctx->net->addTopK(*self, topKOperation, 1, reduce_axes_mask);
+  TORCHTRT_CHECK(topk_layer, "Unable to create topk layer from node: " << *n);
+  auto topk_dims = util::toVec(topk_layer->getOutput(0)->getDimensions());
 
-           auto squeeze_layer_indices = ctx->net->addShuffle(*topk_layer->getOutput(1));
-           squeeze_layer_indices->setReshapeDimensions(
-               util::squeezeDims(topk_layer->getOutput(1)->getDimensions(), dim));
-           TORCHTRT_CHECK(squeeze_layer_indices, "Unable to create squeeze_layer_indices layer from node: " << *n);
-           out1 = ctx->AssociateValueAndTensor(n->outputs()[1], squeeze_layer_indices->getOutput(0));
-         }
-       } else {
-         out0 = ctx->AssociateValueAndTensor(n->outputs()[0], topk_layer->getOutput(0));
-         out1 = ctx->AssociateValueAndTensor(n->outputs()[1], topk_layer->getOutput(1));
-       }
+  nvinfer1::ITensor* out0 = nullptr;
+  nvinfer1::ITensor* out1 = nullptr;
+  if (!keep_dims) {
+    TORCHTRT_CHECK(topk_dims[dim] == 1, "Unexpected size in squeeze dimension. Expected: 1 Actual: " << topk_dims[dim]);
+    auto squeeze_layer = ctx->net->addShuffle(*topk_layer->getOutput(0));
+    squeeze_layer->setReshapeDimensions(util::squeezeDims(topk_layer->getOutput(0)->getDimensions(), dim));
+    TORCHTRT_CHECK(squeeze_layer, "Unable to create squeeze_layer layer from node: " << *n);
+    out0 = ctx->AssociateValueAndTensor(n->outputs()[0], squeeze_layer->getOutput(0));
 
-       LOG_DEBUG("Output tensor(0) shape: " << out0->getDimensions());
-       LOG_DEBUG("Output tensor(1) shape: " << out1->getDimensions());
+    auto squeeze_layer_indices = ctx->net->addShuffle(*topk_layer->getOutput(1));
+    squeeze_layer_indices->setReshapeDimensions(util::squeezeDims(topk_layer->getOutput(1)->getDimensions(), dim));
+    TORCHTRT_CHECK(squeeze_layer_indices, "Unable to create squeeze_layer_indices layer from node: " << *n);
+    out1 = ctx->AssociateValueAndTensor(n->outputs()[1], squeeze_layer_indices->getOutput(0));
+  } else {
+    out0 = ctx->AssociateValueAndTensor(n->outputs()[0], topk_layer->getOutput(0));
+    out1 = ctx->AssociateValueAndTensor(n->outputs()[1], topk_layer->getOutput(1));
+  }
 
-       return true;
-     }});
+  LOG_DEBUG("Output tensor(0) shape: " << out0->getDimensions());
+  LOG_DEBUG("Output tensor(1) shape: " << out1->getDimensions());
+
+  return true;
+}
+
+bool arg_min_max(ConversionCtx* ctx, const torch::jit::Node* n, args& args, nvinfer1::TopKOperation topKOperation) {
+  auto self = args[0].ITensorOrFreeze(ctx);
+  auto dim = args[1].unwrapToInt();
+  auto keep_dims = args[2].unwrapToBool();
+  auto selfDim = util::toVec(self->getDimensions());
+  if (dim < 0) {
+    dim = selfDim.size() + dim;
+  }
+  uint32_t reduce_axes_mask = 1 << dim;
+  auto topk_layer = ctx->net->addTopK(*self, topKOperation, 1, reduce_axes_mask);
+  TORCHTRT_CHECK(topk_layer, "Unable to create topk layer from node: " << *n);
+  auto topk_dims = util::toVec(topk_layer->getOutput(0)->getDimensions());
+
+  nvinfer1::ITensor* out = nullptr;
+  if (!keep_dims) {
+    TORCHTRT_CHECK(topk_dims[dim] == 1, "Unexpected size in squeeze dimension. Expected: 1 Actual: " << topk_dims[dim]);
+    auto squeeze_layer_indices = ctx->net->addShuffle(*topk_layer->getOutput(1));
+    squeeze_layer_indices->setReshapeDimensions(util::squeezeDims(topk_layer->getOutput(1)->getDimensions(), dim));
+    TORCHTRT_CHECK(squeeze_layer_indices, "Unable to create squeeze_layer_indices layer from node: " << *n);
+    out = ctx->AssociateValueAndTensor(n->outputs()[0], squeeze_layer_indices->getOutput(0));
+  } else {
+    out = ctx->AssociateValueAndTensor(n->outputs()[0], topk_layer->getOutput(1));
+  }
+
+  LOG_DEBUG("Output tensor shape: " << out->getDimensions());
+
+  return true;
+}
+
+auto max_registrations TORCHTRT_UNUSED =
+    RegisterNodeConversionPatterns()
+        .pattern(
+            {"aten::max.dim(Tensor self, int dim, bool keepdim=False) -> (Tensor values, Tensor indices)",
+             [](ConversionCtx* ctx, const torch::jit::Node* n, args& args) -> bool {
+               return min_max_dim(ctx, n, args, nvinfer1::TopKOperation::kMAX);
+             }})
+        .pattern(
+            {"aten::min.dim(Tensor self, int dim, bool keepdim=False) -> (Tensor values, Tensor indices)",
+             [](ConversionCtx* ctx, const torch::jit::Node* n, args& args) -> bool {
+               return min_max_dim(ctx, n, args, nvinfer1::TopKOperation::kMIN);
+             }})
+        .pattern(
+            {"aten::argmax(Tensor self, int dim, bool keepdim=False) -> (Tensor)",
+             [](ConversionCtx* ctx, const torch::jit::Node* n, args& args) -> bool {
+               return arg_min_max(ctx, n, args, nvinfer1::TopKOperation::kMAX);
+             }})
+        .pattern(
+            {"aten::argmin(Tensor self, int dim, bool keepdim=False) -> (Tensor)",
+             [](ConversionCtx* ctx, const torch::jit::Node* n, args& args) -> bool {
+               return arg_min_max(ctx, n, args, nvinfer1::TopKOperation::kMIN);
+             }});
 } // namespace
 } // namespace impl
 } // namespace converters
