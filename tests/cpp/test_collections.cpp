06779616e3a7e53f0a969aadaf045044984cfbdf
Anurag Dixit <a.dixit91@gmail.com>
2022-09-08 13:41:43 -0700

    chore: Relaxing the threshold for test_collections
    Signed-off-by: Anurag Dixit <a.dixit91@gmail.com>

--- tests/cpp/test_collections.cpp
+++ tests/cpp/test_collections.cpp
@@ -192,9 +192,9 @@ TEST(CppAPITests, TestCollectionTupleInputOutput) {
   auto trt_out = trt_mod.forward(complex_inputs);
 
   ASSERT_TRUE(torch_tensorrt::tests::util::almostEqual(
-      out.toTuple()->elements()[0].toTensor(), trt_out.toTuple()->elements()[0].toTensor(), 1e-4));
+      out.toTuple()->elements()[0].toTensor(), trt_out.toTuple()->elements()[0].toTensor(), 1e-3));
   ASSERT_TRUE(torch_tensorrt::tests::util::almostEqual(
-      out.toTuple()->elements()[1].toTensor(), trt_out.toTuple()->elements()[1].toTensor(), 1e-5));
+      out.toTuple()->elements()[1].toTensor(), trt_out.toTuple()->elements()[1].toTensor(), 1e-3));
 }
 
 TEST(CppAPITests, TestCollectionListInputOutput) {
