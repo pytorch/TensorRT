7013e9d7a64931f82ead537376fcfbbffb3b9075
Naren Dasan <naren@narendasan.com>
2022-09-22 19:22:13 -0700

    chore: Move master to test nightly only
    Signed-off-by: Naren Dasan <naren@narendasan.com>
    Signed-off-by: Naren Dasan <narens@nvidia.com>

--- py/setup.py
+++ py/setup.py
@@ -376,7 +376,7 @@ setup(
     long_description=long_description,
     ext_modules=ext_modules,
     install_requires=[
-        "torch>=1.12.0+cu113,<1.13.0",
+        "torch>=1.13.0,<1.14.0",
     ],
     setup_requires=[],
     cmdclass={
