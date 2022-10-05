38dfa0c3dfc3fbfc72a21d1689062c7f75abb12a
George S <113141689+gs-olive@users.noreply.github.com>
2022-10-05 16:40:18 -0700

    feat: Add converter for aten::sign unary op (#1391)
    * feat: Add converter for sign unary operator
    
    - Add sign operator
    - Update test cases to test op
    - Ensure tests cover both int and float cases with negative and positive
    sign
    - Ensure tests cover cases where elements equal zero
    
    * Remove round unary from PR

--- tests/core/conversion/converters/test_unary.cpp
+++ tests/core/conversion/converters/test_unary.cpp
@@ -47,6 +47,40 @@ TEST(Converters, ATenReciprocalIntConvertsCorrectly) {
   ASSERT_TRUE(torch_tensorrt::tests::util::exactlyEqual(jit_results[0], trt_results[0]));
 }
 
+TEST(Converters, ATenSignConvertsCorrectly) {
+  const auto graph = gen_test_graph("sign");
+  auto g = std::make_shared<torch::jit::Graph>();
+  torch::jit::parseIR(graph, g.get());
+
+  // Resize range to [-10, 10] to span negative values
+  auto in = -20 * at::rand({2, 3, 5, 5}, {at::kCUDA}) + 10;
+  auto params = torch_tensorrt::core::ir::get_static_params(g->inputs(), {});
+  auto jit_results = torch_tensorrt::tests::util::RunGraph(g, params, {in});
+
+  params = torch_tensorrt::core::ir::get_static_params(g->inputs(), {});
+  auto trt_results = torch_tensorrt::tests::util::RunGraphEngine(g, params, {in});
+
+  ASSERT_TRUE(
+      torch_tensorrt::tests::util::almostEqual(jit_results[0], trt_results[0].reshape_as(jit_results[0]), 2e-6));
+}
+
+TEST(Converters, ATenSignConvertsZerosCorrectly) {
+  const auto graph = gen_test_graph("sign");
+  auto g = std::make_shared<torch::jit::Graph>();
+  torch::jit::parseIR(graph, g.get());
+
+  // Resize range to [-1, 1] to span negative values, cast to int to include zero
+  auto in = (-2 * at::rand({7, 3, 1, 5}, {at::kCUDA}) + 1).to(torch::kInt32);
+  auto params = torch_tensorrt::core::ir::get_static_params(g->inputs(), {});
+  auto jit_results = torch_tensorrt::tests::util::RunGraph(g, params, {in});
+
+  params = torch_tensorrt::core::ir::get_static_params(g->inputs(), {});
+  auto trt_results = torch_tensorrt::tests::util::RunGraphEngine(g, params, {in});
+
+  ASSERT_TRUE(
+      torch_tensorrt::tests::util::almostEqual(jit_results[0], trt_results[0].reshape_as(jit_results[0]), 2e-6));
+}
+
 #define test_unary(unary, name)                                                                  \
   TEST(Converters, ATen##name##ConvertsCorrectly) {                                              \
     const auto graph = gen_test_graph(#unary);                                                   \
