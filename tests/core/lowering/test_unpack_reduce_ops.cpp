6bef925f7c2ed7eef622d93e752f7756cb83c5ec
George S <113141689+gs-olive@users.noreply.github.com>
2022-10-11 17:38:07 -0700

    fix: `torch.std` and `torch.var` support multi-dimensional reductions (#1395)
    * Augment current tests to reproduce error generating bug with standard dev / variance lowering
    
    * fix: Ensure torch.std (and torch.var) support multiple dimensions
    
    - Refactor IR code to avoid the use of select, for which only
    single-dimension support exists currently
    - Update formula in Bessel's correction (unbiased) case
    - Include regression tests to catch multi-dimensional indexing errors
    
    * Improved comments on IR for UnpackVar function

--- tests/core/lowering/test_unpack_reduce_ops.cpp
+++ tests/core/lowering/test_unpack_reduce_ops.cpp
@@ -134,7 +134,8 @@ TEST(LoweringPasses, UnpackStdKeepDimsLowersCorrectly) {
         %5 : bool = prim::Constant[value=0]() # test_zeros.py:10:65
         %4 : bool = prim::Constant[value=1]() # test_zeros.py:10:50
         %3 : int = prim::Constant[value=0]() # test_zeros.py:10:39
-        %6 : int[] = prim::ListConstruct(%3)
+        %one : int = prim::Constant[value=1]()
+        %6 : int[] = prim::ListConstruct(%3, %one)
         %7 : Tensor = aten::std(%x.1, %6, %5, %5) # test_zeros.py:10:26
         return (%7))IR";
 
@@ -184,7 +185,8 @@ TEST(LoweringPasses, UnpackStdUnbiasedKeepDimsLowersCorrectly) {
         %5 : bool = prim::Constant[value=0]() # test_zeros.py:10:65
         %4 : bool = prim::Constant[value=1]() # test_zeros.py:10:50
         %3 : int = prim::Constant[value=0]() # test_zeros.py:10:39
-        %6 : int[] = prim::ListConstruct(%3)
+        %one : int = prim::Constant[value=1]()
+        %6 : int[] = prim::ListConstruct(%3, %one)
         %7 : Tensor = aten::std(%x.1, %6, %4, %5) # test_zeros.py:10:26
         return (%7))IR";
 
