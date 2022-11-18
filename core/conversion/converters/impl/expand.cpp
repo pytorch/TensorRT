131759aa203521b379cdf936758f79c49c249b9c
Naren Dasan <naren@narendasan.com>
2022-11-18 02:06:21 -0800

    chore: lint
    Signed-off-by: Naren Dasan <naren@narendasan.com>
    Signed-off-by: Naren Dasan <narens@nvidia.com>

--- core/conversion/converters/impl/expand.cpp
+++ core/conversion/converters/impl/expand.cpp
@@ -376,7 +376,7 @@ auto expand_registrations TORCHTRT_UNUSED =
                std::vector<int64_t> collapse_shape_vec;
                for (int64_t k = 0; k < repeat_shape_dims.nbDims; k++) {
                  if (k == dim) {
-                   int64_t collapse_dim = repeat_shape_dims.d[k] * repeat_shape_dims.d[k+1];
+                   int64_t collapse_dim = repeat_shape_dims.d[k] * repeat_shape_dims.d[k + 1];
                    // Set dim size to -1 if repeat is being done on dynamic dim
                    collapse_dim = std::max(collapse_dim, (int64_t)-1);
                    collapse_shape_vec.push_back(collapse_dim);
