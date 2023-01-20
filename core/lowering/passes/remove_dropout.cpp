869804530db49b48dede503c481ade65616c7d84
gs-olive <113141689+gs-olive@users.noreply.github.com>
2023-01-20 14:00:14 -0800

    fix: Adapt torch JIT pass for removeDropout
    - Adapt JIT pass to remove dropout to accommodate multiple dropout
    schemas
    - Include additional test cases to verify new removal code

--- core/lowering/passes/remove_dropout.cpp
+++ core/lowering/passes/remove_dropout.cpp
@@ -1,5 +1,4 @@
-#include "torch/csrc/jit/passes/remove_dropout.h"
-#include <torch/csrc/jit/passes/subgraph_rewrite.h>
+#include "torch/csrc/jit/passes/dead_code_elimination.h"
 
 #include "core/util/prelude.h"
 
@@ -8,8 +7,52 @@ namespace core {
 namespace lowering {
 namespace passes {
 
+// Schemas for dropout variants
+const std::unordered_set<c10::Symbol> DropoutNodeKinds = {
+    c10::Symbol::fromQualString("aten::dropout"),
+    c10::Symbol::fromQualString("aten::dropout_"),
+    c10::Symbol::fromQualString("aten::feature_dropout"),
+    c10::Symbol::fromQualString("aten::feature_dropout_"),
+    c10::Symbol::fromQualString("aten::feature_alpha_dropout"),
+    c10::Symbol::fromQualString("aten::feature_alpha_dropout_"),
+};
+
+void removeDropoutInBlock(torch::jit::Block* block) {
+  /*
+  Function adapted from:
+  torch/csrc/jit/passes/remove_dropout.cpp
+
+  Modified for conciseness, documentation, and allowing new variants of dropout operators to be quickly added
+  */
+  std::vector<torch::jit::Node*> dropout_nodes_to_remove;
+
+  for (auto node : block->nodes()) {
+    // Remove dropout for each member block within a node
+    for (auto block : node->blocks()) {
+      removeDropoutInBlock(block);
+    }
+
+    // For each node having a dropout-variant Schema, remove the node
+    if (DropoutNodeKinds.find(node->kind()) != DropoutNodeKinds.end()) {
+      // Extract input and output tensors of dropout operator
+      auto input_value = node->inputs()[0];
+      auto output_value = node->outputs()[0];
+
+      output_value->replaceAllUsesWith(input_value);
+      dropout_nodes_to_remove.push_back(node);
+    }
+  }
+
+  // Delete dropout nodes
+  for (auto del_node : dropout_nodes_to_remove) {
+    del_node->destroy();
+  }
+}
+
 void RemoveDropout(std::shared_ptr<torch::jit::Graph>& graph) {
-  torch::jit::removeDropout(graph);
+  // Remove all instances of dropout variants from graph
+  removeDropoutInBlock(graph->block());
+  torch::jit::EliminateDeadCode(graph);
   LOG_GRAPH("Post remove dropout: " << *graph);
 }
 
