57504d98f736760d25d0cc8a34a1625256879f6a
Naren Dasan <1790613+narendasan@users.noreply.github.com>
2023-02-15 16:46:49 -0600

    feat: build manylinux compatible builds (#1527)
    Signed-off-by: Naren Dasan <naren@narendasan.com>
    Signed-off-by: Naren Dasan <narens@nvidia.com>

--- py/setup.py
+++ py/setup.py
@@ -16,6 +16,13 @@ import subprocess
 import platform
 import warnings
 
+from versions import (
+    __version__,
+    __cuda_version__,
+    __cudnn_version__,
+    __tensorrt_version__,
+)
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 CXX11_ABI = False
@@ -27,11 +34,6 @@ FX_ONLY = False
 RELEASE = False
 
 CI_RELEASE = False
-
-__version__ = "1.4.0dev0"
-__cuda_version__ = "11.6"
-__cudnn_version__ = "8.4"
-__tensorrt_version__ = "8.4"
 
 
 def get_git_revision_short_hash() -> str:
@@ -391,7 +393,7 @@ setup(
     packages=packages if FX_ONLY else find_packages(),
     package_dir=package_dir if FX_ONLY else {},
     classifiers=[
-        "Development Status :: 5 - Stable",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: GPU :: NVIDIA CUDA",
         "License :: OSI Approved :: BSD License",
         "Intended Audience :: Developers",
