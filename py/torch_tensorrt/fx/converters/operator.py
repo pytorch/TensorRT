09a52b99082604279c59b66aa5cb5b9d417db71f
apbose <apbose694@gmail.com>
2023-04-19 08:04:31 -0700

    matmul changes, bmm changes and adding broadcastable

--- py/torch_tensorrt/fx/converters/operator.py
+++ py/torch_tensorrt/fx/converters/operator.py
@@ -16,6 +16,7 @@ from .converter_utils import get_trt_tensor
 from .converter_utils import set_layer_name
 from .converter_utils import get_trt_tensor
 from .converter_utils import broadcast
+from .converter_utils import broadcastable
 from .converter_utils import squeeze_left
 from .converter_utils import dtype_uniform
 from .converter_utils import get_trt_plugin
@@ -1117,7 +1118,17 @@ def add_expand(network, target, kwargs, name):
 
     ranks = len(input_val.shape)
     # TRT does not support different dimension size
-    assert len(shape) == ranks
+    #though this condition is not seen in the case of bmm 
+    # where input_t and shape dimensions are not equal
+    assert len(shape) >= ranks
+    if(len(shape) != ranks):
+            shape_tuple = tuple([0] * len(shape))
+            shape_tensor = get_trt_tensor(network, input_t, f"{name}_shape")
+            input_val, shape_tensor = broadcast(network, input_val, shape_tensor, 
+                                  f"{name}_input_val",
+                                  f"{name}_shape_val")
+            ranks = len(shape)
+            
     shape = [input_val.shape[i] if shape[i] == -1 else shape[i] for i in range(ranks)]
 
     inshape = tuple(input_val.shape)
--- py/torch_tensorrt/fx/test/converters/aten_op/test_matmul_aten.py
+++ py/torch_tensorrt/fx/test/converters/aten_op/test_matmul_aten.py
@@ -6,22 +6,109 @@ from parameterized import param, parameterized
 from torch.testing._internal.common_utils import run_tests
 from torch_tensorrt.fx.tools.common_fx2trt import DispatchTestCase, InputTensorSpec
 
+import torch
+import torch.nn as nn
+import torch_tensorrt.fx.tracer.acc_tracer.acc_ops as acc_ops
+from parameterized import parameterized
+from torch.testing._internal.common_utils import run_tests
+from torch_tensorrt.fx.tools.common_fx2trt import AccTestCase, InputTensorSpec
+
 
 class TestMatMulConverter(DispatchTestCase):
-    def test_matmul(self):
-        class TestModule(torch.nn.Module):
-            def forward(self, x, y):
-                return torch.matmul(x, y)
+    @parameterized.expand(
+        [
+            ("2_2", (2, 3), (3, 2)),
+            ("2_2", (2, 3), (3, 1)),
+            #FIXME torch.ops.aten.mv.default for (2,3), (3,1) - should mv be lowered to mm?
+            # (2,3), (3,) torch.ops.aten.mv.default
+            # Following cases use torch.ops.aten.bmm.defauly 
+            #("4_3", (3,1,3,2), (2,2,3)),
+            #("3_4", (3,1,3,2), (2,2,3)),
+            #("3_4", (2, 2, 3), (3, 1, 3, 3)),
+            #("4_2", (1, 2, 2, 3), (3, 2)),
+        ]
+    )
+    def test_matmul_other_constant(self, _, input_shape, other_shape):
+        class MatMul(nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.other = nn.Parameter(torch.randn(*other_shape))
 
-        inputOne = torch.randn(2, 32)
-        inputTwo = torch.randn(32, 2)
-        inputs = [inputOne, inputTwo]
+            def forward(self, input):
+                return torch.matmul(input, self.other)
+
+        inputs = [torch.randn(*input_shape)]
+        
         self.run_test(
-            TestModule(),
+            MatMul(),
             inputs,
             expected_ops={torch.ops.aten.mm.default},
+            test_explicit_batch_dim=(len(input_shape) >= 1),
         )
 
+    @parameterized.expand(
+        [
+            ("2_2", (2, 3), (3, 2)),
+            ("1_2", (1, 3), (3, 2)),
+            #FIXME torch.ops.aten.mv.default for (2,3), (3,1) - should mv be lowered to mm?
+            # (2,3), (3,) torch.ops.aten.mv.default
+            # Following cases use torch.ops.aten.bmm.defauly 
+            #("4_3", (3,1,3,2), (2,2,3)),
+            #("3_4", (3,1,3,2), (2,2,3)),
+            #("3_4", (2, 2, 3), (3, 1, 3, 3)),
+            #("4_2", (1, 2, 2, 3), (3, 2)),
+            
+        ]
+    )
+    def test_matmul_input_constant(self, _, input_shape, other_shape):
+        class MatMul(nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.input = nn.Parameter(torch.randn(*input_shape))
+
+            def forward(self, other):
+                return torch.matmul(self.input, other)
+
+        inputs = [torch.randn(*other_shape)]
+
+        self.run_test(
+            MatMul(),
+            inputs,
+            expected_ops={torch.ops.aten.mm.default},
+            test_explicit_batch_dim=True 
+            #test_explicit_batch_dim=(len(other_shape) <= 2),
+        )
+
+    @parameterized.expand(
+        [
+            ("2_2", (2, 3), (3, 2)),
+            # ("2_3", (2, 3), (2, 3, 4)),
+            # ("4_4", (2, 2, 2, 3), (2, 1, 3, 2)),
+            # ("4_2", (2, 1, 2, 3), (3, 2)),
+            # ("2_1", (2, 3), (3,)),
+            # ("1_2", (3,), (3, 2)),
+            # ("1_1", (3,), (3,)),
+        ]
+    )
+    def test_matmul(self, _, input_shape, other_shape):
+        class MatMul(nn.Module):
+            def forward(self, input, other):
+                return torch.matmul(input, other)
+
+        inputs = [torch.randn(*input_shape), torch.randn(*other_shape)]
+        test_explicit_batch_dim = not(
+            input_shape[0] == other_shape[0]
+            and len(input_shape) > 2
+            and len(other_shape) > 2
+        )
+        self.run_test(
+            MatMul(),
+            inputs,
+            expected_ops={torch.ops.aten.mm.default},
+            test_explicit_batch_dim=test_explicit_batch_dim,
+        )
+
+    #FIXME: dynamic shape is giving bmm
 
 if __name__ == "__main__":
     run_tests()
