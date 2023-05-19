9311f26f461b6fc72d142ac9efc1fa02775893be
Bo Wang <bowa@nvidia.com>
2023-05-19 01:43:03 +0000

    Fix: fix the bug that uninitialized tensor cannot be found
    This PR fixes the bug that uninitialized tensor cannot be found.
    Specifically, uninitialized value is copied between subgraphs
    previously, this introduces unnecessary complexity and might incur
    bugs. In this PR, this uninitialized value is handled like constants:
    Create one uninitialized value for each subgraph in need.

--- core/partitioning/partitioningctx/PartitioningCtx.cpp
+++ core/partitioning/partitioningctx/PartitioningCtx.cpp
@@ -15,7 +15,8 @@ PartitioningCtx::PartitioningCtx(torch::jit::Block* b, PartitioningInfo info)
 }
 
 void PartitioningCtx::_load_nodes_into_decision_map(torch::jit::Block* b) {
-  if (b->owningNode() && b->owningNode()->kind() == torch::jit::prim::Loop)
+  // won't load nodes if these nodes are in prim::loop or if these nodes are 2-level nested
+  if (b->owningNode() && (b->owningNode()->kind() == torch::jit::prim::Loop || b->owningNode()->owningBlock()->owningNode()))
     return;
 
   original_blocks.push_back(b);
