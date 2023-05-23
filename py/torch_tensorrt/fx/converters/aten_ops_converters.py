2c34c8ebf736347aa55baa25f23d8b45e886883e
Naren Dasan <1790613+narendasan@users.noreply.github.com>
2023-05-23 13:42:27 -0700

    Reorg for converters in (FX Converter Refactor [1/N]) (#1867)
    Signed-off-by: Naren Dasan <naren@narendasan.com>

--- py/torch_tensorrt/fx/converters/aten_ops_converters.py
+++ py/torch_tensorrt/fx/converters/aten_ops_converters.py
@@ -22,6 +22,7 @@ from ..utils import get_dynamic_dims, torch_dtype_from_trt, torch_dtype_to_trt
 
 from .converter_utils import *  # noqa: F403
 import torch_tensorrt.fx.tracer.acc_tracer.acc_utils as acc_utils
+from torch_tensorrt.fx.converters.impl import activation
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
@@ -290,10 +291,14 @@ def aten_ops_relu(
     kwargs: Dict[str, Argument],
     name: str,
 ) -> Union[TRTTensor, Sequence[TRTTensor]]:
-    kwargs_new = {
-        "input": args[0],
-    }
-    return acc_ops_converters.acc_ops_relu(network, target, None, kwargs_new, name)
+
+    return activation.relu(
+        network,
+        target,
+        SourceIR.ATEN,
+        name,
+        args[0],
+    )
 
 
 @tensorrt_converter(torch.ops.aten.sub.Tensor)
---
+++ py/torch_tensorrt/fx/converters/nn_ops_converters.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# @manual=//deeplearning/trt/python:py_tensorrt
+import tensorrt as trt
+import torch
+
+from torch_tensorrt.fx.converter_registry import tensorrt_converter
+from torch_tensorrt.fx.converters.impl import activation
+from torch_tensorrt.fx.converters.converter_utils import SourceIR
+
+
+@tensorrt_converter(torch.nn.functional.relu)
+@tensorrt_converter(torch.nn.modules.activation.ReLU)
+def relu(network, submod, args, kwargs, layer_name):
+    # args/kwargs should have already been normalized to kwargs
+    assert len(args) == 0
+
+    return activation.relu(
+        network=network,
+        target="torch.nn.functional.relu",
+        source_ir=SourceIR.NN,
+        name=layer_name,
+        input_val=kwargs["input"],
+    )
