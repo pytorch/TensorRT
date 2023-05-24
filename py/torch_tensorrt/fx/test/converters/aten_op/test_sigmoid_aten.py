c3c0fe8db4209484afce73e659ca7c332589d97e
Naren Dasan <naren@narendasan.com>
2023-05-23 22:34:36 -0700

    tests: FP16 test for sigmoid
    Signed-off-by: Naren Dasan <naren@narendasan.com>
    Signed-off-by: Naren Dasan <narens@nvidia.com>

--- py/torch_tensorrt/fx/test/converters/aten_op/test_sigmoid_aten.py
+++ py/torch_tensorrt/fx/test/converters/aten_op/test_sigmoid_aten.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.testing._internal.common_utils import run_tests
+from torch_tensorrt.fx.utils import LowerPrecision
 from torch_tensorrt.fx.tools.common_fx2trt import DispatchTestCase, InputTensorSpec
 
 
@@ -46,6 +47,19 @@ class TestSigmoidConverter(DispatchTestCase):
 
         self.run_test_with_dynamic_shape(
             TestModule(), input_specs, expected_ops={torch.ops.aten.sigmoid.default}
+        )
+
+    def test_sigmoid_fp16(self):
+        class TestModule(nn.Module):
+            def forward(self, x):
+                return nn.functional.sigmoid(x)
+
+        inputs = [torch.randn(1, 10)]
+        self.run_test(
+            TestModule(),
+            inputs,
+            expected_ops={torch.ops.aten.sigmoid.default},
+            precision=torch.half,
         )
 
 
