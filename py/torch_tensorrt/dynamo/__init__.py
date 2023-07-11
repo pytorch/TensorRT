e7f4752a6ac66fe7e013052c521f987389398179
George S <113141689+gs-olive@users.noreply.github.com>
2023-07-11 11:24:21 -0700

    fix: Repair import error for legacy TS testing (#2091)

--- py/torch_tensorrt/dynamo/__init__.py
+++ py/torch_tensorrt/dynamo/__init__.py
@@ -1,2 +1,6 @@
-from torch_tensorrt.dynamo import fx_ts_compat
-from .backend import compile
+import torch
+from packaging import version
+
+if version.parse(torch.__version__) >= version.parse("2.1.dev"):
+    from torch_tensorrt.dynamo import fx_ts_compat
+    from .backend import compile
