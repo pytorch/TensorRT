e5b71204a41ffc29d96a6d50ab43cbc215418c75
gs-olive <113141689+gs-olive@users.noreply.github.com>
2023-08-18 14:19:14 -0700

    feat/fix: Update dynamic unsupported implementation
    - Add support for selecting individual argument positions to check and
    expand checking to include symbolic types, which are sometimes passed in
    as arguments

--- py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
+++ py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
@@ -352,28 +352,17 @@ def aten_ops_softmax(
     return impl.normalization.softmax(
         network, target, SourceIR.ATEN, name, args[0], args[1]
     )
-    
-def dynamic_unsupported_split(node: torch.fx.Node) -> bool:
-    # Validate that none of the inputs to the node have Dynamic shapes
-    assert isinstance(
-        node, torch.fx.Node
-    ), "Inputs to validator functions must be FX Nodes"
-
-    if isinstance(node.args[1], torch.fx.Node):
-        if getattr(node.args[1].meta["val"], "_has_symbolic_sizes_strides", True):
-            return False
-    return True
 
 
 @dynamo_tensorrt_converter(
-    torch.ops.aten.split.Tensor, capability_validator=dynamic_unsupported_split
+    torch.ops.aten.split.Tensor, capability_validator=dynamic_unsupported_with_args([1])
 )
 @dynamo_tensorrt_converter(
-    torch.ops.aten.split.sizes, capability_validator=dynamic_unsupported_split
+    torch.ops.aten.split.sizes, capability_validator=dynamic_unsupported_with_args([1])
 )
 @dynamo_tensorrt_converter(
     torch.ops.aten.split_with_sizes.default,
-    capability_validator=dynamic_unsupported_split,
+    capability_validator=dynamic_unsupported_with_args([1]),
 )
 def aten_ops_split(
     network: TRTNetwork,
@@ -381,7 +370,7 @@ def aten_ops_split(
     args: Tuple[Argument, ...],
     kwargs: Dict[str, Argument],
     name: str,
-) -> Union[TRTTensor, Sequence[TRTTensor]]: 
+) -> Union[TRTTensor, Sequence[TRTTensor]]:
     return impl.split.split(
         network,
         target,
