c04a9dae13f58e1014f7c28d920ab1af29a3843b
Zewen (Evan) Li <zewenli@cmu.edu>
2023-09-01 12:12:59 -0700

    feat: support torch.ops.aten.sum.(default and dim_IntList) dynamo converter (#2278)

--- py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
+++ py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
@@ -587,6 +587,26 @@ def aten_ops_amax(
     )
 
 
+@dynamo_tensorrt_converter(torch.ops.aten.sum.default)
+@dynamo_tensorrt_converter(torch.ops.aten.sum.dim_IntList)
+def aten_ops_sum(
+    network: TRTNetwork,
+    target: Target,
+    args: Tuple[Argument, ...],
+    kwargs: Dict[str, Argument],
+    name: str,
+) -> Union[TRTTensor, Sequence[TRTTensor]]:
+    return impl.reduce.sum(
+        network,
+        target,
+        SourceIR.ATEN,
+        name,
+        args[0],
+        args_bounds_check(args, 1, replacement=None),
+        args_bounds_check(args, 2, replacement=False),
+    )
+
+
 @dynamo_tensorrt_converter(torch.ops.aten.exp.default)  # type: ignore[misc]
 def aten_ops_exp(
     network: TRTNetwork,
