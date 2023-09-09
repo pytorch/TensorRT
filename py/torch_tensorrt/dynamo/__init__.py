157bb2dfedfadf033e29aa4926942024bad09631
Dheeraj Peri <peri.dheeraj@gmail.com>
2023-09-08 18:08:18 -0700

    chore: updates
    Signed-off-by: Dheeraj Peri <peri.dheeraj@gmail.com>

--- py/torch_tensorrt/dynamo/__init__.py
+++ py/torch_tensorrt/dynamo/__init__.py
@@ -7,12 +7,10 @@ from packaging import version
 logger = logging.getLogger(__name__)
 
 if version.parse(sanitized_torch_version()) >= version.parse("2.1.dev"):
-    from ._settings import *
-    from ._SourceIR import SourceIR
-    from .aten_tracer import trace
-    from .compile import compile
-    from .conversion import *
-    from .conversion.converter_registry import (
-        DYNAMO_CONVERTERS,
-        dynamo_tensorrt_converter,
-    )
+    from ._settings import *  # noqa: F403
+    from ._SourceIR import SourceIR  # noqa: F403
+    from .aten_tracer import trace  # noqa: F403
+    from .compile import compile  # noqa: F403
+    from .conversion import *  # noqa: F403
+    from .conversion.converter_registry import DYNAMO_CONVERTERS  # noqa: F403
+    from .conversion.converter_registry import dynamo_tensorrt_converter  # noqa: F403
