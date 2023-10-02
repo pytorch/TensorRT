306a9cd1cf73d8f217d6ae4055986aaec11bf97e
Evan Li <zewenl@nvidia.com>
2023-10-02 14:50:22 -0700

    add tests

--- tests/py/dynamo/conversion/test_min_aten.py
+++ tests/py/dynamo/conversion/test_min_aten.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 from parameterized import parameterized
 from torch.testing._internal.common_utils import run_tests
-from torch_tensorrt import Input
 
 from .harness import DispatchTestCase
 
@@ -10,19 +9,107 @@ from .harness import DispatchTestCase
 class TestMinConverter(DispatchTestCase):
     @parameterized.expand(
         [
-            ("2d", (2, 1)),
-            ("3d", (2, 1, 2)),
+            ((3, 2, 4),),
+            ((2, 3, 4, 5),),
+            ((2, 3, 4, 5),),
+            ((6, 7, 5, 4, 5),),
         ]
     )
-    def test_min(self, _, shape):
-        class min(nn.Module):
-            def forward(self, lhs_val, rhs_val):
-                return torch.ops.aten.minimum.default(lhs_val, rhs_val)
+    def test_min_dim_int_default(self, input_shape):
+        class Min(nn.Module):
+            def forward(self, x):
+                return torch.min(x)
 
-        inputs = [torch.randn(shape), torch.randn(shape)]
+        inputs = [torch.randn(*input_shape)]
         self.run_test(
-            min(),
+            Min(),
             inputs,
+            expected_ops={torch.ops.aten.min.default},
+        )
+
+    @parameterized.expand(
+        [
+            ((3, 2, 4), 1, True),
+            ((2, 3, 4, 5), 3, True),
+            ((2, 3, 4, 5), None, False),
+            ((6, 7, 5, 4, 5), 4, False),
+            ((1, 5, 2, 1), -3, False),
+            ((1, 5, 2, 3), -2, True),
+        ]
+    )
+    def test_min_dim_int(self, input_shape, dim, keep_dims):
+        class Min(nn.Module):
+            def forward(self, x):
+                return torch.min(x, dim=dim, keepdim=keep_dims)
+
+        inputs = [torch.randn(*input_shape)]
+        self.run_test(
+            Min(),
+            inputs,
+            expected_ops={torch.ops.aten.min.dim},
+        )
+
+    @parameterized.expand(
+        [
+            ((3, 2, 4), [1], True),
+            ((2, 1, 4, 5), None, True),
+            ((2, 3, 4, 5), [0, 1, 2, 3], False),
+            ((6, 7, 5, 4, 5), [1, 3, 4], False),
+            ((6, 7, 5, 4, 5), [-5, -4, -2], False),
+        ]
+    )
+    def test_min_dim_tuple(self, input_shape, dim, keep_dims):
+        class Min(nn.Module):
+            def forward(self, x):
+                return torch.min(x, dim=dim, keepdim=keep_dims)
+
+        inputs = [torch.randn(*input_shape)]
+        self.run_test(
+            Min(),
+            inputs,
+            expected_ops={torch.ops.aten.min.dim},
+        )
+
+    @parameterized.expand(
+        [
+            ((3, 2, 4), 1, True, torch.int, 0, 5),
+            ((2, 3, 4, 5), None, True, torch.int, -10, 10),
+            ((2, 3, 4, 5), 2, False, torch.int32, -5, 0),
+            ((6, 7, 5, 4, 5), 4, False, torch.int32, -5, 5),
+        ]
+    )
+    def test_min_dim_int_int(self, input_shape, dim, keep_dims, dtype, low, high):
+        class Min(nn.Module):
+            def forward(self, x):
+                return torch.min(x, dim=dim, keepdim=keep_dims)
+
+        inputs = [torch.randint(low, high, input_shape, dtype=dtype)]
+        self.run_test(
+            Min(),
+            inputs,
+            expected_ops={torch.ops.aten.min.dim},
+            check_dtype=False,
+        )
+
+    @parameterized.expand(
+        [
+            ((3, 2, 4), [1], True, torch.int, 0, 5),
+            ((2, 1, 4, 5), [0, 3], True, torch.int, -10, 10),
+            ((2, 3, 4, 5), None, False, torch.int32, -5, 0),
+            ((6, 7, 5, 4, 5), [1, 3, 4], False, torch.int32, -5, 5),
+        ]
+    )
+    def test_min_dim_tuple_int(self, input_shape, dim, keep_dims, dtype, low, high):
+        class Min(nn.Module):
+            def forward(self, x):
+                return torch.min(x, dim=dim, keepdim=keep_dims)
+
+        inputs = [torch.randint(low, high, input_shape, dtype=dtype)]
+        self.run_test(
+            Min(),
+            inputs,
+            expected_ops={torch.ops.aten.min.dim},
+            check_dtype=False,
         )
 
 
