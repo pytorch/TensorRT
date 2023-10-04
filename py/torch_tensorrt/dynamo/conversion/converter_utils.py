30306f859abd0ec213812fb2ff0be71419c9f973
Evan Li <zewenl@nvidia.com>
2023-10-03 19:42:27 -0700

    change to flatten_dims utility

--- py/torch_tensorrt/dynamo/conversion/converter_utils.py
+++ py/torch_tensorrt/dynamo/conversion/converter_utils.py
@@ -17,6 +17,7 @@ from torch_tensorrt.dynamo.conversion.converter_registry import (
 from torch_tensorrt.fx.converters.converter_utils import (
     Frameworks,
     get_axes_for_reduce_op,
+    set_layer_name,
     unified_dtype_converter,
 )
 from torch_tensorrt.fx.types import TRTDataType, TRTTensor
@@ -511,3 +512,32 @@ def to_numpy(
         raise AssertionError(
             f"to_numpy can only be called on None, bool, int, float, np.ndarray, or torch.Tensor, got: {value}"
         )
+
+
+def flatten_dims(
+    ctx: ConversionContext,
+    target: Union[Target, str],
+    source_ir: Optional[SourceIR],
+    name: str,
+    input: Sequence[Union[TRTTensor, torch.Tensor, np.ndarray]],
+    start_dim: int,
+    end_dim: int,
+) -> TRTTensor:
+    shape = input.shape
+    dim_size = len(shape)
+    start_dim = get_positive_dim(start_dim, dim_size)
+    end_dim = get_positive_dim(end_dim, dim_size)
+
+    if not isinstance(input, TRTTensor):
+        input = get_trt_tensor(ctx, input, f"{name}_flatten")
+
+    num_elements = 1
+    for i in range(start_dim, end_dim + 1):
+        num_elements *= shape[i]
+
+    new_shape = tuple(shape[:start_dim]) + (num_elements,) + tuple(shape[end_dim + 1 :])
+
+    layer = ctx.net.add_shuffle(input)
+    layer.reshape_dims = new_shape
+    set_layer_name(layer, target, name, source_ir)
+    return layer.get_output(0)
--- py/torch_tensorrt/dynamo/conversion/impl/shuffle.py
+++ py/torch_tensorrt/dynamo/conversion/impl/shuffle.py
@@ -4,11 +4,7 @@ import numpy as np
 import torch
 from torch.fx.node import Target
 from torch_tensorrt.dynamo.conversion._ConversionContext import ConversionContext
-from torch_tensorrt.dynamo.conversion.converter_utils import (
-    SourceIR,
-    get_positive_dim,
-    get_trt_tensor,
-)
+from torch_tensorrt.dynamo.conversion.converter_utils import SourceIR, get_trt_tensor
 from torch_tensorrt.fx.converters.converter_utils import set_layer_name
 from torch_tensorrt.fx.types import TRTTensor
 
@@ -27,33 +23,3 @@ def reshape(
     layer.reshape_dims = tuple(shape)
     set_layer_name(layer, target, name, source_ir)
     return layer.get_output(0)
-
-
-def flatten(
-    ctx: ConversionContext,
-    target: Union[Target, str],
-    source_ir: Optional[SourceIR],
-    name: str,
-    input: Sequence[Union[TRTTensor, torch.Tensor, np.ndarray]],
-    start_dim: int,
-    end_dim: int,
-) -> TRTTensor:
-    shape = input.shape
-    dim_size = len(shape)
-    start_dim = get_positive_dim(start_dim, dim_size)
-    end_dim = get_positive_dim(end_dim, dim_size)
-
-    if not isinstance(input, TRTTensor):
-        input = get_trt_tensor(ctx, input, f"{name}_flatten")
-
-    num_elements = 1
-    for i in range(start_dim, end_dim + 1):
-        num_elements *= shape[i]
-
-    new_shape = (
-        tuple(shape[:start_dim])
-        + (num_elements,)
-        + (tuple(shape[end_dim + 1 :]) if end_dim + 1 < dim_size else tuple())
-    )
-
-    return reshape(ctx, target, source_ir, name, input, new_shape)
