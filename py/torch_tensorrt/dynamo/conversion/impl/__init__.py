d375d1063c6c8db61fa864164ab26de3cbc34a31
Zewen (Evan) Li <zewenl@nvidia.com>
2023-10-06 14:54:47 -0700

    feat: support flatten and reshape via shuffle_layer (#2354)

--- py/torch_tensorrt/dynamo/conversion/impl/__init__.py
+++ py/torch_tensorrt/dynamo/conversion/impl/__init__.py
@@ -17,6 +17,7 @@ from . import (
     reduce,
     select,
     shape,
+    shuffle,
     slice,
     split,
     squeeze,
--- tests/py/dynamo/conversion/test_converter_utils.py
+++ tests/py/dynamo/conversion/test_converter_utils.py
@@ -1,7 +1,11 @@
 import numpy as np
 import torch
+from parameterized import parameterized
 from torch.testing._internal.common_utils import TestCase, run_tests
-from torch_tensorrt.dynamo.conversion.converter_utils import enforce_tensor_types
+from torch_tensorrt.dynamo.conversion.converter_utils import (
+    enforce_tensor_types,
+    flatten_dims,
+)
 from torch_tensorrt.fx.types import TRTTensor
 
 from ..testing_utilities import DECIMALS_OF_AGREEMENT, lower_graph_testing
@@ -35,6 +39,40 @@ class TestTensorTypeEnforcement(TestCase):
     def test_invalid_invocation_type(self):
         with self.assertRaises(AssertionError):
             enforce_tensor_types({0: (int, bool)})
+
+
+class TestFlattenDimsEnforcement(TestCase):
+    @parameterized.expand(
+        [
+            ((1, 2), 0, 0, (1, 2)),
+            ((1, 2), 0, 1, (2,)),
+            ((2, 3, 4), 1, 2, (2, 12)),
+            ((2, 3, 4), 0, 1, (6, 4)),
+            ((2, 3, 4), -3, 2, (24,)),
+            ((2, 3, 4, 5), 0, -2, (24, 5)),
+            ((2, 3, 4, 5), -4, -1, (120,)),
+        ]
+    )
+    def test_numpy_array(self, input_shape, start_dim, end_dim, true_shape):
+        inputs = np.random.randn(*input_shape)
+        new_shape = flatten_dims(inputs, start_dim, end_dim)
+        self.assertEqual(new_shape, true_shape)
+
+    @parameterized.expand(
+        [
+            ((1, 2), 0, 0, (1, 2)),
+            ((1, 2), 0, 1, (2,)),
+            ((2, 3, 4), 1, 2, (2, 12)),
+            ((2, 3, 4), 0, 1, (6, 4)),
+            ((2, 3, 4), -3, 2, (24,)),
+            ((2, 3, 4, 5), 0, -2, (24, 5)),
+            ((2, 3, 4, 5), -4, -1, (120,)),
+        ]
+    )
+    def test_torch_tensor(self, input_shape, start_dim, end_dim, true_shape):
+        inputs = torch.randn(input_shape)
+        new_shape = flatten_dims(inputs, start_dim, end_dim)
+        self.assertEqual(new_shape, true_shape)
 
 
 if __name__ == "__main__":
