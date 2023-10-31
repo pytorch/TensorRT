f1d777153a8e9069feae84d3a9641e0a14a49c29
George S <113141689+gs-olive@users.noreply.github.com>
2023-10-31 14:21:00 -0700

    cherry-pick: Transformer XL fix to `release/2.1` (#2414)
    Signed-off-by: Torch-TensorRT Github Bot <torch-tensorrt.github.bot@nvidia.com>
    Co-authored-by: Torch-TensorRT Github Bot <torch-tensorrt.github.bot@nvidia.com>

--- tests/py/dynamo/conversion/test_slice_aten.py
+++ tests/py/dynamo/conversion/test_slice_aten.py
@@ -1,38 +1,20 @@
 import torch
 from parameterized import parameterized
 from torch.testing._internal.common_utils import run_tests
+
 from torch_tensorrt import Input
 
 from .harness import DispatchTestCase
 
 
-class TestSelectConverterImplicitBatch(DispatchTestCase):
+class TestSelectConverter(DispatchTestCase):
     @parameterized.expand(
         [
             ("select_dim_start_stop_step", 0, 0, 7, 2),
-        ]
-    )
-    def test_slice(self, _, dim, start, stop, step):
-        class TestModule(torch.nn.Module):
-            def __init__(self):
-                super().__init__()
-
-            def forward(self, input):
-                out = torch.ops.aten.slice.Tensor(input, dim, start, stop, step)
-                return out
-
-        input = [torch.randn(10, 2, 3, 1)]
-        self.run_test(
-            TestModule(),
-            input,
-        )
-
-
-class TestSelectConverterExplicitBatch(DispatchTestCase):
-    @parameterized.expand(
-        [
-            ("select_dim_start_stop_step", 1, 0, 7, 2),
+            ("select_dim_start_stop_step_offset", 1, 0, 7, 2),
             ("select_dim_start_stop_step_exact", 1, 0, 10, 2),
+            ("select_dim_start_stop_step_negatives", -3, -2, -1, 1),
+            ("select_dim_start_stop_step_max_int", 2, 0, 2**63 - 1, 1),
         ]
     )
     def test_slice(self, _, dim, start, stop, step):
