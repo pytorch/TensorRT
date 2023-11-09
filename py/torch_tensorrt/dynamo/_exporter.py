ae733c68008b3442111294a4246494b0a9b17828
Dheeraj Peri <peri.dheeraj@gmail.com>
2023-11-08 17:45:29 -0800

    chore: fix docs for export [release/2.1] (#2448)
    Signed-off-by: Dheeraj Peri <peri.dheeraj@gmail.com>

--- py/torch_tensorrt/dynamo/_exporter.py
+++ py/torch_tensorrt/dynamo/_exporter.py
@@ -53,7 +53,7 @@ def export(
         return exp_program
     else:
         raise ValueError(
-            "Invalid ir : {ir} provided for serialization. Options include torchscript | exported_program"
+            f"Invalid ir : {ir} provided for serialization. Options include torchscript | exported_program"
         )
 
 
