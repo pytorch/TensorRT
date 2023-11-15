0b60aae4522fe7ab04bcf0acbe29ed0d29a9bb1e
Dheeraj Peri <peri.dheeraj@gmail.com>
2023-11-14 23:24:08 -0800

    chore: add sym int converter
    Signed-off-by: Dheeraj Peri <peri.dheeraj@gmail.com>

--- py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
+++ py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
@@ -363,6 +363,22 @@ def aten_ops_sigmoid(
         args[0],
     )
 
+@dynamo_tensorrt_converter(torch.ops.aten.sym_size.int)
+def aten_ops_symsize_int(
+    ctx: ConversionContext,
+    target: Target,
+    args: Tuple[Argument, ...],
+    kwargs: Dict[str, Argument],
+    name: str,
+) -> Union[TRTTensor, Sequence[TRTTensor]]:
+    return impl.shape.shape(
+        ctx,
+        target,
+        SourceIR.ATEN,
+        name,
+        args[0],
+        args_bounds_check(args, 1, None),
+    )
 
 @dynamo_tensorrt_converter(torch.ops.aten.index.Tensor)
 @enforce_tensor_types(
