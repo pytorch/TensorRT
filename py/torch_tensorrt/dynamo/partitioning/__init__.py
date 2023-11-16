93edba415b6e2a7109b935157faedc833315ce6e
Dheeraj Peri <peri.dheeraj@gmail.com>
2023-11-16 08:36:27 -0800

    chore: fix imports
    Signed-off-by: Dheeraj Peri <peri.dheeraj@gmail.com>

--- py/torch_tensorrt/dynamo/partitioning/__init__.py
+++ py/torch_tensorrt/dynamo/partitioning/__init__.py
@@ -2,7 +2,6 @@ from ._adjacency_partitioner import partition as fast_partition
 from ._global_partitioner import partition as global_partition
 from .common import (
     construct_submodule_inputs,
-    fake_tensor_prop,
     get_graph_converter_support,
     get_submod_inputs,
     run_shape_analysis,
