ccc5d3c42b0f851fef1981da4554da2e562094c5
Evan Li <zewenl@nvidia.com>
2023-11-22 17:12:39 -0800

    fix a concat bug

--- py/torch_tensorrt/dynamo/conversion/impl/cat.py
+++ py/torch_tensorrt/dynamo/conversion/impl/cat.py
@@ -24,10 +24,10 @@ def cat(
     trt_inputs = []
     for i, each_input in enumerate(input):
         if not isinstance(each_input, TRTTensor):
-            each_input = get_trt_tensor(ctx, each_input, name + f"_tensor_{i}")
+            each_input = get_trt_tensor(ctx, each_input, f"{name}_tensor_{i}")
         trt_inputs.append(each_input)
     concat_layer = ctx.net.add_concatenation(trt_inputs)
     dim = get_positive_dim(dim, len(input[0].shape))
     concat_layer.axis = dim
-    set_layer_name(concat_layer, target, name + "_gather", source_ir)
+    set_layer_name(concat_layer, target, f"{name}_gather", source_ir)
     return concat_layer.get_output(0)
