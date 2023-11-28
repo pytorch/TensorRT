53401dd89c8229407fad56f6f219872011eff977
Zewen (Evan) Li <zewenl@nvidia.com>
2023-11-28 13:56:38 -0800

    feat: support more elementwise and unary dynamo converters (#2429)

--- tests/py/dynamo/conversion/test_eq_aten.py
+++ tests/py/dynamo/conversion/test_eq_aten.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 from parameterized import parameterized
 from torch.testing._internal.common_utils import run_tests
-from torch_tensorrt import Input
 
 from .harness import DispatchTestCase
 
@@ -10,56 +9,58 @@ from .harness import DispatchTestCase
 class TestEqualConverter(DispatchTestCase):
     @parameterized.expand(
         [
-            ("2d", (2, 1)),
-            ("3d", (2, 1, 2)),
+            ("2d", (5, 3)),
+            ("3d", (5, 3, 2)),
         ]
     )
-    def test_equal_tensor(self, _, shape):
-        class equal(nn.Module):
+    def test_eq_tensor(self, _, shape):
+        class eq(nn.Module):
             def forward(self, lhs_val, rhs_val):
                 return torch.ops.aten.eq.Tensor(lhs_val, rhs_val)
 
-        inputs = [torch.randn(shape), torch.randn(shape)]
+        inputs = [
+            torch.randint(0, 3, shape, dtype=torch.int32),
+            torch.randint(0, 3, shape, dtype=torch.int32),
+        ]
         self.run_test(
-            equal(),
+            eq(),
             inputs,
             output_dtypes=[torch.bool],
         )
 
     @parameterized.expand(
         [
-            ("2d", (2, 1), 1),
-            ("3d", (2, 1, 2), 2.0),
+            ("2d", (5, 3), 1),
+            ("3d", (5, 3, 2), 2.0),
         ]
     )
-    def test_equal_tensor_scalar(self, _, shape, scalar):
-        class equal(nn.Module):
+    def test_eq_tensor_scalar(self, _, shape, scalar):
+        class eq(nn.Module):
             def forward(self, lhs_val):
                 return torch.ops.aten.eq.Tensor(lhs_val, torch.tensor(scalar))
 
-        inputs = [torch.randn(shape)]
+        inputs = [torch.randint(0, 3, shape, dtype=torch.int32)]
         self.run_test(
-            equal(),
+            eq(),
             inputs,
             output_dtypes=[torch.bool],
         )
 
     @parameterized.expand(
         [
-            ("2d", (2, 1), 1),
-            ("3d", (2, 1, 2), 2.0),
+            ("2d", (5, 3), 1),
+            ("3d", (5, 3, 2), 2.0),
         ]
     )
-    def test_equal_scalar(self, _, shape, scalar):
-        class equal(nn.Module):
+    def test_eq_scalar(self, _, shape, scalar):
+        class eq(nn.Module):
             def forward(self, lhs_val):
                 return torch.ops.aten.eq.Scalar(lhs_val, scalar)
 
-        inputs = [torch.randn(shape)]
+        inputs = [torch.randint(0, 3, shape, dtype=torch.int32)]
         self.run_test(
-            equal(),
+            eq(),
             inputs,
-            # expected_ops={torch.ops.aten.eq.Scalar},
             output_dtypes=[torch.bool],
         )
 
--- tests/py/dynamo/conversion/test_lt_aten.py
+++ tests/py/dynamo/conversion/test_lt_aten.py
@@ -2,26 +2,25 @@ import torch
 import torch.nn as nn
 from parameterized import parameterized
 from torch.testing._internal.common_utils import run_tests
-from torch_tensorrt import Input
 
 from .harness import DispatchTestCase
 
 
-class TestGreaterConverter(DispatchTestCase):
+class TestLtConverter(DispatchTestCase):
     @parameterized.expand(
         [
             ("2d", (2, 1)),
             ("3d", (2, 1, 2)),
         ]
     )
-    def test_greater_tensor(self, _, shape):
-        class greater(nn.Module):
+    def test_lt_tensor(self, _, shape):
+        class lt(nn.Module):
             def forward(self, lhs_val, rhs_val):
-                return torch.ops.aten.gt.Tensor(lhs_val, rhs_val)
+                return torch.ops.aten.lt.Tensor(lhs_val, rhs_val)
 
         inputs = [torch.randn(shape), torch.randn(shape)]
         self.run_test(
-            greater(),
+            lt(),
             inputs,
             output_dtypes=[torch.bool],
         )
@@ -32,14 +31,14 @@ class TestGreaterConverter(DispatchTestCase):
             ("3d", (2, 1, 2), 2.0),
         ]
     )
-    def test_greater_tensor_scalar(self, _, shape, scalar):
-        class greater(nn.Module):
+    def test_lt_tensor_scalar(self, _, shape, scalar):
+        class lt(nn.Module):
             def forward(self, lhs_val):
-                return torch.ops.aten.gt.Tensor(lhs_val, torch.tensor(scalar))
+                return torch.ops.aten.lt.Tensor(lhs_val, torch.tensor(scalar))
 
         inputs = [torch.randn(shape)]
         self.run_test(
-            greater(),
+            lt(),
             inputs,
             output_dtypes=[torch.bool],
         )
@@ -50,14 +49,14 @@ class TestGreaterConverter(DispatchTestCase):
             ("3d", (2, 1, 2), 2.0),
         ]
     )
-    def test_greater_scalar(self, _, shape, scalar):
-        class greater(nn.Module):
+    def test_lt_scalar(self, _, shape, scalar):
+        class lt(nn.Module):
             def forward(self, lhs_val):
-                return torch.ops.aten.gt.Scalar(lhs_val, scalar)
+                return torch.ops.aten.lt.Scalar(lhs_val, scalar)
 
         inputs = [torch.randn(shape)]
         self.run_test(
-            greater(),
+            lt(),
             inputs,
             output_dtypes=[torch.bool],
         )
