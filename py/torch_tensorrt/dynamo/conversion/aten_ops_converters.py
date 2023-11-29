a2b5baf5693bcb4a12068dd995415a0de8fd84c0
Evan Li <zewenl@nvidia.com>
2023-11-29 15:56:20 -0800

    feat: support aten.amin dynamo converter

--- py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
+++ py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
@@ -955,19 +955,7 @@ def aten_ops_expand(
     )
 
 
-def amax_param_validator(amax_node: Node) -> bool:
-    if len(amax_node.args) < 2:
-        _LOGGER.debug(
-            f"At least two args input and dim should be provided, but only got {len(amax_node.args)} args."
-        )
-        return False
-
-    return True
-
-
-@dynamo_tensorrt_converter(
-    torch.ops.aten.amax.default, capability_validator=amax_param_validator
-)
+@dynamo_tensorrt_converter(torch.ops.aten.amax.default)
 def aten_ops_amax(
     ctx: ConversionContext,
     target: Target,
@@ -976,6 +964,25 @@ def aten_ops_amax(
     name: str,
 ) -> Union[TRTTensor, Sequence[TRTTensor]]:
     return impl.reduce.amax(
+        ctx,
+        target,
+        SourceIR.ATEN,
+        name,
+        args[0],
+        args_bounds_check(args, 1, replacement=[]),
+        args_bounds_check(args, 2, replacement=False),
+    )
+
+
+@dynamo_tensorrt_converter(torch.ops.aten.amin.default)
+def aten_ops_amin(
+    ctx: ConversionContext,
+    target: Target,
+    args: Tuple[Argument, ...],
+    kwargs: Dict[str, Argument],
+    name: str,
+) -> Union[TRTTensor, Sequence[TRTTensor]]:
+    return impl.reduce.amin(
         ctx,
         target,
         SourceIR.ATEN,
