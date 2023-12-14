818839ae98d5560f357c0aac8c1d197bb957fca9
gs-olive <113141689+gs-olive@users.noreply.github.com>
2023-12-14 15:58:38 -0800

    Build script fix

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -10,7 +10,28 @@ wget https://github.com/bazelbuild/bazelisk/releases/download/v1.17.0/bazelisk-l
     && mv bazelisk-linux-amd64 /usr/bin/bazel \
     && chmod +x /usr/bin/bazel
 
-wget https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/9.2.0/tensorrt-9.2.0.5.linux.x86_64-gnu.cuda-12.2.tar.gz && mkdir -p /usr/tensorrt/lib64 && tar -xzvf tensorrt-9.2.0.5.linux.x86_64-gnu.cuda-12.2.tar.gz -C /usr/tensorrt/lib64 TensorRT-9.2.0.5/targets/x86_64-linux-gnu/lib/stubs --strip-components=5 && export LD_LIBRARY_PATH=/usr/tensorrt/lib64/:$LD_LIBRARY_PATH
+wget https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/9.2.0/tensorrt-9.2.0.5.linux.x86_64-gnu.cuda-12.2.tar.gz
+mkdir -p /usr/tensorrt
+tar -xzvf tensorrt-9.2.0.5.linux.x86_64-gnu.cuda-12.2.tar.gz -C /usr/tensorrt --strip-components=1
+mkdir -p /usr/lib
+cp /usr/tensorrt/lib/* /usr/lib/ || :
+mkdir -p /usr/lib64
+cp /usr/tensorrt/lib/* /usr/lib64/ || :
+mkdir -p /usr/include
+cp /usr/tensorrt/include/* /usr/include/ || :
+
+mkdir -p /usr/lib/aarch64-linux-gnu
+cp /usr/tensorrt/targets/aarch64-linux-gnu/lib/* /usr/lib/aarch64-linux-gnu/ || :
+mkdir -p /usr/include/aarch64-linux-gnu
+cp /usr/tensorrt/targets/aarch64-linux-gnu/include/* /usr/include/aarch64-linux-gnu/ || :
+
+mkdir -p /usr/lib/x86_64-linux-gnu
+cp /usr/tensorrt/targets/x86_64-linux-gnu/lib/* /usr/lib/x86_64-linux-gnu/ || :
+mkdir -p /usr/include/x86_64-linux-gnu
+cp /usr/tensorrt/targets/x86_64-linux-gnu/include/* /usr/include/x86_64-linux-gnu/ || :
+
+rm tensorrt-9.2.0.5.linux.x86_64-gnu.cuda-12.2.tar.gz
+rm -rf /usr/tensorrt
 
 export TORCH_BUILD_NUMBER=$(python -c "import torch, urllib.parse as ul; print(ul.quote_plus(torch.__version__))")
 
