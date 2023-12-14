00bfac71acffedcadf95d7024e510687e6d94fb0
gs-olive <113141689+gs-olive@users.noreply.github.com>
2023-12-14 13:36:36 -0800

    CUDA specification 2

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -3,6 +3,7 @@
 # Install dependencies
 python3 -m pip install pyyaml
 TRT_VERSION=$(python3 -c "import versions; versions.tensorrt_version()")
+CUDA_VERSION=$(python3 -c "import torch; print(torch.version.cuda)")
 CUDA_MAJOR_VERSION=$(python3 -c "import torch; print(torch.version.cuda.split('.')[0])")
 
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
@@ -11,6 +12,10 @@ yum install -y ninja-build gettext tensorrt-${TRT_VERSION}.*.cuda${CUDA_MAJOR_VE
 wget https://github.com/bazelbuild/bazelisk/releases/download/v1.17.0/bazelisk-linux-amd64 \
     && mv bazelisk-linux-amd64 /usr/bin/bazel \
     && chmod +x /usr/bin/bazel
+
+export CUDA_HOME=/usr/local/cuda-${CUDA_VERSION}
+rm -fr /usr/local/cuda
+ln -s /usr/local/cuda-${CUDA_VERSION} /usr/local/cuda
 
 export TORCH_BUILD_NUMBER=$(python -c "import torch, urllib.parse as ul; print(ul.quote_plus(torch.__version__))")
 
