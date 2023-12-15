6f85b90f01e253b0dfe9d424bd8278c33c59121b
gs-olive <113141689+gs-olive@users.noreply.github.com>
2023-12-14 17:50:04 -0800

    deps fix

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -x
+
 # Install dependencies
 python3 -m pip install pyyaml
-TRT_VERSION=$(python3 -c "import versions; versions.tensorrt_version()")
+CUDNN_VERSION=$(python3 -c "import versions; print(versions.__cudnn_version__.split('.')[0])")
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
 yum check-update
-yum install -y ninja-build gettext libcudnn8 libcudnn8-devel
+yum install -y ninja-build gettext libcudnn${CUDNN_VERSION} libcudnn${CUDNN_VERSION}-devel
 wget https://github.com/bazelbuild/bazelisk/releases/download/v1.17.0/bazelisk-linux-amd64 \
     && mv bazelisk-linux-amd64 /usr/bin/bazel \
     && chmod +x /usr/bin/bazel
@@ -38,5 +38,3 @@ export TORCH_BUILD_NUMBER=$(python -c "import torch, urllib.parse as ul; print(u
 
 cat toolchains/ci_workspaces/WORKSPACE.x86_64.release.rhel.tmpl | envsubst > WORKSPACE
 export CI_BUILD=1
-
-find / -name libcudnn*
