744682f7f9672d1a7278b5691d185e23e63fc336
gs-olive <113141689+gs-olive@users.noreply.github.com>
2023-12-27 20:03:24 -0800

    fix: Switch all copies to force cast
    - Fixes the case where engines return multiple copies of the same tensor
    with inserted casts. This behavior is disallowed in TRT
    - Added regression test case
    - This fix should be removed once #2561 is merged

--- py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
+++ py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
@@ -911,7 +911,7 @@ def aten_ops_clone_copy_dtype(
         name,
         args[0],
         kwargs.get("dtype", args[0].dtype),
-        force_layer=False,
+        force_layer=True,
     )
 
 
@@ -1043,7 +1043,7 @@ def aten_ops_sum(
             name,
             sum_,
             kwargs["output_dtype"],
-            force_layer=False,
+            force_layer=True,
         )
     else:
         return sum_
