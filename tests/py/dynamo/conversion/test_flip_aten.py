2dead01511eaccb6b9ccb154baa4896f1f79c964
Evan Li <zewenl@nvidia.com>
2024-01-12 13:24:45 -0800

    add test cases for empty dims

--- tests/py/dynamo/conversion/test_flip_aten.py
+++ tests/py/dynamo/conversion/test_flip_aten.py
@@ -11,6 +11,7 @@ class TestFlipConverter(DispatchTestCase):
         [
             ((3,), [0]),
             ((3,), [-1]),
+            ((3,), []),
             ((3, 3), [0, 1]),
             ((3, 3), [-2, 1]),
             ((2, 3, 4), [0]),
@@ -20,6 +21,7 @@ class TestFlipConverter(DispatchTestCase):
             ((3, 3, 3, 3), [0]),
             ((2, 3, 4, 5), [0, 1, 2, 3]),
             ((2, 3, 4, 5), [-4, 1, -2, 3]),
+            ((2, 3, 4, 5), []),
         ]
     )
     def test_flip(self, shape, dims):
