fa7a52d9a1598ea683199178633bb919eb1ab556
gs-olive <113141689+gs-olive@users.noreply.github.com>
2024-01-19 19:44:29 -0800

    test deletion of cudnn folder

--- packaging/smoke_test.py
+++ packaging/smoke_test.py
@@ -1,3 +1,13 @@
+import os
+import shutil
+import site
+
+python_site_packages = site.getsitepackages()[-1]
+extra_cudnn = os.path.join(python_site_packages, "nvidia", "cudnn")
+print(f"Deleting directory: {extra_cudnn}")
+shutil.rmtree(extra_cudnn)
+
+
 import tensorrt
 import torch
 import torch_tensorrt
