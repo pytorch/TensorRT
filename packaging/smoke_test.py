4b16388e6e886557e3f52d02c926d5af08e46d60
gs-olive <113141689+gs-olive@users.noreply.github.com>
2024-01-26 12:46:11 -0800

    test cudnn overwrite 4

--- packaging/smoke_test.py
+++ packaging/smoke_test.py
@@ -2,6 +2,7 @@ import glob
 import os
 import shutil
 import site
+import subprocess
 
 python_site_packages = site.getsitepackages()[-1]
 extra_cudnn = os.path.join(python_site_packages, "nvidia", "cudnn")
@@ -9,6 +10,14 @@ extra_torch = os.path.join(python_site_packages, "torch", "lib")
 
 for dll in glob.glob(os.path.join(extra_cudnn, "**", "*.dll"), recursive=True):
     shutil.copy(dll, extra_torch)
+
+result = subprocess.run(
+    ['nvidia-smi'],
+    capture_output = True,
+    text = True,
+)
+print(result.stdout)
+print(result.stderr)
 
 
 import tensorrt
