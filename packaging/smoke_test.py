41ae59b7d1e99abbe9473aa0428c74e4b128d431
gs-olive <113141689+gs-olive@users.noreply.github.com>
2024-01-25 17:10:09 -0800

    test cudnn overwrite

--- packaging/smoke_test.py
+++ packaging/smoke_test.py
@@ -1,13 +1,17 @@
+import glob
 import os
 import shutil
 import site
 
 python_site_packages = site.getsitepackages()[-1]
 extra_cudnn = os.path.join(python_site_packages, "nvidia", "cudnn")
-print(f"Deleting directory: {extra_cudnn}")
-shutil.rmtree(extra_cudnn)
+extra_torch = os.path.join(python_site_packages, "torch", "lib")
+
+for dll in glob.glob(os.path.join(extra_cudnn, "**", "*.dll"), recursive=True):
+    shutil.copy(extra_cudnn, extra_torch)
 
 
 # import tensorrt
 import torch
+
 # import torch_tensorrt
