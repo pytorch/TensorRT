97eab11409e5e70bbf2332fd869073d60a1fbc98
gs-olive <113141689+gs-olive@users.noreply.github.com>
2024-01-29 13:16:31 -0800

    Print all DLLS

--- packaging/smoke_test.py
+++ packaging/smoke_test.py
@@ -8,6 +8,9 @@ python_site_packages = site.getsitepackages()[-1]
 extra_cudnn = os.path.join(python_site_packages, "nvidia", "cudnn")
 extra_torch = os.path.join(python_site_packages, "torch", "lib")
 
+print(f"CUDNN DLLS: {list(glob.glob(os.path.join(extra_cudnn, '**', '*.dll'), recursive=True))}")
+print(f"ALL TORCH DLLS: {list(glob.glob(os.path.join(extra_torch, '**', '*.dll'), recursive=True))}")
+
 for dll in glob.glob(os.path.join(extra_cudnn, "**", "*.dll"), recursive=True):
     shutil.copy(dll, extra_torch)
 
