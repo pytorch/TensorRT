f384ffb98c3367fce619fe0700e4962efc1705c8
gs-olive <113141689+gs-olive@users.noreply.github.com>
2024-01-28 23:01:27 -0800

    Update smoke test script

--- packaging/smoke_test.py
+++ packaging/smoke_test.py
@@ -11,10 +11,12 @@ extra_torch = os.path.join(python_site_packages, "torch", "lib")
 for dll in glob.glob(os.path.join(extra_cudnn, "**", "*.dll"), recursive=True):
     shutil.copy(dll, extra_torch)
 
+print(f"Torch CUDA version: {torch.version.cuda}")
+
 result = subprocess.run(
-    ['nvidia-smi'],
-    capture_output = True,
-    text = True,
+    ["nvcc", "--version"],
+    capture_output=True,
+    text=True,
 )
 print(result.stdout)
 print(result.stderr)
@@ -22,5 +24,4 @@ print(result.stderr)
 
 import tensorrt
 import torch
-
 import torch_tensorrt
