a33db08cc9e3eb5bf8778a4a385551e52ff7b390
Evan Li <zewenl@nvidia.com>
2024-01-29 18:15:17 -0800

    test embedding bag on TRT 9

--- py/torch_tensorrt/dynamo/conversion/impl/embedding.py
+++ py/torch_tensorrt/dynamo/conversion/impl/embedding.py
@@ -1,5 +1,5 @@
 import functools
-from typing import Optional, Sequence, Tuple, Union
+from typing import Optional, Tuple
 
 import numpy as np
 import torch
@@ -7,7 +7,7 @@ import torch_tensorrt.dynamo.conversion.impl as impl
 from torch.fx.node import Target
 from torch_tensorrt.dynamo._SourceIR import SourceIR
 from torch_tensorrt.dynamo.conversion._ConversionContext import ConversionContext
-from torch_tensorrt.dynamo.conversion.converter_utils import get_trt_tensor, to_numpy
+from torch_tensorrt.dynamo.conversion.converter_utils import get_trt_tensor
 from torch_tensorrt.fx.converters.converter_utils import set_layer_name
 from torch_tensorrt.fx.types import TRTTensor
 
@@ -47,6 +47,134 @@ def embedding(
     return gather_layer.get_output(0)
 
 
+# def embedding_bag(
+#     ctx: ConversionContext,
+#     target: Target,
+#     source_ir: Optional[SourceIR],
+#     name: str,
+#     weight: TRTTensor,
+#     indices: TRTTensor,
+#     offsets: Union[torch.Tensor, np.ndarray, Sequence[int]],
+#     scale_grad_by_freq: bool,
+#     mode: int,
+#     sparse: bool,
+#     per_sample_weights: Optional[TRTTensor],
+#     include_last_offset: bool,
+# ) -> Tuple[TRTTensor, TRTTensor, TRTTensor, TRTTensor]:
+#     """
+#     This function is for calculating embedding bags.
+
+#     In PyTorch, `offsets` is only used when input is 1D. If input is 2D of shape (B, N),
+#     it will be treated as B bags (sequences) each of fixed length N, and this will return
+#     B values aggregated in a way depending on the mode. `offsets` is ignored and required
+#     to be None in this case.
+
+#     However, according to the schema, `offsets` is required for input with any dimensions.
+#     Accordingly, this function flattens N-D input to 1D and then to calculate embedding bags.
+#     """
+
+#     # TODO: support 2D inputs
+#     # indices = impl.shuffle.reshape(ctx, target, source_ir, f"{name}_reshape_indices", indices, (-1,))
+#     reduce_name = ""
+#     if mode == 0:  # sum
+#         reduce_op = functools.partial(
+#             impl.reduce.sum, ctx=ctx, target=target, source_ir=source_ir
+#         )
+#         reduce_name = "sum"
+#     elif mode == 1:  # mean
+#         reduce_op = functools.partial(
+#             impl.reduce.mean, ctx=ctx, target=target, source_ir=source_ir
+#         )
+#         reduce_name = "mean"
+#     elif mode == 2:  # max
+#         reduce_op = functools.partial(
+#             impl.reduce.max,
+#             ctx=ctx,
+#             target=target,
+#             source_ir=source_ir,
+#             return_indices=False,
+#         )
+#         reduce_name = "max"
+
+#     # calculate embedding
+#     embed = embedding(
+#         ctx,
+#         target,
+#         source_ir,
+#         f"{name}_embedding",
+#         indices,
+#         weight,
+#         scale_grad_by_freq,
+#         sparse,
+#     )
+
+#     # give weights to embedding
+#     if per_sample_weights is not None:
+#         assert (
+#             per_sample_weights.shape == indices.shape
+#         ), f"`per_sample_weights` (shape: {per_sample_weights.shape}) must have exactly the same shape as indices/input (shape: {indices.shape})!"
+#         per_sample_weights = get_trt_tensor(
+#             ctx, per_sample_weights, f"{name}_per_sample_weights", np.float32
+#         )
+#         per_sample_weights = impl.shuffle.reshape(
+#             ctx,
+#             target,
+#             source_ir,
+#             f"{name}_reshape_per_sample_weights",
+#             per_sample_weights,
+#             (-1, 1),
+#         )
+#         embed = impl.elementwise.mul(
+#             ctx,
+#             target,
+#             source_ir,
+#             f"{name}_mul_per_sample_weights",
+#             embed,
+#             per_sample_weights,
+#         )
+
+#     offsets = to_numpy(offsets)
+
+#     if include_last_offset is False:
+#         # add the end index to offsets
+#         offsets = np.append(offsets, indices.shape[0])
+#     else:
+#         # modify the last index of offsets to the end index
+#         # however, pytorch doc says if `include_last_offset` is True, the size of offsets
+#         # is equal to the number of bags + 1. The last element is the size of the input,
+#         # or the ending index position of the last bag (sequence).
+#         offsets[-1] = indices.shape[0]
+
+#     # separately reduce embeddings for different bags
+#     reduced_embed = []
+#     len_offsets = len(offsets)
+#     for i in range(len_offsets - 1):
+#         if offsets[i] < offsets[i + 1]:
+#             sliced_embed = impl.slice.slice_op(
+#                 ctx,
+#                 target,
+#                 source_ir,
+#                 f"{name}_slice_embed_{i}",
+#                 embed,
+#                 0,
+#                 int(offsets[i]),
+#                 int(offsets[i + 1]),
+#                 1,
+#             )
+#             reduced_sliced_embed = reduce_op(
+#                 name=f"{name}_{reduce_name}_{i}",
+#                 input_val=sliced_embed,
+#                 dim=0,
+#                 keepdim=True,
+#             )
+#             reduced_embed.append(reduced_sliced_embed)
+
+#     out = impl.cat.cat(ctx, target, source_ir, f"{name}_cat", reduced_embed, 0)
+#     # out = reduce_op(input_val=embed, dim=1, keepdim=False)  # Note: This implementation doesn't work for N-dim
+
+#     return out, None, None, None
+
+
 def embedding_bag(
     ctx: ConversionContext,
     target: Target,
@@ -54,7 +182,7 @@ def embedding_bag(
     name: str,
     weight: TRTTensor,
     indices: TRTTensor,
-    offsets: Union[torch.Tensor, np.ndarray, Sequence[int]],
+    offsets: TRTTensor,
     scale_grad_by_freq: bool,
     mode: int,
     sparse: bool,
@@ -133,43 +261,49 @@ def embedding_bag(
             per_sample_weights,
         )
 
-    offsets = to_numpy(offsets)
-
-    if include_last_offset is False:
-        # add the end index to offsets
-        offsets = np.append(offsets, indices.shape[0])
+    if include_last_offset:
+        pass
     else:
-        # modify the last index of offsets to the end index
-        # however, pytorch doc says if `include_last_offset` is True, the size of offsets
-        # is equal to the number of bags + 1. The last element is the size of the input,
-        # or the ending index position of the last bag (sequence).
-        offsets[-1] = indices.shape[0]
+        # add the end index to `offsets`
+        offsets = impl.cat.cat(
+            ctx,
+            target,
+            source_ir,
+            f"{name}_concat",
+            [offsets, torch.tensor([indices.shape[0]], dtype=torch.int32)],
+            0,
+        )
 
-    # separately reduce embeddings for different bags
     reduced_embed = []
-    len_offsets = len(offsets)
-    for i in range(len_offsets - 1):
-        if offsets[i] < offsets[i + 1]:
-            sliced_embed = impl.slice.slice_op(
-                ctx,
-                target,
-                source_ir,
-                f"{name}_slice_embed_{i}",
-                embed,
-                0,
-                int(offsets[i]),
-                int(offsets[i + 1]),
-                1,
-            )
-            reduced_sliced_embed = reduce_op(
-                name=f"{name}_{reduce_name}_{i}",
-                input_val=sliced_embed,
-                dim=0,
-                keepdim=True,
-            )
-            reduced_embed.append(reduced_sliced_embed)
+    start = ctx.net.add_gather(
+        offsets, get_trt_tensor(ctx, 0, "tensor_0"), 0
+    ).get_output(0)
+    const_list = get_trt_tensor(ctx, np.array(range(indices.shape[0])), "constant_list")
+
+    for i in range(1, offsets.shape[0]):
+        end = ctx.net.add_gather(
+            offsets, get_trt_tensor(ctx, i, f"tensor_{i}"), 0
+        ).get_output(0)
+        diff = impl.elementwise.sub(
+            ctx, target, source_ir, f"{name}_sub_{i}", end, start
+        )
+
+        slice_layer = ctx.net.add_slice(const_list, (1,), (1,), (1,))
+        slice_layer.set_input(1, start)
+        slice_layer.set_input(2, diff)
+        sliced_indices = slice_layer.get_output(0)
+
+        index_embed = impl.select.index(
+            ctx, target, source_ir, f"{name}_index_{i}", embed, [sliced_indices]
+        )  # np.array([0,1,2,3,4,5])
+        reduced_sliced_embed = reduce_op(
+            name=f"{name}_{reduce_name}_{i}",
+            input_val=index_embed,
+            dim=0,
+            keepdim=True,
+        )
+        reduced_embed.append(reduced_sliced_embed)
+        start = end
 
     out = impl.cat.cat(ctx, target, source_ir, f"{name}_cat", reduced_embed, 0)
-    # out = reduce_op(input_val=embed, dim=1, keepdim=False)  # Note: This implementation doesn't work for N-dim
-
     return out, None, None, None
