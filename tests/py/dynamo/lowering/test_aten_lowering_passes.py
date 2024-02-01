4df76077291ded4c526c75d0f13875119abbe585
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-01-31 16:18:41 -0800

    chore: patch test_hw_compatibility and restrict flash attention test to sm >=86 (#2631)

--- tests/py/dynamo/lowering/test_aten_lowering_passes.py
+++ tests/py/dynamo/lowering/test_aten_lowering_passes.py
@@ -1,9 +1,17 @@
+import unittest
+
 import torch
+import torch_tensorrt
+from torch.testing._internal.common_cuda import PLATFORM_SUPPORTS_FLASH_ATTENTION
 from torch.testing._internal.common_utils import TestCase, run_tests
 
-import torch_tensorrt
-
 from ..testing_utilities import DECIMALS_OF_AGREEMENT, lower_graph_testing
+
+isSM8XDevice = torch.cuda.is_available() and torch.cuda.get_device_capability() in [
+    (8, 6),
+    (8, 7),
+    (8, 9),
+]
 
 
 class TestInputAsOutput(TestCase):
@@ -269,6 +277,10 @@ class TestLowerEfficientAttention(TestCase):
 
 
 class TestLowerFlashAttention(TestCase):
+    @unittest.skipIf(
+        not PLATFORM_SUPPORTS_FLASH_ATTENTION or not isSM8XDevice,
+        "Does not support fused SDPA or not SM86+ hardware",
+    )
     def test_lower_flash_attention(self):
         class FlashAttention(torch.nn.Module):
             def forward(self, q, k, v):
@@ -338,6 +350,10 @@ class TestLowerFlashAttention(TestCase):
         )
         torch._dynamo.reset()
 
+    @unittest.skipIf(
+        not PLATFORM_SUPPORTS_FLASH_ATTENTION or not isSM8XDevice,
+        "Does not support fused SDPA or not SM86+ hardware",
+    )
     def test_flash_attention_converter(self):
         class FlashAttention(torch.nn.Module):
             def forward(self, q, k, v):
