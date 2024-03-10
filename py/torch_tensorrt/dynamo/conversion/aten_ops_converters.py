d667f74957f6d86b979d97bf8bb0efb3477a2865
HolyWu <holywu@gmail.com>
2024-03-10 16:26:02 +0800

    Fix upsample converter not properly registered

--- py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
+++ py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
@@ -2491,6 +2491,7 @@ def aten_ops_pad(
     )
 
 
+@dynamo_tensorrt_converter(torch.ops.aten.upsample_nearest2d.default)
 @dynamo_tensorrt_converter(torch.ops.aten.upsample_nearest2d.vec)
 def upsample_nearest2d(
     ctx: ConversionContext,
@@ -2512,6 +2513,7 @@ def upsample_nearest2d(
     )
 
 
+@dynamo_tensorrt_converter(torch.ops.aten.upsample_bilinear2d.default)
 @dynamo_tensorrt_converter(torch.ops.aten.upsample_bilinear2d.vec)
 def upsample_bilinear2d(
     ctx: ConversionContext,
