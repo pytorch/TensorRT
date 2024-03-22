ea01d54755348ff0037a0f4a45405bf422971512
gs-olive <113141689+gs-olive@users.noreply.github.com>
2024-03-22 16:37:36 -0700

    Bazel install fix

--- packaging/pre_build_script_windows.sh
+++ packaging/pre_build_script_windows.sh
@@ -6,5 +6,5 @@ python -m pip config set global.no-cache-dir true
 python -c "import torch; print('IMPORTED TORCH 2')"
 python -m pip install tensorrt==9.3.0.post12.dev1 tensorrt_libs==9.3.0.post12.dev1 tensorrt_bindings==9.3.0.post12.dev1
 python -c "import torch; print('IMPORTED TORCH 3')"
-choco install bazelisk
+choco install bazelisk -y
 cat toolchains/ci_workspaces/WORKSPACE.win.release.tmpl | envsubst > WORKSPACE
