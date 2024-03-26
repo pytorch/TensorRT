56c6d9d7d52c6ef77b1b97e172a42af649958500
Hoonkyung Cho <chohk88@gmail.com>
2024-03-26 11:42:51 +0900

    chore: remove ITensor convert function

--- py/torch_tensorrt/dynamo/conversion/impl/index.py
+++ py/torch_tensorrt/dynamo/conversion/impl/index.py
@@ -3,7 +3,6 @@ from typing import Optional
 from torch.fx.node import Target
 from torch_tensorrt.dynamo._SourceIR import SourceIR
 from torch_tensorrt.dynamo.conversion._ConversionContext import ConversionContext
-from torch_tensorrt.dynamo.conversion.converter_utils import get_trt_tensor
 from torch_tensorrt.fx.converters.converter_utils import set_layer_name
 from torch_tensorrt.fx.types import TRTTensor
 
@@ -17,11 +16,8 @@ def index_select(
     dim: int,
     index: TRTTensor,
 ) -> TRTTensor:
-    input_tensor = get_trt_tensor(ctx, input, f"{name}_input")
-    index_tensor = get_trt_tensor(ctx, index, f"{name}_index")
-
     # The axis parameter specifies the dimension along which to index.
-    gather_layer = ctx.net.add_gather(input_tensor, index_tensor, axis=dim)
+    gather_layer = ctx.net.add_gather(input, index, axis=dim)
 
     set_layer_name(gather_layer, target, f"{name}_gather", source_ir)
 
