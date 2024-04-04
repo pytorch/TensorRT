3520f35f4553038f60b4b17d4b0fde4372febede
gs-olive <113141689+gs-olive@users.noreply.github.com>
2024-04-03 19:04:40 -0700

    Remove extraneous python references

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -3,8 +3,8 @@ set -eou pipefail
 # Source conda so it's available to the script environment
 source ${BUILD_ENV_FILE}
 /c/Jenkins/Miniconda3/python.exe ${PIP_INSTALL_TORCH} torchvision
-/c/Jenkins/Miniconda3/python.exe python -m pip install pyyaml mpmath==1.3.0
-export TRT_VERSION=$(${CONDA_RUN} python -c "import versions; versions.tensorrt_version()")
-/c/Jenkins/Miniconda3/python.exe python -m pip install /opt/torch-tensorrt-builds/torch_tensorrt*+${CU_VERSION}*.whl tensorrt~=${TRT_VERSION} tensorrt-bindings~=${TRT_VERSION} --extra-index-url=https://pypi.ngc.nvidia.com
+/c/Jenkins/Miniconda3/python.exe -m pip install pyyaml mpmath==1.3.0
+export TRT_VERSION=$(/c/Jenkins/Miniconda3/python.exe -c "import versions; versions.tensorrt_version()")
+/c/Jenkins/Miniconda3/python.exe -m pip install /opt/torch-tensorrt-builds/torch_tensorrt*+${CU_VERSION}*.whl tensorrt~=${TRT_VERSION} tensorrt-bindings~=${TRT_VERSION} --extra-index-url=https://pypi.ngc.nvidia.com
 
 echo -e "Running test script";
