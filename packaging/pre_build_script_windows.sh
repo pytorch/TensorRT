4928fa3cb9f5d3d59b7726a046a5ca19d031754b
HolyWu <holywu@gmail.com>
2024-04-08 15:39:13 +0800

    Upgrade setuptools and wheel

--- packaging/pre_build_script_windows.sh
+++ packaging/pre_build_script_windows.sh
@@ -1,4 +1,4 @@
-python -m pip install pyyaml packaging numpy
+python -m pip install -U numpy packaging pyyaml setuptools wheel
 python -c "import torch; print('IMPORTED TORCH 1')"
 python -m pip config set global.index-url "https://pypi.org/simple https://download.pytorch.org/whl/nightly/cu118 https://download.pytorch.org/whl/nightly/cu121"
 python -m pip config set global.extra-index-url "https://pypi.nvidia.com"
@@ -6,9 +6,9 @@ python -m pip config set global.no-cache-dir true
 python -c "import torch; print('IMPORTED TORCH 2')"
 python -m pip install tensorrt==9.3.0.post12.dev1 tensorrt_libs==9.3.0.post12.dev1 tensorrt_bindings==9.3.0.post12.dev1
 python -c "import torch; print('IMPORTED TORCH 3')"
+
 choco install bazelisk -y
-pwd
-ls
+
 cat ./toolchains/ci_workspaces/WORKSPACE.win.release.tmpl | envsubst > WORKSPACE
 
 # curl -o cuda_12.1.0_windows_network.exe https://developer.download.nvidia.com/compute/cuda/12.1.0/network_installers/cuda_12.1.0_windows_network.exe
