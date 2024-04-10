608a6d274abea87d0939bb0917ae7e42c9efb605
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-04-10 08:20:39 -0700

    chore: updates

--- packaging/post_build_script.sh
+++
@@ -1,3 +0,0 @@
-export LD_LIBRARY_PATH=/opt/torch-tensorrt-builds/TensorRT-10.0.0.6/lib:$LD_LIBRARY_PATH
-echo $LD_LIBRARY_PATH
-python -m pip install /opt/torch-tensorrt-builds/TensorRT-10.0.0.6/python/tensorrt-10.0.0b6-cp38-none-linux_x86_64.whl
