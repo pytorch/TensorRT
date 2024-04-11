7120d75014b5f90ddccd6db70701e049c8903b61
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-04-11 14:49:32 -0700

    feat: Implement symbolic shape propagation, sym_size converter (#2473)
    Signed-off-by: Dheeraj Peri <peri.dheeraj@gmail.com>

--- py/torch_tensorrt/dynamo/partitioning/__init__.py
+++ py/torch_tensorrt/dynamo/partitioning/__init__.py
@@ -1,3 +1,7 @@
 from ._adjacency_partitioner import partition as fast_partition
 from ._global_partitioner import partition as global_partition
-from .common import get_graph_converter_support, get_submod_inputs, run_shape_analysis
+from .common import (
+    construct_submodule_inputs,
+    get_graph_converter_support,
+    run_shape_analysis,
+)
