ba286bdab40c0d0aa418b609ef129306a555b241
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-04-18 16:40:24 -0700

    chore: add fp8 test

--- tests/py/dynamo/models/test_models_export.py
+++ tests/py/dynamo/models/test_models_export.py
@@ -180,3 +180,45 @@ def test_resnet18_half(ir):
 
     # Clean up model env
     torch._dynamo.reset()
+
+
+@pytest.mark.unit
+def test_base_fp8(ir):
+    class SimpleNetwork(nn.Module):
+        def __init__(self):
+            super(SimpleNetwork, self).__init__()
+            self.linear1 = nn.Linear(in_features=10, out_features=5)
+            self.linear2 = nn.Linear(in_features=5, out_features=1)
+
+        def forward(self, x):
+            x = self.linear1(x)
+            x = torch.nn.ReLU()(x)
+            x = self.linear2(x)
+            return x
+
+    import ammo.torch.quantization as atq
+    from ammo.torch.quantization.torch_export import export_torch_mode
+
+    def calibrate_loop(model):
+        """Simple calibration function for testing."""
+        model(input_tensor)
+
+    input_tensor = torch.randn(1, 10).cuda()
+    model = SimpleNetwork().eval().cuda()
+
+    with torch.no_grad():
+        quant_cfg = atq.FP8_DEFAULT_CFG
+        atq.quantize(model, quant_cfg, forward_loop=calibrate_loop)
+        # model has FP8 qdq nodes at this point
+        output_pyt = model(input_tensor)
+
+        with export_torch_mode():
+            exp_program = torch.export.export(model, (input_tensor,))
+            trt_model = torch_tensorrt.dynamo.compile(
+                exp_program,
+                inputs=[input_tensor],
+                enabled_precisions={torch.float8_e4m3fn},
+                min_block_size=1,
+            )
+            outputs_trt = trt_model(input_tensor)
+            assert torch.allclose(output_pyt, outputs_trt, rtol=1e-3, atol=1e-2)
