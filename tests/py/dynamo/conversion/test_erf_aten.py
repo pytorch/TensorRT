dee9aa02d941be5312f7e3cceef5ad4f887aefe1
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-04-19 09:23:09 -0700

    chore: updates

--- tests/py/dynamo/conversion/test_erf_aten.py
+++ tests/py/dynamo/conversion/test_erf_aten.py
@@ -22,7 +22,7 @@ class TestErfConverter(DispatchTestCase):
                 return torch.ops.aten.erf.default(input)
 
         inputs = [torch.randn(x, dtype=type)]
-        self.run_test(erf(), inputs, precision=type, output_dtypes=[type])
+        self.run_test(erf(), inputs, precision=type)
 
     @parameterized.expand(
         [
