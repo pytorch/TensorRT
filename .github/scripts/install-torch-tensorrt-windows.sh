05880524af1d653810ce50d86c9b101841e748d3
gs-olive <113141689+gs-olive@users.noreply.github.com>
2024-04-18 17:43:58 -0700

    Switch to TRT 10 in installer

--- .github/scripts/install-torch-tensorrt-windows.sh
+++ .github/scripts/install-torch-tensorrt-windows.sh
@@ -1,6 +1,6 @@
 source "${BUILD_ENV_FILE}"
 ${CONDA_RUN} ${PIP_INSTALL_TORCH} torchvision
 ${CONDA_RUN} pip install pyyaml mpmath==1.3.0
-${CONDA_RUN} pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl tensorrt==9.3.0.post12.dev1 tensorrt-bindings==9.3.0.post12.dev1 tensorrt-libs==9.3.0.post12.dev1 --extra-index-url https://pypi.nvidia.com
+${CONDA_RUN} pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl tensorrt==10.0.0b6 --extra-index-url https://pypi.nvidia.com --force-reinstall --no-cache
 
 echo -e "Running test script";
