c028b67164006ebdc7e49efe43bea90fb1f8ddf2
HolyWu <holywu@gmail.com>
2024-04-21 02:51:23 +0800

    Install torch and tensorrt according to desired_cuda in matrix

--- .github/scripts/install-torch-tensorrt-windows.sh
+++ .github/scripts/install-torch-tensorrt-windows.sh
@@ -1,9 +1,9 @@
 set -eou pipefail
 source "${BUILD_ENV_FILE}"
 # ${CONDA_RUN} ${PIP_INSTALL_TORCH} torchvision
-${CONDA_RUN} pip install torch torchvision --index-url https://download.pytorch.org/whl/test/cu121
+${CONDA_RUN} pip install torch torchvision --index-url https://download.pytorch.org/whl/test/${CU_VERSION}
 ${CONDA_RUN} pip install pyyaml mpmath==1.3.0
-${CONDA_RUN} pip install tensorrt==10.0.0b6 tensorrt-cu12-bindings==10.0.0b6 tensorrt-cu12-libs==10.0.0b6 --extra-index-url https://pypi.nvidia.com
+${CONDA_RUN} pip install tensorrt==10.0.0b6 tensorrt-${CU_VERSION::4}-bindings==10.0.0b6 tensorrt-${CU_VERSION::4}-libs==10.0.0b6 --extra-index-url https://pypi.nvidia.com
 ${CONDA_RUN} pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
 
 echo -e "Running test script";
--- packaging/pre_build_script_windows.sh
+++ packaging/pre_build_script_windows.sh
@@ -1,6 +1,6 @@
 python -m pip install -U numpy packaging pyyaml setuptools wheel
-python -m pip install torch==2.3.0 --index-url https://download.pytorch.org/whl/test/cu121
-python -m pip install tensorrt==10.0.0b6 tensorrt-cu12-bindings==10.0.0b6 tensorrt-cu12-libs==10.0.0b6 --extra-index-url https://pypi.nvidia.com
+python -m pip install torch==2.3.0 --index-url https://download.pytorch.org/whl/test/${CU_VERSION}
+python -m pip install tensorrt==10.0.0b6 tensorrt-${CU_VERSION::4}-bindings==10.0.0b6 tensorrt-${CU_VERSION::4}-libs==10.0.0b6 --extra-index-url https://pypi.nvidia.com
 
 choco install bazelisk -y
 
