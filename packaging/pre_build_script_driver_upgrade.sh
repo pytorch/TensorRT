422423c998203fd89cfab049c35b84c148916818
HolyWu <holywu@gmail.com>
2024-04-20 21:44:42 +0800

    Delete usused pre_build_script_driver_upgrade.sh

--- packaging/pre_build_script_driver_upgrade.sh
+++
@@ -1,2 +0,0 @@
-curl -so cuda_12.1.0_windows_network.exe https://developer.download.nvidia.com/compute/cuda/12.1.0/network_installers/cuda_12.1.0_windows_network.exe
-./cuda_12.1.0_windows_network.exe -s nvcc_12.1 Display.Driver -n
