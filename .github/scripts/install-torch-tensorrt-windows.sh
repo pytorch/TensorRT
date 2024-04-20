bffcfb2e4b8138f9d6b9cca1ba4a46334e9f6689
HolyWu <holywu@gmail.com>
2024-04-20 23:41:41 +0800

    Install tensorrt-bindings and tensorrt-libs

--- .github/scripts/install-torch-tensorrt-windows.sh
+++ .github/scripts/install-torch-tensorrt-windows.sh
@@ -1,7 +1,8 @@
+set -eou pipefail
 source "${BUILD_ENV_FILE}"
-# ${CONDA_RUN} ${PIP_INSTALL_TORCH} torchvision --extra-index-url https://download.pytorch.org/whl/test/cu121 --extra-index-url https://download.pytorch.org/whl/test/cu118
+${CONDA_RUN} ${PIP_INSTALL_TORCH} torchvision
 ${CONDA_RUN} pip install pyyaml mpmath==1.3.0
-${CONDA_RUN} pip install tensorrt==10.0.0b6 --extra-index-url https://pypi.nvidia.com --no-cache --force-reinstall
-${CONDA_RUN} pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl torch torchvision --extra-index-url https://download.pytorch.org/whl/test/cu121 --extra-index-url https://download.pytorch.org/whl/test/cu118
+${CONDA_RUN} pip install tensorrt==10.0.0b6 tensorrt-cu12-bindings==10.0.0b6 tensorrt-cu12-libs==10.0.0b6 --extra-index-url https://pypi.nvidia.com
+${CONDA_RUN} pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
 
 echo -e "Running test script";
