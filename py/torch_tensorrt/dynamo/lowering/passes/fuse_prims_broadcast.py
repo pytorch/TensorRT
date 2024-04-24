c1574be78645cead5e123f3cfb0722fce2ee7a2b
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-04-24 18:49:22 +0000

    chore: updates

--- py/torch_tensorrt/dynamo/lowering/passes/fuse_prims_broadcast.py
+++ py/torch_tensorrt/dynamo/lowering/passes/fuse_prims_broadcast.py
@@ -2,7 +2,8 @@ import logging
 from typing import Sequence
 
 import torch
-from torch.fx.passes.shape_prop import ShapeProp
+
+# from torch.fx.passes.shape_prop import ShapeProp
 from torch_tensorrt.dynamo.lowering.passes.pass_utils import (
     clean_up_graph_after_modifications,
 )
@@ -17,15 +18,15 @@ def fuse_prims_broadcast(
     """Fuses prim nodes which are effectively the ATen equivalents with keep_dim=True"""
     modified_graph = False
 
-    # Propagate shapes through the graph to determine if broadcast can be resolved
-    try:
-        ShapeProp(gm).propagate(*sample_inputs)
-    except (RuntimeError, AssertionError):
-        logger.warning(
-            "Shape Propagation Failed on Graph, skipping fuse_prims_broadcast lowering pass",
-            exc_info=True,
-        )
-        return gm
+    # # Propagate shapes through the graph to determine if broadcast can be resolved
+    # try:
+    #     ShapeProp(gm).propagate(*sample_inputs)
+    # except (RuntimeError, AssertionError):
+    #     logger.warning(
+    #         "Shape Propagation Failed on Graph, skipping fuse_prims_broadcast lowering pass",
+    #         exc_info=True,
+    #     )
+    #     return gm
 
     for node in gm.graph.nodes:
         # If the node is a sum prims operator, with broadcast_in_dim being the only consumer
