4762a6caa073d03efadca4db9415676317233dc1
kee hyun an <keehyuna@nvidia.com>
2024-05-20 06:46:33 +0000

    fix: Handle dynamic shapes in where ops

--- tests/py/dynamo/conversion/test_where_aten.py
+++ tests/py/dynamo/conversion/test_where_aten.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from parameterized import parameterized
 from torch.testing._internal.common_utils import run_tests
+from torch_tensorrt import Input
 
 from .harness import DispatchTestCase
 
@@ -75,6 +76,86 @@ class TestWhereConverter(DispatchTestCase):
             Where(),
             (condition,),
         )
+
+    # shape, min shape range, opt shape range, max shape range
+    @parameterized.expand(
+        [
+            (
+                "3d_condition_3d_xshape_3d_yshape",
+                (-1, -1, -1),
+                (1, 1, 1),
+                (1, 2, 3),
+                (3, 3, 3),
+                (-1, -1, -1),
+                (1, 1, 1),
+                (1, 2, 3),
+                (3, 3, 3),
+                (-1, -1, -1),
+                (1, 1, 1),
+                (1, 2, 3),
+                (3, 3, 3),
+            ),
+            (
+                "1d_condition_3d_xshape_2d_yshape",
+                (-1),
+                (1,),
+                (2,),
+                (4,),
+                (-1, -1, -1),
+                (1, 1, 1),
+                (3, 2, 2),
+                (3, 2, 4),
+                (
+                    -1,
+                    -1,
+                ),
+                (1, 1),
+                (2, 2),
+                (2, 4),
+            ),
+            (
+                "2d_condition_3d_xshape_2d_yshape",
+                (-1, -1),
+                (4, 1),
+                (4, 2),
+                (5, 4),
+                (-1, -1, -1),
+                (1, 1, 1),
+                (3, 1, 2),
+                (3, 1, 4),
+                (
+                    -1,
+                    -1,
+                ),
+                (4, 1),
+                (4, 2),
+                (5, 4),
+            ),
+        ]
+    )
+    def test_with_dynamic_shape(self, *args):
+        class Where(nn.Module):
+            def forward(self, condition, x, y):
+                return torch.ops.aten.where.self(condition, x, y)
+
+        input_specs = [
+            Input(
+                shape=args[1],
+                dtype=torch.bool,
+                shape_ranges=[(args[2], args[3], args[4])],
+            ),
+            Input(
+                shape=args[5],
+                dtype=torch.float32,
+                shape_ranges=[(args[6], args[7], args[8])],
+            ),
+            Input(
+                shape=args[9],
+                dtype=torch.float32,
+                shape_ranges=[(args[10], args[11], args[12])],
+            ),
+        ]
+        self.run_test_with_dynamic_shape(Where(), input_specs)
 
 
 if __name__ == "__main__":
