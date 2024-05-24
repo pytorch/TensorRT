0d2021dcd4278e65f2860f23a78015eb60b3e2c1
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-05-24 13:58:05 -0700

    chore: updates

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -2,7 +2,7 @@
 set -eou pipefail
 # Source conda so it's available to the script environment
 source ${BUILD_ENV_FILE}
-${CONDA_RUN} ${PIP_INSTALL_TORCH} torchvision
+${CONDA_RUN} ${PIP_INSTALL_TORCH} torchvision==0.18.0
 ${CONDA_RUN} python -m pip install pyyaml mpmath==1.3.0
 export TRT_VERSION=$(${CONDA_RUN} python -c "import versions; versions.tensorrt_version()")
 
