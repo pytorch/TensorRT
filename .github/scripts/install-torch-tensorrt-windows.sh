99d4ae44b417f8922df943431189437760d673e4
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-05-27 11:01:00 -0700

    chore: updates

--- .github/scripts/install-torch-tensorrt-windows.sh
+++ .github/scripts/install-torch-tensorrt-windows.sh
@@ -1,12 +1,22 @@
 set -eou pipefail
 source "${BUILD_ENV_FILE}"
 
-# Install test index version of Torch and Torchvision
-${CONDA_RUN} ${PIP_INSTALL_TORCH} torchvision
+# Install pyyaml first to parse dev_dep_versions.yml in versions.py
 ${CONDA_RUN} pip install pyyaml mpmath==1.3.0
 
+# Install stable versions of Torch and Torchvision since this is a release branch.
+# For main branches, the INDEX_URL will always point to nightly and for RC releases,
+# it will contain test tag in the url.
+export TORCH_VERSION_CI=$(${CONDA_RUN} python -c "import versions; versions.torch_version()")
+export TORCHVISION_VERSION_CI=$(${CONDA_RUN} python -c "import versions; versions.torchvision_version()")
+export TENSORRT_VERSION_CI=$(${CONDA_RUN} python -c "import versions; versions.tensorrt_version()")
+export INDEX_URL_CI=$(${CONDA_RUN} python -c "import versions; versions.index_url()")
+
+# Install appropriate torch and torchvision versions for the platform
+${CONDA_RUN} pip install torch==${TORCH_VERSION_CI} torchvision==${TORCHVISION_VERSION_CI} --index-url ${INDEX_URL}
+
 # Install TRT 10 from PyPi
-${CONDA_RUN} pip install tensorrt==10.0.0b6 tensorrt-${CU_VERSION::4}-bindings==10.0.0b6 tensorrt-${CU_VERSION::4}-libs==10.0.0b6 --extra-index-url https://pypi.nvidia.com
+${CONDA_RUN} pip install tensorrt==${TENSORRT_VERSION_CI}
 
 # Install pre-built Torch-TRT
 ${CONDA_RUN} pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -2,11 +2,24 @@
 set -eou pipefail
 # Source conda so it's available to the script environment
 source ${BUILD_ENV_FILE}
-${CONDA_RUN} ${PIP_INSTALL_TORCH} torchvision
+
+# Install pyyaml first to parse dev_dep_versions.yml in versions.py
 ${CONDA_RUN} python -m pip install pyyaml mpmath==1.3.0
-export TRT_VERSION=$(${CONDA_RUN} python -c "import versions; versions.tensorrt_version()")
+
+# Install stable versions of Torch and Torchvision since this is a release branch.
+# For main branches, the INDEX_URL will always point to nightly and for RC releases,
+# it will contain test tag in the url.
+export TORCH_VERSION_CI=$(${CONDA_RUN} python -c "import versions; versions.torch_version()")
+export TORCHVISION_VERSION_CI=$(${CONDA_RUN} python -c "import versions; versions.torchvision_version()")
+export TENSORRT_VERSION_CI=$(${CONDA_RUN} python -c "import versions; versions.tensorrt_version()")
+export INDEX_URL_CI=$(${CONDA_RUN} python -c "import versions; versions.index_url()")
+
+# Install appropriate torch and torchvision versions for the platform
+${CONDA_RUN} pip install torch==${TORCH_VERSION_CI} torchvision==${TORCHVISION_VERSION_CI} --index-url ${INDEX_URL}
+
+# Install TRT 10 from PyPi
+${CONDA_RUN} pip install tensorrt==${TENSORRT_VERSION_CI}
 
 # Install Torch-TensorRT
-${CONDA_RUN} python -m pip install /opt/torch-tensorrt-builds/torch_tensorrt*+${CU_VERSION}*.whl tensorrt~=${TRT_VERSION} --extra-index-url=https://pypi.ngc.nvidia.com
-
+${CONDA_RUN} python -m pip install /opt/torch-tensorrt-builds/torch_tensorrt*+${CU_VERSION}*.whl
 echo -e "Running test script";
