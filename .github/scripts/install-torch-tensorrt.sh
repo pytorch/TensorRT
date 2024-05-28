d99989d5f76eaddb1f1c7015d1e053027a567073
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-05-27 20:45:54 -0700

    chore: updates

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -2,7 +2,7 @@
 set -eou pipefail
 # Source conda so it's available to the script environment
 source ${BUILD_ENV_FILE}
-export EXTRA_INDEX_URL="https://download.pytorch.org/whl/nightly/${CU_VERSION}"
+export EXTRA_INDEX_URL="https://download.pytorch.org/whl/test/${CU_VERSION}"
 
 # Install all the dependencies required for Torch-TensorRT
 ${CONDA_RUN} pip install --pre -r ${PWD}/tests/py/requirements.txt --use-deprecated=legacy-resolver --extra-index-url=${EXTRA_INDEX_URL}
