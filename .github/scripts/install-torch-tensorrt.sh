65c5c3e24dc9ea7ba970151ed1c67319b47559ec
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-05-27 20:42:24 -0700

    chore: updates

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -2,28 +2,12 @@
 set -eou pipefail
 # Source conda so it's available to the script environment
 source ${BUILD_ENV_FILE}
+export EXTRA_INDEX_URL="https://download.pytorch.org/whl/nightly/${CU_VERSION}"
 
-# Install pyyaml first to parse dev_dep_versions.yml in versions.py
-${CONDA_RUN} python -m pip install pyyaml
-
-# Install stable versions of Torch and Torchvision since this is a release branch.
-# For main branches, the INDEX_URL will always point to nightly and for RC releases,
-# it will contain test tag in the url.
-export TORCH_VERSION_CI=$(${CONDA_RUN} python -c "import versions; versions.torch_version()")
-export TORCHVISION_VERSION_CI=$(${CONDA_RUN} python -c "import versions; versions.torchvision_version()")
-export TENSORRT_VERSION_CI=$(${CONDA_RUN} python -c "import versions; versions.tensorrt_version()")
-export INDEX_URL_CI=$(${CONDA_RUN} python -c "import versions; versions.index_url()")
-
-# Install appropriate torch and torchvision versions for the platform
-${CONDA_RUN} pip install torch==${TORCH_VERSION_CI} torchvision==${TORCHVISION_VERSION_CI} --index-url ${INDEX_URL_CI}
-
-# Install TRT 10 from PyPi
-${CONDA_RUN} pip install tensorrt==${TENSORRT_VERSION_CI}
+# Install all the dependencies required for Torch-TensorRT
+${CONDA_RUN} pip install --pre -r ${PWD}/tests/py/requirements.txt --use-deprecated=legacy-resolver --extra-index-url=${EXTRA_INDEX_URL}
 
 # Install Torch-TensorRT
 ${CONDA_RUN} pip install /opt/torch-tensorrt-builds/torch_tensorrt*+${CU_VERSION}*.whl
-
-# Install additional libraries like timm/transformers which are used during testing
-${CONDA_RUN} pip install --pre -r tests/py/requirements.txt --use-deprecated=legacy-resolver
 
 echo -e "Running test script";
--- packaging/pre_build_script_windows.sh
+++ packaging/pre_build_script_windows.sh
@@ -1,7 +1,7 @@
 python -m pip install -U numpy packaging pyyaml setuptools wheel
 
 # Install TRT 10 from PyPi
-python -m pip install tensorrt==10.0.0b6 tensorrt-${CU_VERSION::4}-bindings==10.0.0b6 tensorrt-${CU_VERSION::4}-libs==10.0.0b6 --extra-index-url https://pypi.nvidia.com
+python -m pip install tensorrt==10.0.1 --extra-index-url https://pypi.nvidia.com
 
 choco install bazelisk -y
 
