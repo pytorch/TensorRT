5346a4542c4be26ac7873943dcafe0b2cec01d15
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-05-29 11:54:35 -0700

    chore: updates

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -8,7 +8,7 @@ export PLATFORM=$(${CONDA_RUN} python -c "import sys; print(sys.platform)")
 ${CONDA_RUN} pip install --pre -r ${PWD}/tests/py/requirements.txt --use-deprecated=legacy-resolver --extra-index-url=${EXTRA_INDEX_URL}
 
 # Install Torch-TensorRT via pre-built wheels. On windows, the location of wheels is not fixed.
-if ${PLATFORM} == "win32"; then
+if [[ "$PLATFORM" == "win32" ]]; then
     ${CONDA_RUN} pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
 else
     ${CONDA_RUN} pip install /opt/torch-tensorrt-builds/torch_tensorrt*.whl
