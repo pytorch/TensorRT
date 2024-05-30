aa1e4946cc25cc689144f8b625c0a37c62bcfb58
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-05-29 23:47:15 -0700

    chore: updates

---
+++ .github/scripts/install-torch-tensorrt-windows.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eou pipefail
+# Source conda so it's available to the script environment
+source ${BUILD_ENV_FILE}
+# Install PyTorch and torchvision from index
+${CONDA_RUN} ${PIP_INSTALL_TORCH} torchvision
+export PLATFORM=$(${CONDA_RUN} python -c "import sys; print(sys.platform)")
+# Install all the dependencies required for Torch-TensorRT
+${CONDA_RUN} pip install --pre -r ${PWD}/tests/py/requirements.txt --use-deprecated=legacy-resolver
+
+# Install Torch-TensorRT via pre-built wheels. On windows, the location of wheels is not fixed.
+${CONDA_RUN} pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
+
+echo -e "Running test script";
