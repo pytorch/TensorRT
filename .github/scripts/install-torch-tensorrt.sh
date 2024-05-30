339d3e9932716d7e16396922b4dcba2ef6a6b663
HolyWu <holywu@gmail.com>
2024-05-30 15:47:15 +0800

    Unify install-torch-tensorrt script for Linux and Windows

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -1,13 +1,16 @@
-#!/usr/bin/env bash
 set -eou pipefail
-# Source conda so it's available to the script environment
-source ${BUILD_ENV_FILE}
-export EXTRA_INDEX_URL="https://download.pytorch.org/whl/nightly/${CU_VERSION}"
+
+EXTRA_INDEX_URL=https://download.pytorch.org/whl/${CHANNEL}/${CU_VERSION}
+PLATFORM=$(python -c "import sys; print(sys.platform)")
 
 # Install all the dependencies required for Torch-TensorRT
-${CONDA_RUN} pip install --pre -r ${PWD}/tests/py/requirements.txt --use-deprecated=legacy-resolver --extra-index-url=${EXTRA_INDEX_URL}
+pip install --pre -r ${PWD}/tests/py/requirements.txt --use-deprecated=legacy-resolver --extra-index-url=${EXTRA_INDEX_URL}
 
 # Install Torch-TensorRT
-${CONDA_RUN} pip install /opt/torch-tensorrt-builds/torch_tensorrt*.whl
+if [[ ${PLATFORM} == win32 ]]; then
+    pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
+else
+    pip install /opt/torch-tensorrt-builds/torch_tensorrt*.whl
+fi
 
 echo -e "Running test script";
