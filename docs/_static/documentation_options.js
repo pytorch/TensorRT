3f937a9432fc90677ff5c096341273bb075150e1
pytorchbot <pytorchbot@pytorch.com>
2024-05-31 11:35:01 +0000

    2024-05-31 nightly release (36e48f23f235647bf36c04da4f0106dcb4da9dae)

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2.4.0.dev0+2cc80d8',
+    VERSION: 'v2.4.0.dev0+2eb1267',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
