b8d289caa223eaecccbc536c789e54afd773bb4d
Lan Luo <lanl@nvidia.com>
2024-06-03 13:47:34 -0700

    Fix lint error

--- py/torch_tensorrt/dynamo/conversion/impl/embedding.py
+++ py/torch_tensorrt/dynamo/conversion/impl/embedding.py
@@ -99,7 +99,7 @@ def embedding_bag_with_traversable_offsets(
         if len(offsets.shape) != 1:
             raise TypeError(
                 f"The offsets should be in 1d array, here offset shape is {offsets.shape}."
-        )
+            )
         offsets[-1] = len_embed
     else:
         # add the end index to offsets
