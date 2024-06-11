32632f571f9e63c70f3fd52e91cc1d70139cf214
pytorchbot <pytorchbot@pytorch.com>
2024-06-11 11:34:58 +0000

    2024-06-11 nightly release (db51f18c83ef66127cd313a4211fc83f1f5fc995)

--- py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
+++ py/torch_tensorrt/dynamo/conversion/aten_ops_converters.py
@@ -223,7 +223,7 @@ def aten_ops_group_norm(
     )
 
 
-@dynamo_tensorrt_converter(torch.ops.aten.cat.default)
+@dynamo_tensorrt_converter(torch.ops.aten.cat.default, supports_dynamic_shapes=True)
 def aten_ops_cat(
     ctx: ConversionContext,
     target: Target,
@@ -1368,7 +1368,7 @@ def aten_ops_abs(
     )
 
 
-@dynamo_tensorrt_converter(torch.ops.aten.sin.default)
+@dynamo_tensorrt_converter(torch.ops.aten.sin.default, supports_dynamic_shapes=True)
 def aten_ops_sin(
     ctx: ConversionContext,
     target: Target,
@@ -1385,7 +1385,7 @@ def aten_ops_sin(
     )
 
 
-@dynamo_tensorrt_converter(torch.ops.aten.cos.default)
+@dynamo_tensorrt_converter(torch.ops.aten.cos.default, supports_dynamic_shapes=True)
 def aten_ops_cos(
     ctx: ConversionContext,
     target: Target,
