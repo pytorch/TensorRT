11d029388231352f0985809aad4ac4a8c1041153
HolyWu <holywu@gmail.com>
2024-06-12 21:14:35 +0800

    Add back CUDA version specialization for bindings and libs

--- packaging/pre_build_script_windows.sh
+++ packaging/pre_build_script_windows.sh
@@ -4,7 +4,7 @@ pip install -U numpy packaging pyyaml setuptools wheel
 
 # Install TRT from PyPI
 TRT_VERSION=$(python -c "import yaml; print(yaml.safe_load(open('dev_dep_versions.yml', 'r'))['__tensorrt_version__'])")
-pip install tensorrt==${TRT_VERSION} tensorrt-cu12-bindings==${TRT_VERSION} tensorrt-cu12-libs==${TRT_VERSION} --extra-index-url https://pypi.nvidia.com
+pip install tensorrt==${TRT_VERSION} tensorrt-${CU_VERSION::4}-bindings==${TRT_VERSION} tensorrt-${CU_VERSION::4}-libs==${TRT_VERSION} --extra-index-url https://pypi.nvidia.com
 
 choco install bazelisk -y
 
