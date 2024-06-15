6130294e3af076590990816ea7f7d42f4091eb73
apbose <apbose694@gmail.com>
2024-06-14 20:04:12 -0700

    scatter CI failures- second try

--- py/torch_tensorrt/dynamo/lowering/_decompositions.py
+++ py/torch_tensorrt/dynamo/lowering/_decompositions.py
@@ -209,7 +209,8 @@ def slice_scatter_decomposition(
     for index in range(start, end, step):
         cat_tensors.append(index * torch.ones(index_tensor_shape, dtype=torch.int64))
     index_tensor = torch.stack(cat_tensors, dim).cuda()
-    output_tensor = torch.scatter(input_tensor, dim, index_tensor, src_tensor)
+    index_tensor_64 = index_tensor.to(torch.int64)
+    output_tensor = torch.scatter(input_tensor, dim, index_tensor_64, src_tensor)
     return output_tensor
 
 
