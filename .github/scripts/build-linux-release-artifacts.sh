ac8dce98ca886554585357e7948703752c85861e
lanluo-nvidia <lanl@nvidia.com>
2024-06-16 23:07:47 -0700

    test

--- .github/scripts/build-linux-release-artifacts.sh
+++ .github/scripts/build-linux-release-artifacts.sh
@@ -55,18 +55,16 @@ libtorchtrt() {
     PY_NAME=python${PYTHON_VERSION}
     PY_DIR=/opt/python/${PY_BUILD_CODE}
 
-    python -m pip install -r ${PROJECT_DIR}/py/requirements.txt
-
-    TORCH_VERSION=$(python -c "from torch import __version__;print(__version__.split('+')[0])")
+    #python -m pip install -r ${PROJECT_DIR}/py/requirements.txt
 
     if [[ '${pre_cxx11_abi}' == 'true' ]]; then
       bazel build //:libtorchtrt --config pre_cxx11_abi --platforms //toolchains:ci_rhel_x86_64_linux -c opt --noshow_progress
       cp ${PROJECT_DIR}/bazel-bin/libtorchtrt.tar.gz \
-      ${PROJECT_DIR}/dist/libtorchtrt-${TORCHTRT_VERSION}-pre-cxx11-abi-tensorrt${TRT_VERSION}-cuda${CU_VERSION}-libtorch${TORCH_VERSION}-x86_64-linux.tar.gz
+      ${PROJECT_DIR}/dist/libtorchtrt-${TORCHTRT_VERSION}-pre-cxx11-abi-tensorrt${TRT_VERSION}-cuda${CU_VERSION}-libtorch${PYTORCH_VERSION}-x86_64-linux.tar.gz
     else
       bazel build //:libtorchtrt --platforms //toolchains:ci_rhel_x86_64_linux -c opt --noshow_progress
       cp ${PROJECT_DIR}/bazel-bin/libtorchtrt.tar.gz \
-      ${PROJECT_DIR}/dist/libtorchtrt-${TORCHTRT_VERSION}-tensorrt${TRT_VERSION}-cuda${CU_VERSION}-libtorch${TORCH_VERSION}-x86_64-linux.tar.gz
+      ${PROJECT_DIR}/dist/libtorchtrt-${TORCHTRT_VERSION}-tensorrt${TRT_VERSION}-cuda${CU_VERSION}-libtorch${PYTORCH_VERSION}-x86_64-linux.tar.gz
     fi
 }
 
