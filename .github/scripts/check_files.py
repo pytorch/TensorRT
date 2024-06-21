965dd132cd8fbee434afc5d28a4e757ae69feb9a
lanluo-nvidia <lanl@nvidia.com>
2024-06-21 15:45:34 -0700

    Test only: Only run certain test cases if the folder has changed

---
+++ .github/scripts/check_files.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""
+Check whether a test suites should be run or not through github actions
+"""
+dummy_output_folders = ("tests/py/dynamo", "tests/py/core")
+print(dummy_output_folders)
