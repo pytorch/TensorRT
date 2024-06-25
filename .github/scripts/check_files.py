17ce09376e363473e85adf97c0beda5fc42b8cdf
lanluo-nvidia <lanl@nvidia.com>
2024-06-25 11:24:58 -0700

    test

--- .github/scripts/check_files.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-"""
-Check whether a test suites should be run or not through github actions
-"""
-dummy_output_folders = ("tests/py/dynamo", "tests/py/core")
-print(dummy_output_folders)
