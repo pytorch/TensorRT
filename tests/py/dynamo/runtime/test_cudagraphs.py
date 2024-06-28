39906b0348942648a211ffe773b9fde58628f789
gs-olive <113141689+gs-olive@users.noreply.github.com>
2024-06-28 13:08:50 -0700

    fix: Test failure + new test cases

--- tests/py/dynamo/runtime/test_cudagraphs.py
+++ tests/py/dynamo/runtime/test_cudagraphs.py
@@ -107,6 +107,93 @@ class TestCudagraphs(TestCase):
         )
         torch._dynamo.reset()
 
+    def test_cudagraphs_enabled_fallback_inference_python(self):
+        torch_tensorrt.runtime.set_cudagraphs_mode(True)
+
+        class SampleModel(torch.nn.Module):
+            def forward(self, x):
+                return torch.softmax((x + 2) * 7, dim=0)
+
+        inputs = [
+            torch.randn(
+                3,
+                5,
+                7,
+            ).cuda()
+        ]
+
+        fx_graph = torch.fx.symbolic_trace(SampleModel())
+
+        # Validate that the results between Torch and Torch-TRT are similar
+        optimized_model = torch_tensorrt.compile(
+            fx_graph,
+            "torch_compile",
+            inputs,
+            min_block_size=1,
+            pass_through_build_failures=True,
+            torch_executed_ops={"torch.ops.aten.mul.Tensor"},
+            use_python_runtime=True,
+        )
+        optimized_model_results = optimized_model(*inputs).detach().cpu()
+        torch_model_results = fx_graph(*inputs).detach().cpu()
+
+        max_diff = float(
+            torch.max(torch.abs(optimized_model_results - torch_model_results))
+        )
+        self.assertAlmostEqual(
+            max_diff,
+            0,
+            DECIMALS_OF_AGREEMENT,
+            msg=f"Safe Mode Python TRT outputs don't match with the original model.",
+        )
+        torch._dynamo.reset()
+
+    @unittest.skipIf(
+        not torch_tensorrt.ENABLED_FEATURES.torch_tensorrt_runtime,
+        "Torch-TensorRT runtime is not available",
+    )
+    def test_cudagraphs_enabled_fallback_inference_cpp(self):
+        class SampleModel(torch.nn.Module):
+            def forward(self, x):
+                return torch.softmax((x + 2) * 7, dim=0)
+
+        inputs = [
+            torch.randn(
+                3,
+                5,
+                7,
+            ).cuda()
+        ]
+
+        fx_graph = torch.fx.symbolic_trace(SampleModel())
+
+        # Validate that the results between Torch and Torch-TRT are similar
+        optimized_model = torch_tensorrt.compile(
+            fx_graph,
+            "torch_compile",
+            inputs,
+            min_block_size=1,
+            pass_through_build_failures=True,
+            torch_executed_ops={"torch.ops.aten.mul.Tensor"},
+            use_python_runtime=False,
+        )
+
+        with torch_tensorrt.runtime.enable_cudagraphs():
+            optimized_model_results = optimized_model(*inputs).detach().cpu()
+
+        torch_model_results = fx_graph(*inputs).detach().cpu()
+
+        max_diff = float(
+            torch.max(torch.abs(optimized_model_results - torch_model_results))
+        )
+        self.assertAlmostEqual(
+            max_diff,
+            0,
+            DECIMALS_OF_AGREEMENT,
+            msg=f"Safe Mode C++ TRT outputs don't match with the original model.",
+        )
+        torch._dynamo.reset()
+
 
 if __name__ == "__main__":
     run_tests()
