cc47ac8d4c7f64fb94887398010afe975fa9da7f
kee hyun an <keehyuna@nvidia.com>
2024-07-03 16:25:04 +0900

    feat: dynamic shape support for pow/mod/eq operator

--- tests/py/dynamo/conversion/test_pow_aten.py
+++ tests/py/dynamo/conversion/test_pow_aten.py
@@ -59,6 +59,65 @@ class TestPowConverter(DispatchTestCase):
             inputs,
         )
 
+    @parameterized.expand(
+        [
+            (
+                "2d_dim_dtype_half",
+                (1, 1),
+                (2, 2),
+                (4, 4),
+                torch.half,
+                torch.half,
+            ),
+            (
+                "3d_dim_dtype_float",
+                (1, 1, 1),
+                (1, 2, 3),
+                (3, 3, 3),
+                torch.float,
+                torch.float,
+            ),
+        ]
+    )
+    def test_pow_dynamic_shape(
+        self, _, min_shape, opt_shape, max_shape, type, output_type
+    ):
+        class pow(nn.Module):
+            def forward(self, lhs_val, rhs_val):
+                return torch.ops.aten.floor_divide.default(lhs_val, rhs_val)
+
+        class pow_scalar(nn.Module):
+            def forward(self, lhs_val, rhs_val):
+                return torch.ops.aten.pow.Tensor_Scalar(lhs_val, 2.0)
+
+        class pow_operator(nn.Module):
+            def forward(self, lhs_val, rhs_val):
+                return lhs_val**rhs_val
+
+        input_specs = [
+            Input(
+                min_shape=min_shape,
+                opt_shape=opt_shape,
+                max_shape=max_shape,
+                dtype=type,
+            ),
+            Input(
+                min_shape=min_shape,
+                opt_shape=opt_shape,
+                max_shape=max_shape,
+                dtype=type,
+            ),
+        ]
+        self.run_test_with_dynamic_shape(
+            pow(), input_specs, output_dtypes=[output_type]
+        )
+        self.run_test_with_dynamic_shape(
+            pow_scalar(), input_specs, output_dtypes=[output_type]
+        )
+        self.run_test_with_dynamic_shape(
+            pow_operator(), input_specs, output_dtypes=[output_type]
+        )
+
 
 if __name__ == "__main__":
     run_tests()
