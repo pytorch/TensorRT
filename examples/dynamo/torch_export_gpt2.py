0d00d8c9ae872bf74bb93d8926c48089810f8279
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-07-11 11:14:17 -0700

    chore: updates

--- examples/dynamo/torch_export_gpt2.py
+++ examples/dynamo/torch_export_gpt2.py
@@ -1,40 +1,7 @@
 import torch
 import torch_tensorrt
-from transformers import AutoModelForCausalLM, AutoTokenizer, StoppingCriteriaList
-from transformers.generation.stopping_criteria import (
-    EosTokenCriteria,
-    MaxLengthCriteria,
-)
-
-
-def export_gpt2(model, inputs):
-    """
-    Exports the llama2 model into an ExportedProgram
-    """
-    with torch.no_grad():
-        # max=1024 has contraint violation error. https://github.com/pytorch/pytorch/issues/125604
-        seq_len = torch.export.Dim("seq_len", min=1, max=1024)
-        try:
-            print("Trying to export the model using torch.export.export()..")
-            # strict=False only enables aotautograd tracing and excludes dynamo.
-            ep = torch.export.export(
-                model, (inputs,), dynamic_shapes=({1: seq_len},), strict=False
-            )
-        except:
-            print(
-                "Trying torch.export._trace._export to trace the graph since torch.export.export() failed"
-            )
-            # This API is used to express the constraint violation guards as asserts in the graph.
-            ep = torch.export._trace._export(
-                model,
-                (inputs,),
-                dynamic_shapes=({1: seq_len},),
-                strict=False,
-                _allow_complex_guards_as_runtime_asserts=True,
-            )
-
-    return ep
-
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from utils import export_llm, generate
 
 # Define tokenizer and model
 torch_device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -44,6 +11,7 @@ model = (
         "gpt2",
         pad_token_id=tokenizer.eos_token_id,
         use_cache=False,
+        attn_implementation="eager",
     )
     .eval()
     .to(torch_device)
@@ -54,51 +22,32 @@ model_inputs = tokenizer("I enjoy walking with my cute dog", return_tensors="pt"
     torch_device
 )
 input_ids = model_inputs["input_ids"]
-max_tokens = 40
+max_tokens = 20
 
-# Pyt model outputs
-greedy_output = model.generate(**model_inputs, max_new_tokens=max_tokens)
-pyt_outputs = model(input_ids)
+# Auto-regressive generation loop for greedy search using PyTorch model
+pyt_gen_tokens = generate(model, input_ids, max_tokens, tokenizer.eos_token_id)
 
 # Compile Torch-TRT model
-gpt2_ep = export_gpt2(model, input_ids)
+gpt2_ep = export_llm(model, input_ids, max_seq_len=1024)
 trt_model = torch_tensorrt.dynamo.compile(
     gpt2_ep,
     inputs=[input_ids],
     enabled_precisions={torch.float32},
     truncate_double=True,
-    torch_executed_ops={"torch.ops.aten.slice.Tensor"},
-    disable_tf32=True,
+    debug=True,
 )
 
-trt_outputs = trt_model(input_ids)
-
-# Auto-regressive generation loop for greedy search
-max_length = len(input_ids) + max_tokens
-stopping_criteria = StoppingCriteriaList(
-    [
-        MaxLengthCriteria(max_length=max_length),
-        EosTokenCriteria(eos_token_id=tokenizer.eos_token_id),
-    ]
-)
-token_id = 0
-while token_id < max_tokens:
-    trt_outputs = model(input_ids)
-    logits = trt_outputs.logits
-    next_token_logits = logits[:, -1, :]
-    next_tokens = torch.argmax(next_token_logits, dim=-1)
-    input_ids = torch.cat([input_ids, next_tokens[:, None]], dim=-1)
-    if stopping_criteria(input_ids, logits).item():
-        break
-    token_id += 1
+# Auto-regressive generation loop for greedy search using Torch-TensorRT model
+generated_token_ids = generate(trt_model, input_ids, max_tokens, tokenizer.eos_token_id)
 
 # Decode the sentence
+print("=============================")
 print(
     "Pytorch model generated text: ",
-    tokenizer.decode(greedy_output[0], skip_special_tokens=True),
+    tokenizer.decode(pyt_gen_tokens[0], skip_special_tokens=True),
 )
 print("=============================")
 print(
     "TensorRT model generated text: ",
-    tokenizer.decode(input_ids[0], skip_special_tokens=True),
+    tokenizer.decode(generated_token_ids[0], skip_special_tokens=True),
 )
--- examples/dynamo/torch_export_llama2.py
+++ examples/dynamo/torch_export_llama2.py
@@ -1,40 +1,7 @@
 import torch
 import torch_tensorrt
-from transformers import AutoModelForCausalLM, AutoTokenizer, StoppingCriteriaList
-from transformers.generation.stopping_criteria import (
-    EosTokenCriteria,
-    MaxLengthCriteria,
-)
-
-
-def export_llama2(model, inputs):
-    """
-    Exports the llama2 model into an ExportedProgram
-    """
-    with torch.no_grad():
-        # max=1024 has contraint violation error. https://github.com/pytorch/pytorch/issues/125604
-        seq_len = torch.export.Dim("seq_len", min=1, max=16)
-        try:
-            print("Trying to export the model using torch.export.export()..")
-            # strict=False only enables aotautograd tracing and excludes dynamo.
-            ep = torch.export.export(
-                model, (inputs,), dynamic_shapes=({1: seq_len},), strict=False
-            )
-        except:
-            print(
-                "Trying torch.export._trace._export to trace the graph since torch.export.export() failed"
-            )
-            # This API is used to express the constraint violation guards as asserts in the graph.
-            ep = torch.export._trace._export(
-                model,
-                (inputs,),
-                dynamic_shapes=({1: seq_len},),
-                strict=False,
-                _allow_complex_guards_as_runtime_asserts=True,
-            )
-
-    return ep
-
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from utils import export_llm, generate
 
 # Define the Llama2 model from hugging face
 # kv_cache is not supported in Torch-TRT currently.
@@ -55,9 +22,11 @@ input_ids = base_inputs.input_ids
 
 max_tokens = 40
 pyt_out = model(input_ids)
-# generate_ids = model.generate(base_inputs.input_ids, max_length=max_tokens)
 
-llama2_ep = export_llama2(model, input_ids)
+# Auto-regressive generation loop for greedy search using PyTorch model
+pyt_gen_tokens = generate(model, input_ids, max_tokens, tokenizer.eos_token_id)
+
+llama2_ep = export_llm(model, input_ids)
 trt_model = torch_tensorrt.dynamo.compile(
     llama2_ep,
     inputs=[input_ids],
@@ -69,42 +38,27 @@ trt_model = torch_tensorrt.dynamo.compile(
 )
 
 trt_out = trt_model(input_ids)
-# breakpoint()
-# print("Mean diff: ", torch.mean(torch.abs(pyt_out.logits-trt_out.logits)))
-print("Mean diff: ", torch.mean(torch.abs(pyt_out.logits - trt_out.logits)))
-breakpoint()
+
 # Auto-regressive generation loop for greedy search
-max_length = len(input_ids) + max_tokens
-stopping_criteria = StoppingCriteriaList(
-    [
-        MaxLengthCriteria(max_length=max_length),
-        EosTokenCriteria(eos_token_id=tokenizer.eos_token_id),
-    ]
-)
+generated_token_ids = generate(trt_model, input_ids, max_tokens, tokenizer.eos_token_id)
 
-token_id = 0
-while token_id < max_tokens:
-    print("Generating token: ", token_id)
-    trt_outputs = model(input_ids)
-    logits = trt_outputs.logits
-    next_token_logits = logits[:, -1, :]
-    next_tokens = torch.argmax(next_token_logits, dim=-1)
-    input_ids = torch.cat([input_ids, next_tokens[:, None]], dim=-1)
-    if stopping_criteria(input_ids, logits).item():
-        break
-    token_id += 1
-
+# Check output difference
+print("Mean diff: ", torch.mean(torch.abs(pyt_out.logits - trt_out.logits)))
 
 # Decode the sentence
 print("=============================")
-# print(
-#     "Pytorch model generated text: ",
-#     tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0],
-# )
+print(
+    "Pytorch model generated text: ",
+    tokenizer.batch_decode(
+        pyt_gen_tokens, skip_special_tokens=True, clean_up_tokenization_spaces=False
+    )[0],
+)
 print("=============================")
 print(
     "TensorRT model generated text: ",
     tokenizer.batch_decode(
-        input_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False
+        generated_token_ids,
+        skip_special_tokens=True,
+        clean_up_tokenization_spaces=False,
     )[0],
 )
