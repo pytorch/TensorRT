f6a4f416d4def13de174a00297a1e43babe88d0d
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-07-30 14:41:50 -0700

    chore: updates

--- tools/perf/benchmark_hf.sh
+++ tools/perf/benchmark_hf.sh
@@ -5,8 +5,8 @@ backends=("torch" "dynamo" "inductor")
 
 # GPT2 model context length = 1024
 echo "Benchmarking GPT2 model"
-isl=(128 256)
-osl=(256 512)
+isl=(256 512)
+osl=(512 1024)
 for bs in ${batch_sizes[@]}
   do
     for backend in ${backends[@]}
@@ -32,8 +32,8 @@ rm -rf ~/.cache/huggingface/hub/
 
 # "meta-llama/Llama-2-7b-chat-hf" model, context length = 4K
 echo "Benchmarking Llama2-7B model"
-isl=(128 256)
-osl=(256 512)
+isl=(512 1024)
+osl=(1024 2048)
 for bs in ${batch_sizes[@]}
   do
     for backend in ${backends[@]}
@@ -58,8 +58,8 @@ rm -rf ~/.cache/huggingface/hub/
 
 # "meta-llama/Meta-Llama-3-8B" model, context length = 8K
 echo "Benchmarking Llama3 model"
-isl=(128 256)
-osl=(256 512)
+isl=(512 1024)
+osl=(1024 2048)
 for bs in ${batch_sizes[@]}
   do
     for backend in ${backends[@]}
@@ -84,8 +84,8 @@ rm -rf ~/.cache/huggingface/hub/
 
 # "meta-llama/Meta-Llama-3.1-8B-Instruct" model, context length = 128K
 echo "Benchmarking Llama3.1 model"
-isl=(128 256)
-osl=(256 512)
+isl=(512 1024)
+osl=(1024 2048)
 for bs in ${batch_sizes[@]}
   do
     for backend in ${backends[@]}
@@ -102,6 +102,32 @@ for bs in ${batch_sizes[@]}
                             --truncate \
                             --backends ${backend} \
                             --report "llama3.1_8b_instruct_perf_bs${bs}_backend_${backend}_isl${isl[i]}_osl${osl[i]}.csv"
+          done
+        done
+  done
+# Clear HF cache
+rm -rf ~/.cache/huggingface/hub/
+
+# "mistralai/Mistral-7B-Instruct-v0.3" model, context length 32k
+echo "Benchmarking mistralai/Mistral-7B-Instruct-v0.3 model"
+isl=(512 1024)
+osl=(1024 2048)
+for bs in ${batch_sizes[@]}
+  do
+    for backend in ${backends[@]}
+        do
+        for i in ${!isl[@]};
+          do
+          echo "Benchmarking mistralai/Mistral-7B-Instruct-v0.3 model for bs ${bs} with ISL ${isl[i]}, OSL ${osl[i]} and backend ${backend}"
+          python perf_run.py --model_torch "mistralai/Mistral-7B-Instruct-v0.3" \
+                            --is_text_llm \
+                            --precision fp16 \
+                            --inputs "(${bs}, ${isl[i]})@int64" \
+                            --output_sequence_length ${osl[i]} \
+                            --batch_size ${bs} \
+                            --truncate \
+                            --backends ${backend} \
+                            --report "mistralai_7b_v0.3_perf_bs${bs}_backend_${backend}_isl${isl[i]}_osl${osl[i]}.csv"
           done
         done
   done
@@ -134,32 +160,6 @@ rm -rf ~/.cache/huggingface/hub/
 #   done
 # # Clear HF cache
 # rm -rf ~/.cache/huggingface/hub/
-
-# "mistralai/Mistral-7B-Instruct-v0.3" model, context length 32k
-echo "Benchmarking mistralai/Mistral-7B-Instruct-v0.3 model"
-isl=(128 256)
-osl=(256 512)
-for bs in ${batch_sizes[@]}
-  do
-    for backend in ${backends[@]}
-        do
-        for i in ${!isl[@]};
-          do
-          echo "Benchmarking mistralai/Mistral-7B-Instruct-v0.3 model for bs ${bs} with ISL ${isl[i]}, OSL ${osl[i]} and backend ${backend}"
-          python perf_run.py --model_torch "mistralai/Mistral-7B-Instruct-v0.3" \
-                            --is_text_llm \
-                            --precision fp16 \
-                            --inputs "(${bs}, ${isl[i]})@int64" \
-                            --output_sequence_length ${osl[i]} \
-                            --batch_size ${bs} \
-                            --truncate \
-                            --backends ${backend} \
-                            --report "mistralai_7b_v0.3_perf_bs${bs}_backend_${backend}_isl${isl[i]}_osl${osl[i]}.csv"
-          done
-        done
-  done
-# Clear HF cache
-rm -rf ~/.cache/huggingface/hub/
 
 # "microsoft/Phi-3-mini-4k-instruct" model, context length = 4K
 # echo "Benchmarking microsoft/Phi-3-mini-4k-instruct model"
