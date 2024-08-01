2d5776e4b4de7b793d3c33eaf1322267d4ae904d
apbose <apbose694@gmail.com>
2024-08-01 11:01:08 -0700

    Dynamic shape chunk

--- tests/py/dynamo/conversion/test_chunk_aten.py
+++ tests/py/dynamo/conversion/test_chunk_aten.py
@@ -1,6 +1,7 @@
 import torch
 from parameterized import parameterized
 from torch.testing._internal.common_utils import run_tests
+from torch_tensorrt import Input
 
 from .harness import DispatchTestCase
 
@@ -75,6 +76,95 @@ class TestChunkConverter(DispatchTestCase):
         self.run_test(
             TestChunk(),
             input,
+        )
+
+    @parameterized.expand(
+        [
+            ((1,), (1,), (3,), 3, 0),
+            ((3,), (3,), (4,), 3, 0),
+            ((4,), (4,), (6,), 3, 0),
+            ((6,), (6,), (9,), 3, 0),
+            ((3,), (3,), (4,), 1, -1),
+            ((3,), (3,), (4,), 3, -1),
+            ((3,), (3,), (4,), 4, -1),
+        ]
+    )
+    def test_chunk_1D(self, min_shape, opt_shape, max_shape, chunks, dim):
+        class TestChunk(torch.nn.Module):
+            def forward(self, input):
+                out = torch.ops.aten.chunk.default(input, chunks, dim)
+                return out
+
+        input_specs = [
+            Input(
+                min_shape=min_shape,
+                opt_shape=opt_shape,
+                max_shape=max_shape,
+            ),
+        ]
+        self.run_test_with_dynamic_shape(
+            TestChunk(),
+            input_specs,
+        )
+
+    @parameterized.expand(
+        [
+            ((3, 4), (3, 4), (4, 4), 1, 0),
+            ((3, 4), (3, 4), (4, 4), 3, 0),
+            ((3, 4), (3, 4), (4, 4), 4, 0),
+            ((3, 4), (3, 4), (4, 4), 2, -2),
+            ((3, 4), (3, 4), (4, 4), 6, -2),
+            ((3, 4), (3, 4), (4, 4), 3, 1),
+            ((3, 4), (3, 4), (4, 4), 4, 1),
+            ((3, 4), (3, 4), (4, 4), 5, -1),
+        ]
+    )
+    def test_chunk_2D(self, min_shape, opt_shape, max_shape, chunks, dim):
+        class TestChunk(torch.nn.Module):
+            def forward(self, input):
+                out = torch.ops.aten.chunk.default(input, chunks, dim)
+                return out
+
+        input_specs = [
+            Input(
+                min_shape=min_shape,
+                opt_shape=opt_shape,
+                max_shape=max_shape,
+            ),
+        ]
+        self.run_test_with_dynamic_shape(
+            TestChunk(),
+            input_specs,
+        )
+
+    @parameterized.expand(
+        [
+            ((3, 4, 2), (3, 4, 2), (4, 4, 2), 1, 0),
+            ((3, 4, 2), (3, 4, 2), (4, 4, 2), 3, -3),
+            ((3, 4, 2), (3, 4, 2), (4, 4, 2), 3, 1),
+            ((3, 4, 2), (3, 4, 2), (4, 4, 2), 4, 1),
+            ((3, 4, 2), (3, 4, 2), (4, 4, 2), 6, -2),
+            ((3, 4, 2), (3, 4, 2), (4, 4, 2), 1, 2),
+            ((3, 4, 2), (3, 4, 2), (4, 4, 2), 3, -1),
+            ((3, 4, 2), (3, 4, 2), (4, 4, 2), 4, -1),
+        ]
+    )
+    def test_chunk_3D(self, min_shape, opt_shape, max_shape, chunks, dim):
+        class TestChunk(torch.nn.Module):
+            def forward(self, input):
+                out = torch.ops.aten.chunk.default(input, chunks, dim)
+                return out
+
+        input_specs = [
+            Input(
+                min_shape=min_shape,
+                opt_shape=opt_shape,
+                max_shape=max_shape,
+            ),
+        ]
+        self.run_test_with_dynamic_shape(
+            TestChunk(),
+            input_specs,
         )
 
 
