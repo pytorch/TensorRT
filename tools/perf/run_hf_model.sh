58dae5af167d8fd4a4c1adbfe3d1de81216afbc2
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-08-05 14:19:46 -0700

    chore: updates

--- tools/perf/run_hf_model.sh
+++ tools/perf/run_hf_model.sh
@@ -6,7 +6,7 @@ isl=$4
 osl=$5
 precision=$6
 iterations=$7
-model_name=$(echo "$model_name" | sed 's/\//-/g')
+modified_model_name=$(echo "$model_name" | sed 's/\//-/g')
 echo "Benchmarking ${model_name} model for bs ${batch_size} with ISL ${isl[i]}, OSL ${osl[i]} and backend ${backend} for ${iterations} iterations"
 python perf_run.py --model_torch ${model_name} \
                 --is_text_llm \
@@ -17,7 +17,7 @@ python perf_run.py --model_torch ${model_name} \
                 --truncate \
                 --backends ${backend} \
                 --iterations ${iterations} \
-                --report "${model_name}_perf_bs${bs}_backend_${backend}_isl${isl[i]}_osl${osl[i]}.csv"
+                --report "${modified_model_name}_perf_bs${bs}_backend_${backend}_isl${isl[i]}_osl${osl[i]}.csv"
 
 # Clear HF cache
 rm -rf ~/.cache/huggingface/hub/
