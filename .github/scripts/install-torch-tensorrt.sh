02fb58183261fb4a7000ddbd61f190ce989bb967
lanluo-nvidia <lanl@nvidia.com>
2024-08-09 19:59:19 -0700

    test

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -1,10 +1,12 @@
-#set -eou pipefail
-
-set -x
+set -eou pipefail
 
 TORCH_TORCHVISION=$(grep "^torch" ${PWD}/py/requirements.txt)
 INDEX_URL=https://download.pytorch.org/whl/${CHANNEL}/${CU_VERSION}
 PLATFORM=$(python -c "import sys; print(sys.platform)")
+
+echo -e "TORCH_TORCHVISION=${TORCH_TORCHVISION}"
+echo -e "INDEX_URL=${INDEX_URL}"
+echo -e "PLATFORM=${PLATFORM}"
 
 # Install all the dependencies required for Torch-TensorRT
 pip install --pre ${TORCH_TORCHVISION} --index-url ${INDEX_URL}
@@ -12,6 +14,7 @@ pip install --pre -r ${PWD}/tests/py/requirements.txt --use-deprecated legacy-re
 
 # Install Torch-TensorRT
 if [[ ${PLATFORM} == win32 ]]; then
+    echo -e "RUNNER_ARTIFACT_DIR=${RUNNER_ARTIFACT_DIR}"
     pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
 else
     pip install /opt/torch-tensorrt-builds/torch_tensorrt*.whl
