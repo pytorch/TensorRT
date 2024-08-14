8603555fafae697caa1b7c3556eeaed3c62b2cb3
lanluo-nvidia <lanl@nvidia.com>
2024-08-14 14:48:48 -0700

    test

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-set -exou pipefail
+set -x
 
 # Install dependencies
 python3 -m pip install pyyaml
+
+yum install -y ninja-build gettext
 
 wget https://github.com/bazelbuild/bazelisk/releases/download/v1.17.0/bazelisk-linux-amd64 \
     && mv bazelisk-linux-amd64 /usr/bin/bazel \
@@ -13,4 +15,5 @@ export TORCH_BUILD_NUMBER=$(python -c "import torch, urllib.parse as ul; print(u
 export TORCH_INSTALL_PATH=$(python -c "import torch, os; print(os.path.dirname(torch.__file__))")
 
 cat toolchains/ci_workspaces/MODULE.bazel.tmpl | envsubst > MODULE.bazel
+
 export CI_BUILD=1
