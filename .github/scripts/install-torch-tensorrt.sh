fd6c43014d751b23e99c1bd10836a2e89733b7f9
lanluo-nvidia <lanl@nvidia.com>
2024-08-14 11:36:08 -0700

    test

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -4,12 +4,17 @@ TORCH_TORCHVISION=$(grep "^torch" ${PWD}/py/requirements.txt)
 INDEX_URL=https://download.pytorch.org/whl/${CHANNEL}/${CU_VERSION}
 PLATFORM=$(python -c "import sys; print(sys.platform)")
 
-cat ${PWD}/MODULE.bazel
-
 echo "PLATFORM=${PLATFORM}"
 # Install all the dependencies required for Torch-TensorRT
 pip install --pre ${TORCH_TORCHVISION} --index-url ${INDEX_URL}
 pip install --pre -r ${PWD}/tests/py/requirements.txt --use-deprecated legacy-resolver
+
+export CUDA_HOME="$(echo ${CUDA_PATH} | sed -e 's#\\#\/#g')"
+export TORCH_INSTALL_PATH="$(python -c "import torch, os; print(os.path.dirname(torch.__file__))" | sed -e 's#\\#\/#g')"
+
+cat ${PWD}toolchains/ci_workspaces/MODULE.bazel.tmpl | envsubst > ${PWD}/MODULE.bazel
+
+cat ${PWD}/MODULE.bazel
 
 # Install Torch-TensorRT
 if [[ ${PLATFORM} == win32 ]]; then
