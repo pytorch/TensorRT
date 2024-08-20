bc39698ed244ba8363f227a5531cc9e6f3642a7f
lanluo-nvidia <lanl@nvidia.com>
2024-08-20 10:21:55 -0700

    fix tensorrt dependencies issue

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -23,6 +23,15 @@ pip install --pre -r tests/py/requirements.txt --use-deprecated legacy-resolver
 export TORCH_BUILD_NUMBER=$(python -c "import torch, urllib.parse as ul; print(ul.quote_plus(torch.__version__))")
 export TORCH_INSTALL_PATH=$(python -c "import torch, os; print(os.path.dirname(torch.__file__))")
 
+if [[ "${CU_VERSION::4}" < "cu12" ]]; then
+  # replace dependencies from tensorrt-cu12-bindings/libs to tensorrt-cu11-bindings/libs
+  sed -i -e "s/tensorrt-cu12==/tensorrt-${CU_VERSION::4}==/g" \
+         -e "s/tensorrt-cu12-bindings==/tensorrt-${CU_VERSION::4}-bindings==/g" \
+         -e "s/tensorrt-cu12-libs==/tensorrt-${CU_VERSION::4}-libs==/g" \
+         pyproject.toml
+  cat pyproject.toml
+fi
+
 cat toolchains/ci_workspaces/MODULE.bazel.tmpl | envsubst > MODULE.bazel
 
 cat MODULE.bazel
--- packaging/pre_build_script_windows.sh
+++ packaging/pre_build_script_windows.sh
@@ -4,9 +4,18 @@ pip install -U numpy packaging pyyaml setuptools wheel
 
 # Install TRT from PyPI
 TRT_VERSION=$(python -c "import yaml; print(yaml.safe_load(open('dev_dep_versions.yml', 'r'))['__tensorrt_version__'])")
-pip install tensorrt==${TRT_VERSION} tensorrt-${CU_VERSION::4}-bindings==${TRT_VERSION} tensorrt-${CU_VERSION::4}-libs==${TRT_VERSION} --extra-index-url https://pypi.nvidia.com
+pip install tensorrt==${TRT_VERSION} tensorrt-${CU_VERSION::4}==${TRT_VERSION} tensorrt-${CU_VERSION::4}-bindings==${TRT_VERSION} tensorrt-${CU_VERSION::4}-libs==${TRT_VERSION} --extra-index-url https://pypi.nvidia.com
 
 choco install bazelisk -y
+
+if [[ "${CU_VERSION::4}" < "cu12" ]]; then
+  # replace dependencies from tensorrt-cu12-bindings/libs to tensorrt-cu11-bindings/libs
+  sed -i -e "s/tensorrt-cu12==/tensorrt-${CU_VERSION::4}==/g" \
+         -e "s/tensorrt-cu12-bindings==/tensorrt-${CU_VERSION::4}-bindings==/g" \
+         -e "s/tensorrt-cu12-libs==/tensorrt-${CU_VERSION::4}-libs==/g" \
+         pyproject.toml
+  cat pyproject.toml
+fi
 
 #curl -Lo TensorRT.zip https://developer.download.nvidia.com/compute/machine-learning/tensorrt/10.0.1/zip/TensorRT-10.0.1.6.Windows10.win10.cuda-12.4.zip
 #unzip -o TensorRT.zip -d C:/
