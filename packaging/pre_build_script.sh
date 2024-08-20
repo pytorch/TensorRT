440dc06c247ef240274a2089ba8674c4a5673e17
lanluo-nvidia <lanl@nvidia.com>
2024-08-20 13:53:24 -0700

    test

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -15,8 +15,8 @@ TORCH_TORCHVISION=$(grep "^torch" py/requirements.txt)
 INDEX_URL=https://download.pytorch.org/whl/${CHANNEL}/${CU_VERSION}
 
 TRT_VERSION=$(python -c "import yaml; print(yaml.safe_load(open('dev_dep_versions.yml', 'r'))['__tensorrt_version__'])")
-pip install -y --no-dependencies tensorrt==${TRT_VERSION} --extra-index-url https://pypi.nvidia.com
-pip install -y tensorrt-${CU_VERSION::4}==${TRT_VERSION} tensorrt-${CU_VERSION::4}-bindings==${TRT_VERSION} tensorrt-${CU_VERSION::4}-libs==${TRT_VERSION} --extra-index-url https://pypi.nvidia.com
+pip install --no-dependencies tensorrt==${TRT_VERSION} --extra-index-url https://pypi.nvidia.com
+pip install tensorrt-${CU_VERSION::4}==${TRT_VERSION} tensorrt-${CU_VERSION::4}-bindings==${TRT_VERSION} tensorrt-${CU_VERSION::4}-libs==${TRT_VERSION} --extra-index-url https://pypi.nvidia.com
 
 # Install all the dependencies required for Torch-TensorRT
 pip uninstall -y torch torchvision
