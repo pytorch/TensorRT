Moved output check to the util. Redo refit if fast refit yields incorrect result when varify_output is set to True

tests/py/dynamo/models/test_model_refit.py
@@ -91,8 +91,8 @@ def test_mapping():
 @pytest.mark.unit
 def test_refit_one_engine_with_weightmap():
 
-    model = models.resnet18(pretrained=True).eval().to("cuda")
-    model2 = models.resnet18(pretrained=False).eval().to("cuda")
+    model = models.resnet18(pretrained=False).eval().to("cuda")
+    model2 = models.resnet18(pretrained=True).eval().to("cuda")
     inputs = [torch.randn((1, 3, 224, 224)).to("cuda")]
     enabled_precisions = {torch.float}
     debug = False
@@ -117,6 +117,7 @@ def test_refit_one_engine_with_weightmap():
         new_weight_module=exp_program2,
         arg_inputs=inputs,
         use_weight_map_cache=True,
+        verify_output=True,
     )
 
     # Check the output