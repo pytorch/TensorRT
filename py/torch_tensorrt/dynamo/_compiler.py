6f30e8a41469a02de6caf06e6bda8ba484ea3761
kee hyun an <keehyuna@nvidia.com>
2024-08-27 15:28:04 +0000

    chore: reset context in rt module

--- py/torch_tensorrt/dynamo/_compiler.py
+++ py/torch_tensorrt/dynamo/_compiler.py
@@ -90,7 +90,7 @@ def compile(
     custom_engine_cache: Optional[BaseEngineCache] = _defaults.CUSTOM_ENGINE_CACHE,
     use_explicit_typing: bool = _defaults.USE_EXPLICIT_TYPING,
     use_fp32_acc: bool = _defaults.USE_FP32_ACC,
-    weight_streaming: bool = _defaults.WEIGHT_STREAMING,
+    enable_weight_streaming: bool = _defaults.WEIGHT_STREAMING,
     **kwargs: Any,
 ) -> torch.fx.GraphModule:
     """Compile an ExportedProgram module for NVIDIA GPUs using TensorRT
@@ -163,7 +163,7 @@ def compile(
         custom_engine_cache (Optional[BaseEngineCache]): Engine cache instance to use for saving and loading engines. Users can provide their own engine cache by inheriting from BaseEngineCache. If used, engine_cache_dir and engine_cache_size will be ignored.
         use_explicit_typing (bool): This flag enables strong typing in TensorRT compilation which respects the precisions set in the Pytorch model. This is useful when users have mixed precision graphs.
         use_fp32_acc (bool): This option inserts cast to FP32 nodes around matmul layers and TensorRT ensures the accumulation of matmul happens in FP32. Use this only when FP16 precision is configured in enabled_precisions.
-        weight_streaming (bool): Enable weight streaming
+        enable_weight_streaming (bool): Enable weight streaming. This enforces stronly typed network
         **kwargs: Any,
     Returns:
         torch.fx.GraphModule: Compiled FX Module, when run it will execute via TensorRT
@@ -303,7 +303,7 @@ def compile(
         "reuse_cached_engines": reuse_cached_engines,
         "use_explicit_typing": use_explicit_typing,
         "use_fp32_acc": use_fp32_acc,
-        "weight_streaming": weight_streaming,
+        "enable_weight_streaming": enable_weight_streaming,
     }
 
     settings = CompilationSettings(**compilation_options)
--- py/torch_tensorrt/runtime/_weight_streaming.py
+++ py/torch_tensorrt/runtime/_weight_streaming.py
@@ -10,98 +10,43 @@ from torch_tensorrt.dynamo.runtime import PythonTorchTensorRTModule, TorchTensor
 logger = logging.getLogger(__name__)
 
 
-class WeightStreamingBudget(object):
-    def __init__(self, name, rt_mod) -> None:
-        self.name = name
-        self.rt_mod = rt_mod
-        self.engine = rt_mod.engine
-
-    def __new__(cls, *args):
-        if tensorrt.__version__ < "10.1":
-            return super().__new__(WeightStreamingBudgetV1)
-        else:
-            return super().__new__(WeightStreamingBudgetV2)
-
-    def get_weight_streaming_budget(self):
-        pass
-
-    def set_weight_streaming_budget(self, budget_bytes):
-        pass
-
-    def set_automatic_streaming_budget(self):
-        pass
-
-    def reset_context(self):
-        self.rt_mod.reset_context()
-
-
-class WeightStreamingBudgetV1(WeightStreamingBudget):
-    def __init__(self, name, rt_mod) -> None:
-        super().__init__(name, rt_mod)
-
-    def get_weight_streaming_budget(self):
-        print("todo")
-
-    def set_weight_streaming_budget(self, budget_bytes):
-        print("todo")
-
-
-class WeightStreamingBudgetV2(WeightStreamingBudget):
-    def __init__(self, name, rt_mod) -> None:
-        super().__init__(name, rt_mod)
-        self.streamable_weights_size = self.engine.streamable_weights_size
-
-    def get_weight_streaming_budget(self):
-        return self.streamable_weights_size
-
-    def set_weight_streaming_budget(self, budget_bytes):
-        self.engine.weight_streaming_budget_v2 = budget_bytes
-        if self.engine.weight_streaming_budget_v2 != budget_bytes:
-            RuntimeError(f"Failed to set weight streaming budget to {budget_bytes}!")
-
-    def set_automatic_streaming_budget(self):
-        budget_bytes = self.engine.get_weight_streaming_automatic_budget()
-        self.engine.weight_streaming_budget_v2 = budget_bytes
-        if self.engine.weight_streaming_budget_v2 != budget_bytes:
-            RuntimeError(f"Failed to set weight streaming budget to {budget_bytes}!")
-
-
 class _WeightStreamingContextManager(object):
     """
     Helper class used to setup weight streaming budget
     """
 
     def __init__(self, module) -> None:
-        ws_budget = []
+        rt_mods = []
         for name, rt_mod in module.named_children():
             if "_run_on_acc" in name and (
                 isinstance(rt_mod, PythonTorchTensorRTModule)
                 or isinstance(rt_mod, TorchTensorRTModule)
             ):
-                ws_budget.append(WeightStreamingBudget(name, rt_mod))
-        self.streamable_budget = [sb.get_weight_streaming_budget() for sb in ws_budget]
-        self.ws_budget = ws_budget
-        self.module = module
+                rt_mods.append((name, rt_mod))
+        self.streamable_budget = [
+            mod.get_weight_streaming_budget() for _, mod in rt_mods
+        ]
+        self.rt_mods = rt_mods
 
     def __enter__(self) -> "_WeightStreamingContextManager":
         return self
 
     def __exit__(self, *args: Any) -> None:
-        self._reset_context()
-
-    def _reset_context(self):
-        for ws_budget in self.ws_budget:
-            ws_budget.reset_context()
+        for _, rt_mod in self.rt_mods:
+            rt_mod.reset_context()
 
     def get_streamable_weight_bytes(self):
         return sum(self.streamable_budget)
 
     def set_streamable_weight_bytes(self, budget_bytes):
-        # The weight streaming budget cannot be modified while there are active IExecutionContexts
-        self._reset_context()
-
+        ws_budget_bytes = 0
         total_bytes = self.get_streamable_weight_bytes()
-        if total_bytes <= budget_bytes:
+        if total_bytes == 0:
+            logger.error(
+                f"streamable bytes are zero. Was module complied with enable_weight_streaming=True option?"
+            )
+            return 0
+        elif total_bytes <= budget_bytes:
             logger.error(
                 f"Requested budget is equal or greater than streamable bytes: {total_bytes}"
             )
@@ -112,11 +57,15 @@ class _WeightStreamingContextManager(object):
             int(streamable_bytes / total_bytes * budget_bytes)
             for streamable_bytes in self.streamable_budget
         ]
-        for i, ws_budget in enumerate(self.ws_budget):
-            ws_budget.set_weight_streaming_budget(normalized_size[i])
-            logger.debug(
-                f"Set weight streaming size {normalized_size[i]} for {ws_budget.name}"
-            )
+        for i, (name, rt_mod) in enumerate(self.rt_mods):
+            ws_budget_bytes += rt_mod.set_weight_streaming_budget(normalized_size[i])
+            logger.debug(f"Set weight streaming size {normalized_size[i]} for {name}")
+        return ws_budget_bytes
+
+    def set_automatic_streaming_budget(self):
+        total_bytes = 0
+        for _, rt_mod in self.rt_mods:
+            total_bytes += rt_mod.set_automatic_streaming_budget()
 
         return total_bytes
 
