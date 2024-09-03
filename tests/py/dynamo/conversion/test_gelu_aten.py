d75f588e49ca241cf44bbdb8aad0ef3d9578da75
HolyWu <holywu@gmail.com>
2024-09-03 22:45:55 +0800

    feat: Support `aten.gelu` dynamo converter (#3134)

--- tests/py/dynamo/conversion/test_gelu_aten.py
+++ tests/py/dynamo/conversion/test_gelu_aten.py
@@ -1,49 +1,67 @@
-import pytest
 import torch
 import torch.nn as nn
+from parameterized import parameterized
 from torch.testing._internal.common_utils import run_tests
 from torch_tensorrt import Input
 
 from .harness import DispatchTestCase
 
 
-@pytest.mark.skip(reason="This test will be skipped.")
-class TestGeLUConverter(DispatchTestCase):
-    def test_gelu(self):
+class TestGELUConverter(DispatchTestCase):
+    @parameterized.expand(
+        [
+            ("none",),
+            ("tanh",),
+        ]
+    )
+    def test_gelu(self, approximate):
         class TestModule(nn.Module):
             def forward(self, x):
-                return torch.ops.aten.gelu.default(x)
+                return torch.ops.aten.gelu.default(x, approximate=approximate)
 
         inputs = [torch.randn(1, 10)]
         self.run_test(TestModule(), inputs)
 
-    def test_gelu_with_dynamic_shape(self):
+    @parameterized.expand(
+        [
+            ("none",),
+            ("tanh",),
+        ]
+    )
+    def test_gelu_with_dynamic_shape(self, approximate):
         class TestModule(nn.Module):
             def forward(self, x):
-                return torch.ops.aten.gelu.default(x)
+                return torch.ops.aten.gelu.default(x, approximate=approximate)
 
         input_specs = [
             Input(
-                shape=(-1, -1, -1),
+                min_shape=(1, 1, 1),
+                opt_shape=(1, 2, 3),
+                max_shape=(3, 3, 3),
                 dtype=torch.float32,
-                shape_ranges=[((1, 1, 1), (1, 2, 3), (3, 3, 3))],
             ),
         ]
         self.run_test_with_dynamic_shape(TestModule(), input_specs)
 
-    def test_gelu_with_dynamic_shape_four_dimensions(self):
+    @parameterized.expand(
+        [
+            ("none",),
+            ("tanh",),
+        ]
+    )
+    def test_gelu_with_dynamic_shape_four_dimensions(self, approximate):
         class TestModule(nn.Module):
             def forward(self, x):
-                return torch.ops.aten.gelu.default(x)
+                return torch.ops.aten.gelu.default(x, approximate=approximate)
 
         input_specs = [
             Input(
-                shape=(-1, -1, -1, -1),
+                min_shape=(1, 1, 1, 5),
+                opt_shape=(1, 2, 3, 5),
+                max_shape=(3, 3, 3, 5),
                 dtype=torch.float32,
-                shape_ranges=[((1, 1, 1, 5), (1, 2, 3, 5), (3, 3, 3, 5))],
             ),
         ]
-
         self.run_test_with_dynamic_shape(TestModule(), input_specs)
 
 
