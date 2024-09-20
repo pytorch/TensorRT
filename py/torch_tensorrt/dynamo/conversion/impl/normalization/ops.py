ffce01bc111551292ff3df5ed9422f307c13cbe9
cehongwang <wangcehong@gmail.com>
2024-09-20 21:00:08 +0000

    Changed test cases

--- py/torch_tensorrt/dynamo/conversion/impl/normalization/ops.py
+++ py/torch_tensorrt/dynamo/conversion/impl/normalization/ops.py
@@ -57,12 +57,12 @@ def batch_norm(
         else get_trt_tensor(ctx, weight, f"{name}_weight")
     )
     bias = (
-        get_trt_tensor(ctx, 1.0, f"{name}_bias")
+        get_trt_tensor(ctx, 0.0, f"{name}_bias")
         if bias is None
         else get_trt_tensor(ctx, bias, f"{name}_bias")
     )
     running_mean = (
-        get_trt_tensor(ctx, 1.0, f"{name}_running_mean")
+        get_trt_tensor(ctx, 0.0, f"{name}_running_mean")
         if running_mean is None
         else get_trt_tensor(ctx, running_mean, f"{name}_running_mean")
     )
