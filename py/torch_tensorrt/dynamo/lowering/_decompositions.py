b6ed1c5896d340c0905e34a816fe0faed86ab3b8
HolyWu <holywu@gmail.com>
2024-10-23 05:47:18 +0800

    chore: Adapt CIA ops decomposition handling in upsample converters to torch 2.6 (#3227)

--- py/torch_tensorrt/dynamo/lowering/_decompositions.py
+++ py/torch_tensorrt/dynamo/lowering/_decompositions.py
@@ -3,7 +3,8 @@ from enum import Enum, auto
 from typing import Any, Callable, Dict, List, Optional
 
 import torch
-from torch._decomp import _decomp_table_to_post_autograd_aten, register_decomposition
+from torch._decomp import register_decomposition
+from torch._export.utils import _decomp_table_to_post_autograd_aten
 from torch._ops import OpOverload
 from torch_tensorrt.dynamo._defaults import default_device
 from torch_tensorrt.dynamo.conversion.converter_utils import get_positive_dim
@@ -411,8 +412,15 @@ def get_decompositions(
         return {**CORE_ATEN_DECOMPOSITIONS_FILTERED, **TORCH_TRT_DECOMPOSITIONS}
     else:
         # changes made here due to torch2.6 changes https://github.com/pytorch/pytorch/pull/135080
+        decomp_table = _decomp_table_to_post_autograd_aten()
+        DECOMP_TABLE_FILTERED: Dict[OpOverload, Callable[[Any], Any]] = {
+            decomp: decomp_table[decomp]
+            for decomp in decomp_table
+            if decomp not in torch_disabled_decompositions
+        }
+
         return {
             **ENABLED_TORCH_DECOMPOSITIONS,
-            **_decomp_table_to_post_autograd_aten(),
+            **DECOMP_TABLE_FILTERED,
             **TORCH_TRT_DECOMPOSITIONS,
         }
