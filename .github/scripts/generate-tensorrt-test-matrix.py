a02d9443cb75fa033f53597df91756d0655b705a
lanluo-nvidia <lanl@nvidia.com>
2024-10-28 09:31:08 -0700

    test

--- .github/scripts/generate-tensorrt-test-matrix.py
+++ .github/scripts/generate-tensorrt-test-matrix.py
@@ -19,24 +19,24 @@ PYTHON_VERSIONS_DICT = {
 
 TENSORRT_VERSIONS_DICT = {
     "windows": {
-        "10.4": {
+        "10.4.0": {
             "urls": "https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.4.0/zip/TensorRT-10.4.0.26.Windows.win10.cuda-12.6.zip",
             "strip_prefix": "TensorRT-10.4.0.26",
             "sha256": "3a7de83778b9e9f812fd8901e07e0d7d6fc54ce633fcff2e340f994df2c6356c",
         },
-        "10.5": {
+        "10.5.0": {
             "urls": "https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.5.0/zip/TensorRT-10.5.0.18.Windows.win10.cuda-12.6.zip",
             "strip_prefix": "TensorRT-10.5.0.18",
             "sha256": "e6436f4164db4e44d727354dccf7d93755efb70d6fbfd6fa95bdfeb2e7331b24",
         },
     },
     "linux": {
-        "10.4": {
+        "10.4.0": {
             "urls": "https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.4.0/tars/TensorRT-10.4.0.26.Linux.x86_64-gnu.cuda-12.6.tar.gz",
             "strip_prefix": "TensorRT-10.4.0.26",
             "sha256": "cb0273ecb3ba4db8993a408eedd354712301a6c7f20704c52cdf9f78aa97bbdb",
         },
-        "10.5": {
+        "10.5.0": {
             "urls": "https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.5.0/tars/TensorRT-10.5.0.18.Linux.x86_64-gnu.cuda-12.6.tar.gz",
             "strip_prefix": "TensorRT-10.5.0.18",
             "sha256": "f404d379d639552a3e026cd5267213bd6df18a4eb899d6e47815bbdb34854958",
@@ -92,8 +92,9 @@ def main(args: list[str]) -> None:
             item["desired_cuda"] in cuda_versions
             and item["python_version"] in python_versions
         ):
-            for _, tensorrt_json in tensorrt_versions.items():
+            for tensorrt_version, tensorrt_json in tensorrt_versions.items():
                 new_item = copy.deepcopy(item)
+                tensorrt_json["version"] = tensorrt_version
                 new_item["tensorrt"] = tensorrt_json
                 filtered_includes.append(new_item)
     filtered_matrix_dict = {}
