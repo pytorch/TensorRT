595ea6e28af38f85a64014e33a0324caec2dfa61
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-10-28 16:50:14 -0700

    chore: updates

--- py/torch_tensorrt/dynamo/runtime/register_fake_class.py
+++ py/torch_tensorrt/dynamo/runtime/register_fake_class.py
@@ -1,20 +1,29 @@
 import base64
-from typing import Any
+from typing import Any, List
 
 import torch
+from torch.fx.experimental.proxy_tensor import unset_fake_temporarily
 
 
-@torch.library.register_fake("tensorrt::execute_engine")
-def fake_tensorrt_execute_engine(inputs, trt_engine):
-    print("==============done==========")
-    return trt_engine(inputs)
+@torch.library.register_fake("tensorrt::execute_engine")  # type: ignore
+def fake_tensorrt_execute_engine(inputs: List[torch.Tensor], trt_engine: Any) -> Any:
+    with unset_fake_temporarily():
+        real_inputs = []
+        for input in inputs:
+            real_inputs.append(
+                torch.rand(input.shape, dtype=input.dtype, device=input.device)
+            )
+
+    # This will call the FakeTRTEngine.__call__ method which runs inference on real TRT engine and inputs and returns the outputs
+    # The output should be fake tensors as per general understanding but we are returning real tensors as outputs here which works.
+    return trt_engine.wrapped_obj(real_inputs)
 
 
 # namespace::class_name
 @torch._library.register_fake_class("tensorrt::Engine")
 class FakeTRTEngine:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, engine_info: List[Any]):
+        self.engine = torch.classes.tensorrt.Engine(engine_info)
 
     @classmethod
     def __obj_unflatten__(cls, flattened_tq: Any) -> Any:
@@ -23,5 +32,10 @@ class FakeTRTEngine:
         engine_info[4] = str(engine_info[4][0])  # input names
         engine_info[5] = str(engine_info[5][0])  # output names
         engine_info[6] = str(int(engine_info[6]))  # hw compatible
-        trt_engine = torch.classes.tensorrt.Engine(engine_info)
-        return trt_engine
+        return cls(engine_info)
+
+    def __call__(self, inputs: List[torch.Tensor]) -> Any:
+        # We use unset_fake_temporarily because if we don't it keeps calling the fake_tensorrt_execute_engine which is the caller.
+        # So, we disable fake tensor mode to explicitly run inference and get outputs of TRTEngine.
+        with unset_fake_temporarily():
+            return torch.ops.tensorrt.execute_engine(inputs, self.engine)
