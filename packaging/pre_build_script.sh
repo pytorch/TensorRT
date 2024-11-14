a38bee7eb8ef3b5b1f8ca6c67690616780bdb4e6
lanluo-nvidia <lanl@nvidia.com>
2024-11-14 13:57:58 -0800

    Add tensorrt test workflow (#3266)

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -21,6 +21,15 @@ pip install --force-reinstall --pre ${TORCH_TORCHVISION} --index-url ${INDEX_URL
 export TORCH_BUILD_NUMBER=$(python -c "import torch, urllib.parse as ul; print(ul.quote_plus(torch.__version__))")
 export TORCH_INSTALL_PATH=$(python -c "import torch, os; print(os.path.dirname(torch.__file__))")
 
+if [[ ${TENSORRT_VERSION} != "" ]]; then
+  # this is the upgraded TensorRT version, replace current tensorrt version to the upgrade tensorRT version in the pyproject.toml
+  current_version=$(cat dev_dep_versions.yml | grep __tensorrt_version__ | sed 's/__tensorrt_version__: //g' | sed 's/"//g')
+  sed -i -e "s/tensorrt-cu12==${current_version}/tensorrt-cu12==${TENSORRT_VERSION}/g" \
+         -e "s/tensorrt-cu12-bindings==${current_version}/tensorrt-cu12-bindings==${TENSORRT_VERSION}/g" \
+         -e "s/tensorrt-cu12-libs==${current_version}/tensorrt-cu12-libs==${TENSORRT_VERSION}/g" \
+          pyproject.toml
+fi
+
 if [[ "${CU_VERSION::4}" < "cu12" ]]; then
   # replace dependencies from tensorrt-cu12-bindings/libs to tensorrt-cu11-bindings/libs
   sed -i -e "s/tensorrt-cu12==/tensorrt-${CU_VERSION::4}==/g" \
@@ -29,7 +38,11 @@ if [[ "${CU_VERSION::4}" < "cu12" ]]; then
          pyproject.toml
 fi
 
-cat toolchains/ci_workspaces/MODULE.bazel.tmpl | envsubst > MODULE.bazel
+if [[ ${TENSORRT_VERSION} != "" ]]; then
+  cat toolchains/ci_workspaces/MODULE_tensorrt.bazel.tmpl | envsubst > MODULE.bazel
+else
+  cat toolchains/ci_workspaces/MODULE.bazel.tmpl | envsubst > MODULE.bazel
+fi
 
 cat MODULE.bazel
 export CI_BUILD=1
