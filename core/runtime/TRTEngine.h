4a5f0d1280f2ebcfdce6d51fc737936869307465
kee hyun an <keehyuna@nvidia.com>
2024-11-18 21:19:14 +0900

    chore: rebase and rename variable

--- core/runtime/TRTEngine.h
+++ core/runtime/TRTEngine.h
@@ -103,7 +103,7 @@ struct TRTEngine : torch::CustomClassHolder {
   std::vector<at::Tensor> input_buffers = {};
   std::vector<at::Tensor> output_buffers = {};
   std::string shape_key = "None";
-  bool cudagraphs_enabled = false;
+  bool prev_cudagraphs_enabled = false;
   bool use_pre_allocated_outputs = true;
   std::vector<at::Tensor> pre_allocated_outputs;
 
