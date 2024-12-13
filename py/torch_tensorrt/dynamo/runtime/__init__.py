d47db765bfe67b5458442d896084e4c3e2650af6
Dheeraj Peri <peri.dheeraj@gmail.com>
2024-12-12 17:30:30 -0800

    fix: Fix meta kernel import in python builds

--- py/torch_tensorrt/dynamo/runtime/__init__.py
+++ py/torch_tensorrt/dynamo/runtime/__init__.py
@@ -1,7 +1,10 @@
+import torch_tensorrt
 from torch_tensorrt.dynamo.runtime._PythonTorchTensorRTModule import (  # noqa: F401
     PythonTorchTensorRTModule,
 )
 from torch_tensorrt.dynamo.runtime._TorchTensorRTModule import (  # noqa: F401
     TorchTensorRTModule,
 )
-from torch_tensorrt.dynamo.runtime.register_fake_class import *
+
+if torch_tensorrt.ENABLED_FEATURES.torch_tensorrt_runtime:
+    from torch_tensorrt.dynamo.runtime.register_fake_class import *
