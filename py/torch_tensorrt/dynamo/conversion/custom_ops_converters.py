68b3346aeda0618402aca34be4defdb9fac64607
apbose <apbose694@gmail.com>
2025-02-13 12:10:45 -0800

    TensorRT-LLM import fix and aot_joint_export specify as explicit setting in dynamo.compile
    TRT-LLM installation utilities and adding test cases
    
    adding the option in _compiler.py
    
    changes in the TRT-LLM loading tool- removing install_wget, install_unzip, install_mpi
    
    Further changes in error logging of the TRT-LLM installation tool
    
    moving the load_tensorrt_llm to dynamo/utils.py
    
    correcting misprint for TRT LLM load
    
    Using python lib for download to make it platform agnostic
    
    dll file path update for windows
    
    correcting the non critical lint error
    
    Including version in versions.txt
    
    linting error fixes and rebase fix
    
    removing Platform enum from converter_utils.py
    
    Addressing review comments- tmp dir for wheel download and wheel extraction, variable for py_version
    
    checks for windows where NCCL backend is not supported
    
    adding checks for windows and jetson devices
    
    Keeping the extracted and deleting download file, restructuring test
    
    modifying the error warning of missing libmpi libs
    
    removing the redundant initializations
    
    adding tests in CI
    
    correcting the skip test condition
    
    install MPI libs for linux x86
    
    adding SBSA to the supported platform of TRT-LLM libs and installing MPI libs for the distributed tests
    
    Using python package for platform detection
    
    using python platform

--- py/torch_tensorrt/dynamo/conversion/custom_ops_converters.py
+++ py/torch_tensorrt/dynamo/conversion/custom_ops_converters.py
@@ -11,15 +11,15 @@ from torch_tensorrt.dynamo.conversion._ConversionContext import ConversionContex
 from torch_tensorrt.dynamo.conversion._ConverterRegistry import (
     dynamo_tensorrt_converter,
 )
-from torch_tensorrt.dynamo.conversion.converter_utils import load_tensorrt_llm
+from torch_tensorrt.dynamo.lowering.passes.fuse_distributed_ops import (
+    tensorrt_fused_nccl_all_gather_op,
+    tensorrt_fused_nccl_reduce_scatter_op,
+)
+from torch_tensorrt.dynamo.utils import load_tensorrt_llm_for_nccl
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
-if load_tensorrt_llm():
-    from torch_tensorrt.dynamo.lowering.passes.fuse_distributed_ops import (
-        tensorrt_fused_nccl_all_gather_op,
-        tensorrt_fused_nccl_reduce_scatter_op,
-    )
+if load_tensorrt_llm_for_nccl():
 
     @dynamo_tensorrt_converter(tensorrt_fused_nccl_all_gather_op)
     def fused_nccl_gather(
