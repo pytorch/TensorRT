3386b92a87ac9006e79b85e71f706824eb60f73f
Evan Li <zewenl@nvidia.com>
2025-02-19 21:00:48 -0800

    bump to cuda 12.8 and TRT 10.8

--- packaging/pre_build_script.sh
+++ packaging/pre_build_script.sh
@@ -24,7 +24,7 @@ export TORCH_INSTALL_PATH=$(python -c "import torch, os; print(os.path.dirname(t
 if [[ ${TENSORRT_VERSION} != "" ]]; then
   # this is the upgraded TensorRT version, replace current tensorrt version to the upgrade tensorRT version in the pyproject.toml
   # example: __tensorrt_version__: ">=10.3.0,<=10.6.0"
-  # replace: tensorrt-cu12>=10.3.0,<=10.6.0 to tensorrt-cu12==10.7.0
+  # replace: tensorrt-cu12>=10.3.0,<=10.6.0 to tensorrt-cu12==10.8.0
   current_version=$(cat dev_dep_versions.yml | grep __tensorrt_version__ | sed 's/__tensorrt_version__: //g' | sed 's/"//g')
   sed -i -e "s/tensorrt-cu12${current_version}/tensorrt-cu12==${TENSORRT_VERSION}/g" \
          -e "s/tensorrt-cu12-bindings${current_version}/tensorrt-cu12-bindings==${TENSORRT_VERSION}/g" \
