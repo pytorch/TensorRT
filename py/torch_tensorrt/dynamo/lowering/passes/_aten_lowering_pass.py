0f744be06c16ca02bb4c3413d78bcb77b2405c5c
Evan Li <zewenl@nvidia.com>
2025-02-28 17:08:47 -0800

    remove sym_size lowering pass

--- py/torch_tensorrt/dynamo/lowering/passes/_aten_lowering_pass.py
+++ py/torch_tensorrt/dynamo/lowering/passes/_aten_lowering_pass.py
@@ -13,7 +13,6 @@ from .remove_assert_nodes import remove_assert_nodes
 from .remove_detach import remove_detach
 from .remove_input_alias_fixing_clones import remove_input_alias_fixing_clones
 from .remove_num_users_is_0_nodes import remove_num_users_is_0_nodes
-from .remove_sym_size_and_constrain_nodes import remove_sym_size_and_constrain_nodes
 from .repair_input_as_output import repair_input_as_output
 from .replace_max_pool_with_indices import replace_max_pool_with_indices
 
@@ -27,7 +26,6 @@ ATEN_POST_LOWERING_PASSES = DynamoPassManager.build_from_passlist(
         replace_max_pool_with_indices,
         remove_assert_nodes,
         accumulate_fp32_matmul,
-        remove_sym_size_and_constrain_nodes,
         remove_num_users_is_0_nodes,
     ]
 )
