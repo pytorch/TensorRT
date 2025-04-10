3ad21560cc5be879f05686dc27432c443d1339ff
Evan Li <zewenl@nvidia.com>
2025-04-09 23:22:27 -0700

    attempt fix

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -4,6 +4,9 @@ TORCH_TORCHVISION=$(grep "^torch" ${PWD}/py/requirements.txt)
 INDEX_URL=https://download.pytorch.org/whl/${CHANNEL}/${CU_VERSION}
 PLATFORM=$(python -c "import sys; print(sys.platform)")
 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
 # Install all the dependencies required for Torch-TensorRT
 pip install --pre ${TORCH_TORCHVISION} --index-url ${INDEX_URL}
 pip install --pre -r ${PWD}/tests/py/requirements.txt --use-deprecated legacy-resolver
