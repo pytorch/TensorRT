af6eb74011dbd2cd2936ad304fa8c58a3e2384ce
apbose <apbose694@gmail.com>
2025-04-14 13:31:32 -0700

    slight code reorg and bug correction for cross_compile

--- py/torch_tensorrt/runtime/_utils.py
+++ py/torch_tensorrt/runtime/_utils.py
@@ -144,6 +144,7 @@ def no_op_placeholder_for_execute_engine(
     serialized_hardware_compatible: str,
     serialized_metadata: str,
     serialized_target_platform: str,
+    serialized_require_output_allocator: str,
 ) -> List[torch.Tensor]:
     raise RuntimeError(
         "The saved model is cross compiled for windows in Linux, should only be loadded in Windows via torch_tensorrt.load_cross_compiled_exported_program() api."
