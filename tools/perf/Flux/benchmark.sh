b06e6328dc82f3b99927a53c2e77f40dd3b948b6
cehongwang <wangcehong@gmail.com>
2025-05-02 03:31:51 +0000

    Optimized lowering and decomposition to benchmark quantization again

--- tools/perf/Flux/benchmark.sh
+++ tools/perf/Flux/benchmark.sh
@@ -3,3 +3,4 @@ huggingface-cli login --token HF_TOKEN
 
 python flux_quantization.py --dtype fp8 > fp8_benchmark.txt
 python flux_quantization.py --dtype int8 > int8_benchmark.txt
+python flux_perf.py > fp16_benchmark.txt
