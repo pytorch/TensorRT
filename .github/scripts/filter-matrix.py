c589931e1ab7bcc7ee6e1fea842f2b14b88c3e85
lanluo-nvidia <lanl@nvidia.com>
2025-05-11 11:22:47 -0700

    add limit_pr_build

--- .github/scripts/filter-matrix.py
+++ .github/scripts/filter-matrix.py
@@ -2,9 +2,11 @@
 
 import argparse
 import json
+import os
 import sys
 from typing import List
 
+# this was introduced to avoid build for py3.13, currently we support py3.13, but keep this for future use with other python versions
 disabled_python_versions: List[str] = []
 
 
@@ -17,15 +19,34 @@ def main(args: list[str]) -> None:
         default="",
     )
 
+    parser.add_argument(
+        "--limit-pr-builds",
+        help="Limit PR builds to single python/cuda config(py3.11/cu12.8): true or false",
+        type=str,
+        choices=["true", "false"],
+        default=os.getenv("LIMIT_PR_BUILDS", "false"),
+    )
+
     options = parser.parse_args(args)
 
     if options.matrix == "":
         raise Exception("--matrix needs to be provided")
+
     matrix_dict = json.loads(options.matrix)
     includes = matrix_dict["include"]
     filtered_includes = []
     for item in includes:
-        if item["python_version"] not in disabled_python_versions:
+        if item["python_version"] in disabled_python_versions:
+            continue
+        if options.limit_pr_builds == "true":
+            # currently if it is the pr build, it build using py3.9 with all cuda versions, we want to change to py3.11 with singlecu12.8
+            if item["desired_cuda"] == "cu128":
+                item["python_version"] = "py3.11"
+                build_names = item["build_name"].split("-")
+                build_names[1] = "py3_11"
+                item["build_name"] = "-".join(build_names)
+                filtered_includes.append(item)
+        else:
             filtered_includes.append(item)
     filtered_matrix_dict = {}
     filtered_matrix_dict["include"] = filtered_includes
