beace9b1fa73835aae4161d16e04ceeaab2dbdcd
lanluo-nvidia <lanl@nvidia.com>
2025-05-12 14:36:53 -0700

    revert the limit pr build will address it in another issue for fail fast

--- .github/scripts/filter-matrix.py
+++ .github/scripts/filter-matrix.py
@@ -10,23 +10,6 @@ from typing import Any, Dict, List
 disabled_python_versions: List[str] = []
 
 
-def replace_aarch64_container_image(item: Dict[str, Any]) -> Dict[str, Any] | None:
-    if item["gpu_arch_type"] == "cuda-aarch64":
-        if item["desired_cuda"] == "cu128":
-            # pytorch image:pytorch/manylinuxaarch64-builder:cuda12.8 comes with glibc2.28
-            # however, TensorRT requires glibc2.31 on aarch64 platform
-            # TODO: in future, if pytorch supports aarch64 with glibc2.31, we should switch to use the pytorch image
-            # item["container_image"] = "sameli/manylinux_2_34_aarch64_cuda_12.8"
-            item["container_image"] = "quay.io/pypa/manylinux_2_34_aarch64"
-            return item
-        else:
-            # for aarch64 do not test on other cuda versions
-            return None
-    # if it is not aarch64, do nothing and return the item
-    else:
-        return item
-
-
 def main(args: list[str]) -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -34,14 +17,6 @@ def main(args: list[str]) -> None:
         help="matrix blob",
         type=str,
         default="",
-    )
-
-    parser.add_argument(
-        "--limit-pr-builds",
-        help="Limit PR builds to single python/cuda config(py3.11/cu12.8): true or false",
-        type=str,
-        choices=["true", "false"],
-        default=os.getenv("LIMIT_PR_BUILDS", "false"),
     )
 
     options = parser.parse_args(args)
@@ -55,17 +30,13 @@ def main(args: list[str]) -> None:
     for item in includes:
         if item["python_version"] in disabled_python_versions:
             continue
-        if options.limit_pr_builds == "true":
-            # currently if it is the pr build, it build using py3.9 with all cuda versions, we want to change to py3.11 with singlecu12.8
-            if item["desired_cuda"] == "cu128":
-                item["python_version"] = "3.11"
-                build_names = item["build_name"].split("-")
-                build_names[1] = "py3_11"
-                item["build_name"] = "-".join(build_names)
-                item = replace_aarch64_container_image(item)
-                filtered_includes.append(item)
+        if item["gpu_arch_type"] == "cuda-aarch64":
+            # pytorch image:pytorch/manylinuxaarch64-builder:cuda12.8 comes with glibc2.28
+            # however, TensorRT requires glibc2.31 on aarch64 platform
+            # TODO: in future, if pytorch supports aarch64 with glibc2.31, we should switch to use the pytorch image
+            item["container_image"] = "quay.io/pypa/manylinux_2_34_aarch64"
+            filtered_includes.append(item)
         else:
-            item = replace_aarch64_container_image(item)
             filtered_includes.append(item)
     filtered_matrix_dict = {}
     filtered_matrix_dict["include"] = filtered_includes
