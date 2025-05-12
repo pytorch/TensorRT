3a0ed336406c41ccfbfe0c6f26e35dbbbe0a8571
lanluo-nvidia <lanl@nvidia.com>
2025-05-11 23:59:49 -0700

    add install cuda for aarch64 test

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -12,6 +12,17 @@ pip install --pre -r ${PWD}/tests/py/requirements.txt
 if [[ ${PLATFORM} == win32 ]]; then
     pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
 else
+    if [[ $(uname -m) == "aarch64" ]]; then
+        # install cuda 12.8 only for aarch64 for now
+        VER="12-8"
+        dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/sbsa/cuda-rhel8.repo
+        dnf -y install cuda-compiler-${VER}.aarch64 \
+                    cuda-libraries-${VER}.aarch64 \
+                    cuda-libraries-devel-${VER}.aarch64
+        dnf clean all
+        export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+        echo "cuda installed successfully"
+    fi
     pip install /opt/torch-tensorrt-builds/torch_tensorrt*.whl
 fi
 
