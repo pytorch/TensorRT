f91467a1e508c7628ddfb0be4f3729c0e168cd21
Marcel <maleo@google.com>
2025-06-25 19:13:17 +0200

    Remove Bazel files from wheel
    Once the wheel is built, the Bazel files are no longer needed.
    
    Fixes #3477
    
    changes for DLFW for l4t target and removal of BUILD files from package
    
    removing the C++ library target not required by cpp and core tests

--- setup.py
+++ setup.py
@@ -611,8 +611,6 @@ if not (PY_ONLY or NO_TS):
     package_data.update(
         {
             "torch_tensorrt": [
-                "BUILD",
-                "WORKSPACE",
                 "include/torch_tensorrt/*.h",
                 "include/torch_tensorrt/core/*.h",
                 "include/torch_tensorrt/core/conversion/*.h",
@@ -642,8 +640,6 @@ elif NO_TS:
     package_data.update(
         {
             "torch_tensorrt": [
-                "BUILD",
-                "WORKSPACE",
                 "include/torch_tensorrt/*.h",
                 "include/torch_tensorrt/core/*.h",
                 "include/torch_tensorrt/core/runtime/*.h",
