4fdc046e8047237fa5306fb5e36d8eef79076feb
lanluo-nvidia <lanl@nvidia.com>
2025-08-05 21:30:58 -0700

    fix windows ci

--- packaging/smoke_test_windows.py
+++ packaging/smoke_test_windows.py
@@ -1,9 +1,12 @@
 import subprocess
 
-import tensorrt  # noqa: F401
 import torch
+import torch_tensorrt
+from torch_tensorrt._utils import is_tensorrt_rtx
 
 print(f"Torch CUDA version: {torch.version.cuda}")
+print(f"Torch TensorRT version: {torch_tensorrt.__version__}")
+print(f"Is TensorRT RTX: {is_tensorrt_rtx()}")
 
 result = subprocess.run(
     ["systeminfo"],
