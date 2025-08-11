7ac3505a904f3379dac28f304595aad9c065e03e
Apurba Bose <44209735+apbose@users.noreply.github.com>
2025-08-11 08:17:26 -1000

    Removal of BAZEL build files from python package and changes to make cpp tests work (#3641)
    Co-authored-by: Marcel <maleo@google.com>

--- setup.py
+++ setup.py
@@ -597,8 +597,6 @@ if not (PY_ONLY or NO_TS):
     package_data.update(
         {
             "torch_tensorrt": [
-                "BUILD",
-                "WORKSPACE",
                 "include/torch_tensorrt/*.h",
                 "include/torch_tensorrt/core/*.h",
                 "include/torch_tensorrt/core/conversion/*.h",
@@ -628,8 +626,6 @@ elif NO_TS:
     package_data.update(
         {
             "torch_tensorrt": [
-                "BUILD",
-                "WORKSPACE",
                 "include/torch_tensorrt/*.h",
                 "include/torch_tensorrt/core/*.h",
                 "include/torch_tensorrt/core/runtime/*.h",
