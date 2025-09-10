e6e0eca7ec67d59cf434fdbbe8ea0e3768722be1
lanluo-nvidia <lanl@nvidia.com>
2025-09-10 12:55:51 -0700

    update 25.10 pytorch base commit (#3816)

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ def get_git_revision_short_hash() -> str:
         )
     except:
         print("WARNING: Could not get git revision short hash, using default one")
-        # in release/ngc/25.10 branch this is the commit hash of the pytorch commit that is used for dlfw package
-        return "0000000"
+        # 25.10 dlfw branch pytorch short commit hash
+        return "145a3a7"
 
 
 def get_base_version() -> str:
