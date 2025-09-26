89c2a2a5b41f3da8c0b64aa6c4dd2f1167157693
lanluo-nvidia <lanl@nvidia.com>
2025-09-26 13:26:50 -0700

    skip llm bfloat16 in rtx

--- tests/py/dynamo/llm/test_llm_models.py
+++ tests/py/dynamo/llm/test_llm_models.py
@@ -16,7 +16,8 @@ from torchtrt_ext import register_sdpa
 @pytest.mark.unit
 @pytest.mark.parametrize("precision", ["FP16", "BF16", "FP32"])
 def test_llm_decoder_layer(precision):
-
+    if torch_tensorrt.ENABLED_FEATURES.tensorrt_rtx and precision == "BF16":
+        pytest.skip("TensorRT-RTX does not support bfloat16, skipping test")
     with torch.inference_mode():
         args = argparse.Namespace()
         args.debug = False
