c02c7d30819b960bfebfe5a5b365d0c72194626f
apbose <apbose694@gmail.com>
2025-10-08 13:16:15 -0700

    updating the hash in the github setup.py

--- setup.py
+++ setup.py
@@ -49,8 +49,8 @@ def get_git_revision_short_hash() -> str:
         )
     except:
         print("WARNING: Could not get git revision short hash, using default one")
-        # in release/ngc/25.10 branch this is the commit hash of the pytorch commit that is used for dlfw package
-        return "0000000"
+        # in release/ngc/25.11 branch this is the commit hash of the pytorch commit that is used for dlfw package
+        return "b558c986e8"
 
 
 def get_base_version() -> str:
