3894730a1fe2ef59e2a3d981556098adf5f12c75
Lan Luo <lanl@nvidia.com>
2026-01-22 13:07:54 -0800

    update 26.02 short commit hash

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def get_git_revision_short_hash() -> str:
     except:
         print("WARNING: Could not get git revision short hash, using default one")
         # in release/ngc/25.10 branch this is the commit hash of the pytorch commit that is used for dlfw package
-        return "0000000"
+        return "eb65b36"
 
 
 def get_base_version() -> str:
