720c2d63ef3355e878e6cab475d7f9e14a5e669e
lanluo-nvidia <lanl@nvidia.com>
2026-02-12 10:05:03 -0800

    upgrade trt from 10.14.1 to 10.15.1 (#4075)

--- setup.py
+++ setup.py
@@ -777,7 +777,7 @@ def get_sbsa_requirements(base_requirements):
         # also due to we use sbsa torch_tensorrt wheel for thor, so when we build sbsa wheel, we need to only include tensorrt dependency.
         return requirements + [
             "torch>=2.11.0.dev,<2.12.0",
-            "tensorrt>=10.14.1,<10.15.0",
+            "tensorrt>=10.15.1,<10.16.0",
         ]
 
 
@@ -794,7 +794,7 @@ def get_x86_64_requirements(base_requirements):
             ]
         else:
             requirements = requirements + [
-                "tensorrt>=10.14.1,<10.15.0",
+                "tensorrt>=10.15.1,<10.16.0",
             ]
             cuda_version = torch.version.cuda
             if cuda_version.startswith("12"):
@@ -802,16 +802,16 @@ def get_x86_64_requirements(base_requirements):
                 # which will cause the conflict due to cuda-toolkit 13 is also pulled in, so we need to specify tensorrt_cu12 here
                 tensorrt_prefix = "tensorrt-cu12"
                 requirements = requirements + [
-                    f"{tensorrt_prefix}>=10.14.1,<10.15.0",
-                    f"{tensorrt_prefix}-bindings>=10.14.1,<10.15.0",
-                    f"{tensorrt_prefix}-libs>=10.14.1,<10.15.0",
+                    f"{tensorrt_prefix}>=10.15.1,<10.16.0",
+                    f"{tensorrt_prefix}-bindings>=10.15.1,<10.16.0",
+                    f"{tensorrt_prefix}-libs>=10.15.1,<10.16.0",
                 ]
             elif cuda_version.startswith("13"):
                 tensorrt_prefix = "tensorrt-cu13"
                 requirements = requirements + [
-                    f"{tensorrt_prefix}>=10.14.1,<10.15.0,!=10.14.1.48",
-                    f"{tensorrt_prefix}-bindings>=10.14.1,<10.15.0,!=10.14.1.48",
-                    f"{tensorrt_prefix}-libs>=10.14.1,<10.15.0,!=10.14.1.48",
+                    f"{tensorrt_prefix}>=10.15.1,<10.16.0",
+                    f"{tensorrt_prefix}-bindings>=10.15.1,<10.16.0",
+                    f"{tensorrt_prefix}-libs>=10.15.1,<10.16.0",
                 ]
             else:
                 raise ValueError(f"Unsupported CUDA version: {cuda_version}")
@@ -825,8 +825,6 @@ def get_requirements():
         "typing-extensions>=4.7.0",
         "dllist",
         "psutil",
-        # dummy package as a WAR for the tensorrt dependency on nvidia-cuda-runtime-cu13
-        "nvidia-cuda-runtime-cu13==0.0.0a0",
     ]
 
     if IS_JETPACK:
