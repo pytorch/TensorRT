fe1c6f40014adf4e18625b0d1ca7bc42c8232a64
apbose <apbose694@gmail.com>
2026-04-09 13:47:12 -0700

    moving setup_nccl_library call to example script

--- tools/llm/tensor_parallel_llama_llm.py
+++ tools/llm/tensor_parallel_llama_llm.py
@@ -93,6 +93,9 @@ logger.info(
 )
 
 import torch_tensorrt
+from torch_tensorrt.dynamo.runtime._nccl_utils import setup_nccl_library
+
+setup_nccl_library()
 from torch.distributed._tensor import Replicate, Shard
 from torch.distributed.tensor.parallel import (
     ColwiseParallel,
