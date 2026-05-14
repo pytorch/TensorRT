e559086f16eca15f8ee0d5190600294d467c3630
Victor Stone <vstone@nvidia.com>
2026-05-14 12:06:27 -0700

    Keep dependency-group CI changes on the active pytest path and use python -m pip for runner installs.

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -12,8 +12,8 @@ if [[ $(uname -m) == "aarch64" ]]; then
 fi
 
 # Install all the dependencies required for Torch-TensorRT
-pip install --upgrade "pip>=25.1" "tomli>=1.1.0; python_version < '3.11'"
-pip install \
+python -m pip install --upgrade "pip>=25.1" "tomli>=1.1.0; python_version < '3.11'"
+python -m pip install \
     --pre \
     --extra-index-url https://pypi.nvidia.com \
     --extra-index-url https://download.pytorch.org/whl/nightly/cu130 \
@@ -40,9 +40,9 @@ PY
 # test dependencies might install a different version of torch or torchvision
 # eg. timm will install the latest torchvision, however we want to use the torchvision from nightly
 # reinstall torch torchvision to make sure we have the correct version
-pip uninstall -y torch torchvision
-pip install --force-reinstall --pre ${TORCHVISION} --index-url ${INDEX_URL} --extra-index-url https://pypi.org/simple
-pip install --force-reinstall --pre ${TORCH} --index-url ${INDEX_URL} --extra-index-url https://pypi.org/simple
+python -m pip uninstall -y torch torchvision
+python -m pip install --force-reinstall --pre ${TORCHVISION} --index-url ${INDEX_URL} --extra-index-url https://pypi.org/simple
+python -m pip install --force-reinstall --pre ${TORCH} --index-url ${INDEX_URL} --extra-index-url https://pypi.org/simple
 
 # If CUDA 13 (cu13), prepend venv's NVIDIA CUDA 13 libs to LD_LIBRARY_PATH
 if [[ "${CU_VERSION}" == cu13* ]]; then
@@ -52,9 +52,9 @@ fi
 
 # Install Torch-TensorRT
 if [[ ${PLATFORM} == win32 ]]; then
-    pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
+    python -m pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
 else
-    pip install /opt/torch-tensorrt-builds/torch_tensorrt*.whl --use-deprecated=legacy-resolver
+    python -m pip install /opt/torch-tensorrt-builds/torch_tensorrt*.whl --use-deprecated=legacy-resolver
 fi
 
 echo -e "Running test script";
