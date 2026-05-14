b5299b2a214ee0893dd6054547194dae2a04c043
Victor Stone <vstone@nvidia.com>
2026-05-14 12:30:31 -0700

    Move all Python test dependencies to pyproject (#4256)

--- .github/scripts/install-torch-tensorrt.sh
+++ .github/scripts/install-torch-tensorrt.sh
@@ -2,7 +2,6 @@
 set -x
 
 TORCH=$(grep "^torch>" ${PWD}/py/requirements.txt)
-TORCHVISION=$(grep "^torchvision>" ${PWD}/tests/py/requirements.txt)
 INDEX_URL=https://download.pytorch.org/whl/${CHANNEL}/${CU_VERSION}
 PLATFORM=$(python -c "import sys; print(sys.platform)")
 
@@ -13,13 +12,37 @@ if [[ $(uname -m) == "aarch64" ]]; then
 fi
 
 # Install all the dependencies required for Torch-TensorRT
-pip install --pre -r ${PWD}/tests/py/requirements.txt
-# dependencies in the tests/py/requirements.txt might install a different version of torch or torchvision
+python -m pip install --upgrade "pip>=25.1" "tomli>=1.1.0; python_version < '3.11'"
+python -m pip install \
+    --pre \
+    --extra-index-url https://pypi.nvidia.com \
+    --extra-index-url https://download.pytorch.org/whl/nightly/cu130 \
+    --group test \
+    --group test-ext \
+    --group quantization
+TORCHVISION=$(python - <<'PY'
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+with open("pyproject.toml", "rb") as f:
+    deps = tomllib.load(f)["dependency-groups"]["test-ext"]
+
+for dep in deps:
+    if dep.startswith("torchvision"):
+        print(dep)
+        break
+else:
+    raise SystemExit("torchvision was not found in dependency group test-ext")
+PY
+)
+# test dependencies might install a different version of torch or torchvision
 # eg. timm will install the latest torchvision, however we want to use the torchvision from nightly
-# reinstall torch torchvisionto make sure we have the correct version
-pip uninstall -y torch torchvision
-pip install --force-reinstall --pre ${TORCHVISION} --index-url ${INDEX_URL} --extra-index-url https://pypi.org/simple
-pip install --force-reinstall --pre ${TORCH} --index-url ${INDEX_URL} --extra-index-url https://pypi.org/simple
+# reinstall torch torchvision to make sure we have the correct version
+python -m pip uninstall -y torch torchvision
+python -m pip install --force-reinstall --pre ${TORCHVISION} --index-url ${INDEX_URL} --extra-index-url https://pypi.org/simple
+python -m pip install --force-reinstall --pre ${TORCH} --index-url ${INDEX_URL} --extra-index-url https://pypi.org/simple
 
 # If CUDA 13 (cu13), prepend venv's NVIDIA CUDA 13 libs to LD_LIBRARY_PATH
 if [[ "${CU_VERSION}" == cu13* ]]; then
@@ -29,9 +52,9 @@ fi
 
 # Install Torch-TensorRT
 if [[ ${PLATFORM} == win32 ]]; then
-    pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
+    python -m pip install ${RUNNER_ARTIFACT_DIR}/torch_tensorrt*.whl
 else
-    pip install /opt/torch-tensorrt-builds/torch_tensorrt*.whl --use-deprecated=legacy-resolver
+    python -m pip install /opt/torch-tensorrt-builds/torch_tensorrt*.whl --use-deprecated=legacy-resolver
 fi
 
 echo -e "Running test script";
