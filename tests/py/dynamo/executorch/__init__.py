415d53ecb27f003e90925b805e5ff6ab287a48e4
Lan Luo <lanl@nvidia.com>
2026-05-15 15:01:15 -0700

    test

--- tests/py/dynamo/executorch/__init__.py
+++ tests/py/dynamo/executorch/__init__.py
@@ -1,1 +1,5 @@
+from pkgutil import extend_path
 
+# CI runs these tests from tests/py/dynamo, where this package would otherwise
+# shadow the installed ExecuTorch package and hide executorch.exir.
+__path__ = extend_path(__path__, __name__)
