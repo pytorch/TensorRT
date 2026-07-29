d53faea873a52fe6c088cab474ff5730ad49dd95
Lan Luo <lanl@nvidia.com>
2026-07-29 10:23:33 -0700

    add python 3.14 support

--- .github/scripts/filter-matrix.py
+++ .github/scripts/filter-matrix.py
@@ -6,8 +6,8 @@ import os
 import sys
 from typing import Any, Dict, List
 
-# currently we don't support python 3.13t due to tensorrt does not support 3.13t
-disabled_python_versions: List[str] = ["3.13t", "3.14", "3.14t"]
+# currently we don't support python 3.13t, 3.14t due to tensorrt does not support 3.13t, 3.14t
+disabled_python_versions: List[str] = ["3.13t", "3.14t"]
 disabled_cuda_versions: List[str] = []
 
 # jetpack 6.2 only officially supports python 3.10 and cu126
