b4e4b54fa29f68684ed121edc6fad7371c14a11f
Lan Luo <lanl@nvidia.com>
2026-08-13 11:40:36 -0700

    fix

--- tests/py/dynamo/executorch/test_api.py
+++ tests/py/dynamo/executorch/test_api.py
@@ -117,6 +117,7 @@ def test_runtime_extension_has_dependency_wheel_rpaths():
     assert "$ORIGIN/../tensorrt_libs" in cmake
     assert "$ORIGIN/../nvidia/cuda_runtime/lib" in cmake
     assert "$ORIGIN/../nvidia/cu13/lib" in cmake
+    assert "-Wl,-Bsymbolic" not in cmake
 
 
 def _setup_tree():
