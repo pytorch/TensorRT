1b026915030a8261a8590c339751e6380dddbf21
Dongwoon Hyun <dhyun@aurora.tech>
2026-09-16 12:20:03 -0700

    Fixed the underlying lift_fresh_copy failure by using register_buffer
    The device=x.device change didn't fix the underlying problem. Test passes locally when rebasing this branch onto main (can't get 2.11 to run locally for some reason).

--- tests/py/dynamo/conversion/test_scatter_aten.py
+++ tests/py/dynamo/conversion/test_scatter_aten.py
@@ -338,12 +338,15 @@ class TestScatterDtypeFixConverter(DispatchTestCase):
 
     def test_scatter_src_dtype_mismatch_after_argmax(self):
         class TestModule(torch.nn.Module):
-            def forward(self, x):
-                index = torch.tensor(
-                    [[0, 1], [1, 0]], dtype=torch.int64, device=x.device
+            def __init__(self):
+                super().__init__()
+                self.register_buffer(
+                    "index", torch.tensor([[0, 1], [1, 0]], dtype=torch.int64)
                 )
+
+            def forward(self, x):
                 src = torch.argmax(x, dim=1, keepdim=True).expand(2, 2)
-                return torch.ops.aten.scatter.src(x, 1, index, src)
+                return torch.ops.aten.scatter.src(x, 1, self.index, src)
 
         inputs = [torch.arange(8, dtype=torch.int64).reshape(2, 4)]
         self.run_test(TestModule(), inputs, use_dynamo_tracer=True)
