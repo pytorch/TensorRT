2a7d601d617840d8be3645e512438075b8bc21bb
Dongwoon Hyun <dhyun@aurora.tech>
2026-09-16 15:38:26 -0700

    Fix BF16 TypeError

--- py/torch_tensorrt/dynamo/conversion/impl/select.py
+++ py/torch_tensorrt/dynamo/conversion/impl/select.py
@@ -5,7 +5,9 @@ import logging
 from typing import List, Optional, Sequence, Union
 
 import numpy as np
+import tensorrt as trt
 import torch
+from tensorrt import ITensor
 from torch.fx.node import Target
 from torch_tensorrt._enums import dtype
 from torch_tensorrt.dynamo._SourceIR import SourceIR
@@ -22,10 +24,7 @@ from torch_tensorrt.dynamo.conversion.converter_utils import (
 )
 from torch_tensorrt.dynamo.conversion.impl.elementwise import convert_binary_elementwise
 from torch_tensorrt.dynamo.conversion.impl.shape import shape as get_shape
-from torch_tensorrt.dynamo.utils import DYNAMIC_DIM, Frameworks, unified_dtype_converter
-
-import tensorrt as trt
-from tensorrt import ITensor
+from torch_tensorrt.dynamo.utils import DYNAMIC_DIM
 
 _LOGGER: logging.Logger = logging.getLogger(__name__)
 
@@ -555,11 +554,11 @@ def scatter(
             )
         else:
             # Static shape: use numpy to create the filled tensor.
-            input_np_dtype = unified_dtype_converter(input.dtype, Frameworks.NUMPY)
             src_tensor = get_trt_tensor(
                 ctx,
-                np.full(index_shape_list, src, dtype=input_np_dtype),
+                np.full(index_shape_list, src),
                 name + "_value_tensor",
+                dtype=input.dtype,
             )
     # scatter.src
     elif not (isinstance(src, ITensor)):
